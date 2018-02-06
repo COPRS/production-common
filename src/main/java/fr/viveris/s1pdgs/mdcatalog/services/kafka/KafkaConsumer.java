c2677027b8ed6899d8f29dcaa4751a61c88c99cf
Anonymous <anon@anon.com>
2018-02-06 15:11:33 +0100

    SPDGS-4: Initialize the catalog and publish a metadata send by kafka in Elastic

---
+++ src/main/java/fr/viveris/s1pdgs/mdcatalog/services/kafka/KafkaConsumer.java
@@ -0,0 +1,63 @@
+package fr.viveris.s1pdgs.mdcatalog.services.kafka;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import fr.viveris.s1pdgs.mdcatalog.model.dto.KafkaMetadataDto;
+import fr.viveris.s1pdgs.mdcatalog.services.es.EsServices;
+
+/**
+ * KAFKA consumer.
+ * Consume on a topic defined in configuration file
+ * @author Cyrielle Gailliard
+ *
+ */
+@Service
+public class KafkaConsumer {
+
+	/**
+	 * Logger
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+	@Autowired
+	private EsServices esServices;
+
+	/**
+	 * Count down latch which allows the POJO to signal that a message is received
+	 */
+	private CountDownLatch latchMetadata = new CountDownLatch(1);
+
+	/**
+	 * Message listener container. Read a message
+	 * 
+	 * @param payload
+	 */
+	@KafkaListener(topics = "${kafka.topic.metadata}", groupId = "${kafka.group-id}")
+	public void receive(KafkaMetadataDto metadata) {
+		LOGGER.debug("[receive] Consume message {}", metadata);
+		this.latchMetadata.countDown();
+		// Create metadata
+		if (metadata.getAction().equals("CREATE")) {
+			try {
+				if (esServices.isMetadataExist(metadata.getMetadata())) {
+					esServices.createMetadata(metadata.getMetadata());
+				}
+				LOGGER.info("Metadata created for {}", metadata.getMetadata().getString("productName"));
+			} catch (Exception e){
+				LOGGER.error(e.getMessage());
+			}
+		} else {
+			LOGGER.error("Invalid action {} for metadata {}", metadata.getAction(), metadata.getMetadata());
+		}
+	}
+	
+	public CountDownLatch getLatchMetadata() {
+		return this.latchMetadata;
+	}
+}
