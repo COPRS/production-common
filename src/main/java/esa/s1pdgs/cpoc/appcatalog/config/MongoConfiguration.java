7aef6cd2652880340e87f8d7d455b4424bd8c605
Anonymous <anon@anon.com>
2018-07-27 12:00:26 +0200

    Set a list of servers

--- src/main/java/esa/s1pdgs/cpoc/appcatalog/config/MongoConfiguration.java
+++ src/main/java/esa/s1pdgs/cpoc/appcatalog/config/MongoConfiguration.java
@@ -4,6 +4,9 @@
 package esa.s1pdgs.cpoc.appcatalog.config;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
 
 /**
  * Configuration for the MongoDB client
@@ -22,7 +26,7 @@ import com.mongodb.MongoClient;
 public class MongoConfiguration {
 
     @Value("${mongodb.host}")
-    private String mongoDBHost;
+    private List<String> mongoDBHost;
 
     @Value("${mongodb.port}")
     private int mongoDBPort;
@@ -31,7 +35,11 @@ public class MongoConfiguration {
     private String mongoDBDatabase;
     
     public @Bean MongoClient mongoClient() {
-        return new MongoClient(mongoDBHost, mongoDBPort);
+        List<ServerAddress> servers = new ArrayList<>();
+        mongoDBHost.forEach(host -> {
+            servers.add(new ServerAddress(host, mongoDBPort));
+        });
+        return new MongoClient(servers);
     }
 
     public @Bean MongoTemplate mongoTemplate() {
