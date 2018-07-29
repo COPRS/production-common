d9a1d62968c20b7cc9a9a7092b6457095ada75ba
Anonymous <anon@anon.com>
2018-07-29 08:00:50 +0200

    Add client error for the new API of the calatog: get and get nb reading

---
+++ src/main/java/esa/s1pdgs/cpoc/common/errors/appcatalog/AppCatalogMqiGetApiError.java
@@ -0,0 +1,71 @@
+package esa.s1pdgs.cpoc.common.errors.appcatalog;
+
+import esa.s1pdgs.cpoc.common.ProductCategory;
+import esa.s1pdgs.cpoc.common.errors.AbstractCodedException;
+
+/**
+ * @author Viveris Technologies
+ */
+public class AppCatalogMqiGetApiError extends AbstractCodedException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8096417675612082535L;
+    /**
+     * Category
+     */
+    private final ProductCategory category;
+
+    /**
+     * Force
+     */
+    private final String uri;
+
+    /**
+     * @param category
+     * @param message
+     */
+    public AppCatalogMqiGetApiError(final ProductCategory category,
+            final String uri, final String message) {
+        super(ErrorCode.APPCATALOG_MQI_GET_API_ERROR, message);
+        this.category = category;
+        this.uri = uri;
+    }
+
+    /**
+     * @param category
+     * @param message
+     * @param cause
+     */
+    public AppCatalogMqiGetApiError(final ProductCategory category,
+            final String uri, final String message, final Throwable cause) {
+        super(ErrorCode.APPCATALOG_MQI_GET_API_ERROR, message, cause);
+        this.category = category;
+        this.uri = uri;
+    }
+
+    /**
+     * @return the category
+     */
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    /**
+     * @return the uri
+     */
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public String getLogMessage() {
+        return String.format("[category %s] [uri %s] [msg %s]", category, uri,
+                getMessage());
+    }
+
+}
---
+++ src/test/java/esa/s1pdgs/cpoc/common/errors/appcatalog/AppCatalogMqiGetNbReadingApiErrorTest.java
@@ -0,0 +1,51 @@
+package esa.s1pdgs.cpoc.common.errors.appcatalog;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import esa.s1pdgs.cpoc.common.ProductCategory;
+import esa.s1pdgs.cpoc.common.errors.AbstractCodedException.ErrorCode;
+
+/**
+ * Test the class AppCatalogMqiGetOffsetApiError
+ * 
+ * @author Viveris Technologies
+ */
+public class AppCatalogMqiGetNbReadingApiErrorTest {
+
+    /**
+     * Test the exception
+     */
+    @Test
+    public void test() {
+
+        AppCatalogMqiGetNbReadingApiError e1 = new AppCatalogMqiGetNbReadingApiError(
+                ProductCategory.EDRS_SESSIONS, "uri-mqi", "error-message");
+        assertEquals(ProductCategory.EDRS_SESSIONS, e1.getCategory());
+        assertEquals("uri-mqi", e1.getUri());
+        assertEquals("error-message", e1.getMessage());
+        assertEquals(ErrorCode.APPCATALOG_MQI_NB_READ_API_ERROR,
+                e1.getCode());
+        assertNull(e1.getCause());
+
+        String str1 = e1.getLogMessage();
+        assertTrue(str1.contains("[category EDRS_SESSIONS]"));
+        assertTrue(str1.contains("[uri uri-mqi]"));
+        assertTrue(str1.contains("[msg error-message]"));
+
+        AppCatalogMqiGetNbReadingApiError e2 = new AppCatalogMqiGetNbReadingApiError(
+                ProductCategory.EDRS_SESSIONS, "uri-mqi", "error-message",
+                new Exception("cause-message"));
+        assertEquals(ProductCategory.EDRS_SESSIONS, e2.getCategory());
+        assertEquals("uri-mqi", e1.getUri());
+        assertEquals("error-message", e2.getMessage());
+        assertEquals(ErrorCode.APPCATALOG_MQI_NB_READ_API_ERROR,
+                e2.getCode());
+        assertNotNull(e2.getCause());
+    }
+
+}
