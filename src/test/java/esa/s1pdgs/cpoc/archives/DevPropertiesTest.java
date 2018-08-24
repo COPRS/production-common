57338a9fae150356d89315220bf30551f1114730
Anonymous <anon@anon.com>
2018-08-24 15:55:36 +0200

    adding test feature that download only manifest.safe file

---
+++ src/test/java/esa/s1pdgs/cpoc/archives/DevPropertiesTest.java
@@ -0,0 +1,39 @@
+package esa.s1pdgs.cpoc.archives;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test the properties of development
+ * 
+ * @author Viveris Technologies
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext
+public class DevPropertiesTest {
+
+    /**
+     * Properties to test
+     */
+    @Autowired
+    private DevProperties properties;
+
+    /**
+     * Test the parsing of the properties
+     */
+    @Test
+    public void testInit() {
+        // Check global properties
+        assertEquals(1, properties.getActivations().size());
+        assertTrue(properties.getActivations().get("download-manifest"));
+    }
+
+}
