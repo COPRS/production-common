fb01eb6bd4b30bf27eab19cda6bcf55e285afe7c
Anonymous <anon@anon.com>
2018-08-30 23:06:41 +0200

    Correct unit test

--- src/test/java/esa/s1pdgs/cpoc/common/errors/AbstractCodedExceptionTest.java
+++ src/test/java/esa/s1pdgs/cpoc/common/errors/AbstractCodedExceptionTest.java
@@ -13,7 +13,7 @@ public class AbstractCodedExceptionTest {
 
     @Test
     public void testEnumErroCode() {
-        assertEquals(54, ErrorCode.values().length);
+        assertEquals(60, ErrorCode.values().length);
         
         assertEquals(ErrorCode.ES_CREATION_ERROR,
                 ErrorCode.valueOf("ES_CREATION_ERROR"));
