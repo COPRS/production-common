b1f2d14a8992013b48ac1069cc0b549e95f861c3
Anonymous <anon@anon.com>
2018-09-05 17:26:34 +0200

    SPDGS-301 Externalize configuration

---
+++ config/start_l1.sh
@@ -0,0 +1,3 @@
+/bin/sh
+wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/wrapper/ -P /app --reject="index.html*"
+ENTRYPOINT /var/tmp/configure_pdgs_custom.sh pac1 8 && ulimit -s unlimited && java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-wrapper.jar --spring.config.location=classpath:/application.yml
