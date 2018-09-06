581e376abaf910c881a45c3d4bfdd35c5bff4c14
Anonymous <anon@anon.com>
2018-09-06 11:16:30 +0200

    [FIX]Modification of script

--- config/start.sh
+++ config/start.sh
@@ -1,3 +1,3 @@
 /bin/sh
 wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/archives/ -P /app --reject="index.html*"
-java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-archives.jar --spring.config.location=classpath:/application.yml
+java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-archives.jar --spring.config.location=/app/application.yml
