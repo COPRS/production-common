33c7175f51602899240e57d886c0e17d5b86646c
Anonymous <anon@anon.com>
2018-09-06 11:19:46 +0200

    [FIX]Modification of script

--- config/start.sh
+++ config/start.sh
@@ -1,3 +1,3 @@
 /bin/sh
 wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/mqi-server/ -P /app --reject="index.html*"
-java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=classpath:/application.yml
+java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/application.yml
