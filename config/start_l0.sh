3770994353d586b6ff4297f6f0f2c654c34f2677
Anonymous <anon@anon.com>
2018-09-06 11:28:42 +0200

    [FIX]Modification of script

--- config/start_l0.sh
+++ config/start_l0.sh
@@ -1,3 +1,3 @@
 /bin/sh
 wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/wrapper/ -P /app --reject="index.html*"
-/var/tmp/conf.sh && java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-wrapper.jar --spring.config.location=classpath:/application.yml
+/var/tmp/conf.sh && java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-wrapper.jar --spring.config.location=/app/application.yml
