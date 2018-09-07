e559e975e7c18cd197de1fe9e81c65390acdabdc
Anonymous <anon@anon.com>
2018-09-07 09:10:09 +0200

    FIX scripts

--- config/start_l1.sh
+++ config/start_l1.sh
@@ -1,3 +1,3 @@
-/bin/sh
+#/bin/sh
 wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/l1-wrapper/ -P /app --reject="index.html*"
 /var/tmp/configure_pdgs_custom.sh pac1 8 && ulimit -s unlimited && java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-wrapper.jar --spring.config.location=/app/application.yml
