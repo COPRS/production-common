c04cebde8ccd81d7fcc7921f3e07597f245df6de
Anonymous <anon@anon.com>
2018-09-07 09:18:36 +0200

    Fix script

--- config/start.sh
+++ config/start.sh
@@ -1,4 +1,3 @@
-/bin/sh
+#/bin/sh
 wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/job-generator/ -P /app --reject="index.html*"
-pwd
 java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-job-generator.jar --spring.config.location=/app/application.yml
