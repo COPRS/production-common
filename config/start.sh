0a09ebf7a353f90b746108fe9921c8c2013a2c72
Anonymous <anon@anon.com>
2018-09-07 09:19:19 +0200

    Fix script

--- config/start.sh
+++ config/start.sh
@@ -1,3 +1,3 @@
-/bin/sh
+#/bin/sh
 wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/metadata-catalog/ -P /app --reject="index.html*"
 java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-metadata-catalog.jar --spring.config.location=/app/application.yml
