358c54bfab625ccf09615c858a8ea57b4db1f6d1
Anonymous <anon@anon.com>
2018-10-10 11:23:31 +0200

    Externalize configuration file in server: one per app

--- config/start.sh
+++ config/start.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/mqi-server/ -P /app --reject="index.html*"
+wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/$externalconf_dir/mqi-server/ -P /app --reject="index.html*"
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/application.yml
