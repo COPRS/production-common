e0a31d6d92d1f41226f7eb8b1460a32dcff1588d
Anonymous <anon@anon.com>
2018-10-15 09:46:05 +0200

    update dir for external conf

--- config/start_l0.sh
+++ config/start_l0.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/l0-wrapper/app/ -P /app --reject="index.html*"
+wget --cut-dirs=2 --no-parent -nH -r $externalconf_host/l0-wrapper/app/ -P /app --reject="index.html*"
 /var/tmp/conf.sh
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-wrapper.jar --spring.config.location=/app/application.yml
