4958eca4888c18e381adc57280d16603c8930bdf
Anonymous <anon@anon.com>
2018-10-23 13:18:23 +0200

    Set start and stop time in string format whereas Date

--- src/main/java/esa/s1pdgs/cpoc/common/utils/DateUtils.java
+++ src/main/java/esa/s1pdgs/cpoc/common/utils/DateUtils.java
@@ -3,12 +3,14 @@ package esa.s1pdgs.cpoc.common.utils;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import esa.s1pdgs.cpoc.common.errors.InternalErrorException;
 
 public class DateUtils {
-    
+
     /**
      * Convert a date in ISO format "yyyyMMdd'T'HHmmss"
      * 
@@ -16,7 +18,8 @@ public class DateUtils {
      * @return
      * @throws InternalErrorException
      */
-    public static Date convertDateIso(final String dateStr) throws InternalErrorException {
+    public static Date convertDateIso(final String dateStr)
+            throws InternalErrorException {
         return convertWithSimpleDateFormat(dateStr, "yyyyMMdd'T'HHmmss");
     }
 
@@ -28,13 +31,28 @@ public class DateUtils {
      * @return
      * @throws InternalErrorException
      */
-    public static Date convertWithSimpleDateFormat(final String dateStr, final String dateFormat)
-            throws InternalErrorException {
+    public static Date convertWithSimpleDateFormat(final String dateStr,
+            final String dateFormat) throws InternalErrorException {
         try {
             DateFormat format = new SimpleDateFormat(dateFormat);
             return format.parse(dateStr);
         } catch (ParseException pe) {
-            throw new InternalErrorException("Cannot convert date " + dateStr, pe);
+            throw new InternalErrorException("Cannot convert date " + dateStr,
+                    pe);
         }
+    }
+
+    /**
+     * Convert a date
+     * 
+     * @param dateStr
+     * @param inFormatter
+     * @param outFormatter
+     * @return
+     */
+    public static String convertToAnotherFormat(String dateStr,
+            DateTimeFormatter inFormatter, DateTimeFormatter outFormatter) {
+        LocalDateTime dateToConvert = LocalDateTime.parse(dateStr, inFormatter);
+        return dateToConvert.format(outFormatter);
     }
 }
--- src/test/java/esa/s1pdgs/cpoc/appcatalog/common/rest/model/job/AppDataJobProductDtoTest.java
+++ src/test/java/esa/s1pdgs/cpoc/appcatalog/common/rest/model/job/AppDataJobProductDtoTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Date;
 
 import org.junit.Test;
 
@@ -33,8 +34,8 @@ public class AppDataJobProductDtoTest {
         obj.setSatelliteId("B");
         obj.setMissionId("S1");
         obj.setAcquisition("EW");
-        obj.setStartTime(new Date());
-        obj.setStopTime(new Date());
+        obj.setStartTime("2018-12-12T20:25:12.123456Z");
+        obj.setStopTime("2018-12-12T20:25:12.654321Z");
         obj.setRaws1(Arrays.asList(new AppDataJobFileDto("message1"),
                 new AppDataJobFileDto("message2")));
         obj.setRaws2(Arrays.asList(new AppDataJobFileDto("gen1"),
@@ -60,6 +61,8 @@ public class AppDataJobProductDtoTest {
         assertEquals(125, obj.getNumberSlice());
         assertEquals(1425, obj.getTotalNbOfSlice());
         assertEquals("product-type", obj.getProductType());
+        assertEquals("2018-12-12T20:25:12.123456Z", obj.getStartTime());
+        assertEquals("2018-12-12T20:25:12.654321Z", obj.getStopTime());
         assertEquals("2018-12-12T21:25:12", obj.getSegmentStartDate());
         assertEquals("2018-12-12T21:28:12", obj.getSegmentStopDate());
 
@@ -83,6 +86,8 @@ public class AppDataJobProductDtoTest {
         assertTrue(str.contains("numberSlice: 125"));
         assertTrue(str.contains("totalNbOfSlice: 1425"));
         assertTrue(str.contains("productType: product-type"));
+        assertTrue(str.contains("startTime: 2018-12-12T20:25:12.123456Z"));
+        assertTrue(str.contains("stopTime: 2018-12-12T20:25:12.654321Z"));
         assertTrue(str.contains("segmentStartDate: 2018-12-12T21:25:12"));
         assertTrue(str.contains("segmentStopDate: 2018-12-12T21:28:12"));
     }
@@ -94,6 +99,12 @@ public class AppDataJobProductDtoTest {
     public void testEquals() {
         EqualsVerifier.forClass(AppDataJobProductDto.class).usingGetClass()
                 .suppress(Warning.NONFINAL_FIELDS).verify();
+    }
+    
+    @Test
+    public void testParseDate() {
+        LocalDateTime time = LocalDateTime.parse("2012-01-24T12:15:22.124578Z", AppDataJobProductDto.TIME_FORMATTER);
+        assertEquals("2012-01-24T12:15:22.124578Z", time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")));
     }
 
 }
