d387602a568a33f0617553f92e7797e295901510
Anonymous <anon@anon.com>
2019-05-22 14:11:56 +0200

    S1PRO-3: Adding obs property file for L2 slices and acn and changing OBS family consuming it

--- obs-sdk/src/main/java/esa/s1pdgs/cpoc/obs_sdk/ObsFamily.java
+++ obs-sdk/src/main/java/esa/s1pdgs/cpoc/obs_sdk/ObsFamily.java
@@ -6,5 +6,5 @@ package esa.s1pdgs.cpoc.obs_sdk;
  * @author Viveris Technologies
  */
 public enum ObsFamily {
-    AUXILIARY_FILE, EDRS_SESSION, L0_SLICE, L0_ACN, L1_SLICE, L1_ACN, L0_SEGMENT, L0_BLANK, UNKNOWN;
+    AUXILIARY_FILE, EDRS_SESSION, L0_SLICE, L0_ACN, L1_SLICE, L1_ACN, L0_SEGMENT, L0_BLANK, L2_SLICE, L2_ACN, UNKNOWN;
 }
