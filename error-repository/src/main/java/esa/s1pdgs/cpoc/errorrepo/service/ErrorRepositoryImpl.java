739a486f5d50c35fa14c69013b9e8577f4ec08f6
Anonymous <anon@anon.com>
2019-06-13 14:32:58 +0200

    S1PRO-52: making error-repo testable

--- error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/service/ErrorRepositoryImpl.java
+++ error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/service/ErrorRepositoryImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
 
 import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessing;
 
+@Component
 public class ErrorRepositoryImpl implements ErrorRepository {
 
 	private final MongoTemplate mongoTemplate;
