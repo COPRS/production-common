a0d5c08d3299e18b4dbf8594194857ab4e8c9708
Anonymous <anon@anon.com>
2019-06-13 16:41:31 +0200

    S1PRO-52: unittest for retrieval of failed processings

---
+++ error-repository/src/test/java/esa/s1pdgs/cpoc/errorrepo/service/ErrorRepositoryTest.java
@@ -0,0 +1,67 @@
+package esa.s1pdgs.cpoc.errorrepo.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessingDto;
+import esa.s1pdgs.cpoc.mqi.model.rest.LevelProductsMessageDto;
+
+public class ErrorRepositoryTest {
+
+	@Mock
+	private MongoTemplate mongoTemplate;
+
+	private ErrorRepository errorRepository;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		this.errorRepository = new ErrorRepositoryImpl(mongoTemplate);
+	}
+
+	@Test
+	public void getFailedProcessings() {
+
+		List<FailedProcessingDto<LevelProductsMessageDto>> failedProcessingsToReturn = new ArrayList<>();
+
+		LevelProductsMessageDto levelProductsMsgDto = new LevelProductsMessageDto();
+		FailedProcessingDto<LevelProductsMessageDto> fpDto = new FailedProcessingDto<>();
+		fpDto.setIdentifier(123);
+		fpDto.setDto(levelProductsMsgDto);
+		failedProcessingsToReturn.add(fpDto);
+
+		doReturn(failedProcessingsToReturn).when(mongoTemplate).findAll(FailedProcessingDto.class);
+
+		List<FailedProcessingDto> failedProcessings = errorRepository.getFailedProcessings();
+
+		assertEquals(123, failedProcessings.get(0).getIdentifier());
+		assertTrue(failedProcessings.get(0).getDto() instanceof LevelProductsMessageDto);
+	}
+
+	@Test
+	public void getFailedProcessingById() {
+
+		LevelProductsMessageDto levelProductsMsgDto = new LevelProductsMessageDto();
+		FailedProcessingDto<LevelProductsMessageDto> fpDtoToReturn = new FailedProcessingDto<>();
+		fpDtoToReturn.setIdentifier(123);
+		fpDtoToReturn.setDto(levelProductsMsgDto);
+
+		doReturn(fpDtoToReturn).when(mongoTemplate).findById("123", FailedProcessingDto.class);
+
+		FailedProcessingDto<LevelProductsMessageDto> failedProcessing = errorRepository.getFailedProcessingsById("123");
+
+		assertEquals(123, failedProcessing.getIdentifier());
+		assertTrue(failedProcessing.getDto() instanceof LevelProductsMessageDto);
+	}
+
+}
