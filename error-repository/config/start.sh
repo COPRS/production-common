7751434c8c15785673869db1bcd7912f8c8c0871
Anonymous <anon@anon.com>
2019-06-14 20:27:02 +0200

    script seems to have double app folder on download

--- error-repository/config/start.sh
+++ error-repository/config/start.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/error-repository/ -P /app --reject="index.html*"
+wget --cut-dirs=2 --no-parent -nH -r $externalconf_host/error-repository/app -P /app --reject="index.html*"
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-error-repository.jar --spring.config.location=/app/application.yml
