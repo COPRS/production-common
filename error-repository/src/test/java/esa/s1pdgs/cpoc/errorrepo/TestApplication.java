049687822865d395ca7caffa3fec51b961cada51
Anonymous <anon@anon.com>
2019-06-17 17:26:56 +0200

    trying to fix test issues

--- error-repository/src/test/java/esa/s1pdgs/cpoc/errorrepo/TestApplication.java
+++ error-repository/src/test/java/esa/s1pdgs/cpoc/errorrepo/TestApplication.java
@@ -3,12 +3,14 @@ package esa.s1pdgs.cpoc.errorrepo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DirtiesContext
+@EmbeddedKafka
 public class TestApplication {
 	
 	@Test
