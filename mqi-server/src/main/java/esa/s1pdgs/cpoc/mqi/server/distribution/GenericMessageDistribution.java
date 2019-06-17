29e39e8bc7c7e3dbec9d59a77c92f458778790e7
Anonymous <anon@anon.com>
2019-06-17 09:42:37 +0200

    * Fixing PMD

--- mqi-server/src/main/java/esa/s1pdgs/cpoc/mqi/server/distribution/GenericMessageDistribution.java
+++ mqi-server/src/main/java/esa/s1pdgs/cpoc/mqi/server/distribution/GenericMessageDistribution.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import esa.s1pdgs.cpoc.common.ProductCategory;
 import esa.s1pdgs.cpoc.common.ResumeDetails;
