22a996e55b62683204e9175dc94ae56e7ae44bc0
Anonymous <anon@anon.com>
2019-06-18 13:24:01 +0200

    S1PRO-7: Fixing compression script

--- compression/config/compression.sh
+++ compression/config/compression.sh
@@ -4,7 +4,8 @@ INPUT=$1
 OUTPUT=$2
 
 echo "Compressing ${INPUT} to ${OUTPUT}"
-7za a -tzip ${INPUT} ${OUTPUT} 
+7za a -tzip ${OUTPUT} ${INPUT}
 #cp -rv ${INPUT} ${OUTPUT}
 
+echo "Compression script done."
 exit 0
