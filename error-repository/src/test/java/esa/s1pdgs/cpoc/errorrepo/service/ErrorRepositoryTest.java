fb5e84a14e2c0920825b39151baedf94718ad256
Anonymous <anon@anon.com>
2019-06-18 10:14:11 +0200

    adds unit tests to ErrorRepositoryTest

--- error-repository/src/test/java/esa/s1pdgs/cpoc/errorrepo/service/ErrorRepositoryTest.java
+++ error-repository/src/test/java/esa/s1pdgs/cpoc/errorrepo/service/ErrorRepositoryTest.java
@@ -1,6 +1,7 @@
 package esa.s1pdgs.cpoc.errorrepo.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -13,6 +14,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import com.mongodb.client.result.DeleteResult;
+
+import esa.s1pdgs.cpoc.appcatalog.common.MqiMessage;
 import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessingDto;
 import esa.s1pdgs.cpoc.mqi.model.rest.LevelProductsMessageDto;
 
@@ -62,6 +66,57 @@ public class ErrorRepositoryTest {
 
 		assertEquals(123, failedProcessing.getIdentifier());
 		assertTrue(failedProcessing.getDto() instanceof LevelProductsMessageDto);
+	}
+	
+	@Test
+	public void deleteFailedProcessing( ) {
+		
+		LevelProductsMessageDto levelProductsMsgDto = new LevelProductsMessageDto();
+		FailedProcessingDto<LevelProductsMessageDto> fpDto = new FailedProcessingDto<>();
+		fpDto.setIdentifier(123);
+		fpDto.setDto(levelProductsMsgDto);
+		
+		doReturn(fpDto).when(mongoTemplate).findById("123", FailedProcessingDto.class);
+		DeleteResult deleteResult = new DeleteResult() {
+			
+			@Override
+			public boolean wasAcknowledged() {
+				return true;
+			}
+			
+			@Override
+			public long getDeletedCount() {
+				
+				return 1;
+			}
+		};
+		
+		doReturn(deleteResult).when(mongoTemplate).remove(fpDto);
+		
+		assertTrue(errorRepository.deleteFailedProcessing("123"));
+		assertFalse(errorRepository.deleteFailedProcessing("4"));
+	}
+	
+	@Test
+	public void saveFailedProcessing() {
+		
+		LevelProductsMessageDto levelProductsMsgDto = new LevelProductsMessageDto();
+		levelProductsMsgDto.setIdentifier(1);
+		FailedProcessingDto<LevelProductsMessageDto> fpDto = new FailedProcessingDto<>();
+		fpDto.setIdentifier(123);
+		fpDto.setDto(levelProductsMsgDto);
+		
+		MqiMessage mqiMsg = new MqiMessage();		
+				doReturn(mqiMsg).when(mongoTemplate).findOne(any(), eq(MqiMessage.class));
+
+		errorRepository.saveFailedProcessing(fpDto);
+		verify(mongoTemplate, times(1)).insert(fpDto);
+		
+	}
+	
+	@Test
+	public void restartAndDeleteFailedProcessing() {
+		//TODO
 	}
 
 }
