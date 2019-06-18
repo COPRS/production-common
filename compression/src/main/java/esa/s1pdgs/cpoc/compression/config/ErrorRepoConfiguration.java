1736a463e6a8ae05805a1a27a49dbf7574e4bae8
Anonymous <anon@anon.com>
2019-06-18 16:22:45 +0200

    Revert "set ADD_TYPE_INFO_HEADERS to true in errorRepoConfig"
    This reverts commit 48cd9b8d5d92e9e9fa8d7161b00794e7d27870cf.

--- compression/src/main/java/esa/s1pdgs/cpoc/compression/config/ErrorRepoConfiguration.java
+++ compression/src/main/java/esa/s1pdgs/cpoc/compression/config/ErrorRepoConfiguration.java
@@ -37,7 +37,7 @@ public class ErrorRepoConfiguration {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true);
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         props.put(ProducerConfig.RETRIES_CONFIG, maxRetries);        
         return new KafkaTemplate<String, FailedProcessingDto<?>>(new DefaultKafkaProducerFactory<>(props));
 	}
