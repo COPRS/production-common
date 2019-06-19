f2721997a168a7bd9f8914fb16feac9e9676eea8
Anonymous <anon@anon.com>
2019-06-19 16:09:07 +0200

    S1PRO-6 queue-watcher service

---
+++ queue-watcher/src/main/java/esa/s1pdgs/cpoc/queuewatcher/mqi/MqiConfiguration.java
@@ -0,0 +1,115 @@
+package esa.s1pdgs.cpoc.queuewatcher.mqi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import esa.s1pdgs.cpoc.mqi.client.AuxiliaryFilesMqiService;
+import esa.s1pdgs.cpoc.mqi.client.CompressedProductsMqiService;
+import esa.s1pdgs.cpoc.mqi.client.GenericMqiService;
+import esa.s1pdgs.cpoc.mqi.client.LevelProductsMqiService;
+import esa.s1pdgs.cpoc.mqi.client.LevelSegmentsMqiService;
+import esa.s1pdgs.cpoc.mqi.model.queue.AuxiliaryFileDto;
+import esa.s1pdgs.cpoc.mqi.model.queue.CompressionJobDto;
+import esa.s1pdgs.cpoc.mqi.model.queue.LevelProductDto;
+import esa.s1pdgs.cpoc.mqi.model.queue.LevelSegmentDto;
+
+/**
+ * Configuration of MQI client.<br/>
+ * Creation of 3 services: LEVEL_PRODUCTS, LEVEL_REPORTS, LEVEL_JOBS
+ * 
+ * @author Faisal Rafi
+ */
+@Configuration
+public class MqiConfiguration {
+
+    /**
+     * Host URI for the MQI server
+     */
+    private final String hostUri;
+
+    /**
+     * Maximal number of retries when query fails
+     */
+    private final int maxRetries;
+
+    /**
+     * Temporisation in ms between 2 retries
+     */
+    private final int tempoRetryMs;
+
+    /**
+     * Constructor
+     * 
+     * @param hostUri
+     * @param maxRetries
+     * @param tempoRetryMs
+     */
+    @Autowired
+    public MqiConfiguration(@Value("${file.mqi.host-uri}") final String hostUri,
+            @Value("${file.mqi.max-retries}") final int maxRetries,
+            @Value("${file.mqi.tempo-retry-ms}") final int tempoRetryMs) {
+        this.hostUri = hostUri;
+        this.maxRetries = maxRetries;
+        this.tempoRetryMs = tempoRetryMs;
+    }
+
+    /**
+     * Service for querying MQI for LEVEL_SEGMENT category
+     * 
+     * @param builder
+     * @return
+     */
+    @Bean(name = "mqiServiceForLevelSegments")
+    public GenericMqiService<LevelSegmentDto> mqiServiceForLevelSegments(
+            final RestTemplateBuilder builder) {
+        RestTemplate template = builder.build();
+        return new LevelSegmentsMqiService(template, hostUri, maxRetries,
+                tempoRetryMs);
+    }
+
+    /**
+     * Service for querying MQI for LEVEL_PRODUCT category
+     * 
+     * @param builder
+     * @return
+     */
+    @Bean(name = "mqiServiceForLevelProducts")
+    public GenericMqiService<LevelProductDto> mqiServiceForLevelProducts(
+            final RestTemplateBuilder builder) {
+        RestTemplate template = builder.build();
+        return new LevelProductsMqiService(template, hostUri, maxRetries,
+                tempoRetryMs);
+    }
+
+    /**
+     * Service for querying MQI for LEVEL_REPORTS category
+     * 
+     * @param builder
+     * @return
+     */
+    @Bean(name = "mqiServiceForAuxiliaryFiles")
+    public GenericMqiService<AuxiliaryFileDto> mqiServiceForAuxiliaryFiles(
+            final RestTemplateBuilder builder) {
+        RestTemplate template = builder.build();
+        return new AuxiliaryFilesMqiService(template, hostUri, maxRetries,
+                tempoRetryMs);
+    }
+
+    /**
+     * Service for querying MQI for COMPRESSED_PRODUCTS category
+     * 
+     * @param builder
+     * @return
+     */
+    @Bean(name = "mqiServiceForCompression")
+    public GenericMqiService<CompressionJobDto> mqiServiceForCompression(
+            final RestTemplateBuilder builder) {
+        RestTemplate template = builder.build();
+        return new CompressedProductsMqiService(template, hostUri, maxRetries,
+                tempoRetryMs);
+    }
+}
