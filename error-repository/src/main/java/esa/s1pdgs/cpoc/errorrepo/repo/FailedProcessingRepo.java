4fffdee4fccd3d79d5ef5c913c10670282beb1ff
Anonymous <anon@anon.com>
2019-06-20 20:25:04 +0200

    S1PRO-52, S1PRO-53, S1PRO-57: added @Service annotation to find repos in component scan

--- error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/repo/FailedProcessingRepo.java
+++ error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/repo/FailedProcessingRepo.java
@@ -3,9 +3,11 @@ package esa.s1pdgs.cpoc.errorrepo.repo;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
 
 import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessingDto;
 
+@Service
 public interface FailedProcessingRepo extends MongoRepository<FailedProcessingDto, Long>{
 
 	public FailedProcessingDto findByIdentifier(long identifier);
--- error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/repo/MqiMessageRepo.java
+++ error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/repo/MqiMessageRepo.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
 
 import esa.s1pdgs.cpoc.appcatalog.common.MqiMessage;
 
+@Service
 public interface MqiMessageRepo extends MongoRepository<MqiMessage, Long> {
 	
 	MqiMessage findByIdentifier(long identifier);
