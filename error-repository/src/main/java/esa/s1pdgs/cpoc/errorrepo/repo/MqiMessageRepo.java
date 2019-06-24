fbf7f9e9bdd7b18dbec03dc4cac7d64108a5bfd4
Anonymous <anon@anon.com>
2019-06-24 17:19:00 +0200

    deletes method findAll in MqiMessageRepo as it is not used

--- error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/repo/MqiMessageRepo.java
+++ error-repository/src/main/java/esa/s1pdgs/cpoc/errorrepo/repo/MqiMessageRepo.java
@@ -1,7 +1,5 @@
 package esa.s1pdgs.cpoc.errorrepo.repo;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +10,4 @@ public interface MqiMessageRepo extends MongoRepository<MqiMessage, Long> {
 	
 	MqiMessage findByIdentifier(long identifier);
 	
-	Page<MqiMessage> findAll(Pageable p);
 }
