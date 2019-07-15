1343d02ae696c43b8f2bea11c0c0eb96f28be970
Anonymous <anon@anon.com>
2019-07-15 19:39:50 +0200

    S1PRO-299: Created new submodule for new service inbox-polling

---
+++ inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/Application.java
@@ -0,0 +1,12 @@
+package esa.s1pdgs.cpoc.inbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
