ac5483ddd9ab424b0123e69aa5eedb16b43daf97
Anonymous <anon@anon.com>
2019-07-16 11:05:36 +0200

    * Adding debugging messages

--- scaler/src/main/java/esa/s1pdgs/cpoc/scaler/openstack/services/ServerService.java
+++ scaler/src/main/java/esa/s1pdgs/cpoc/scaler/openstack/services/ServerService.java
@@ -156,6 +156,11 @@ public class ServerService {
     }
 
     public Server get(OSClientV3 osClient, String serverId) {
+    	org.openstack4j.api.compute.ServerService service = osClient.compute().servers();
+    	for (Server server: service.list()) {
+    		LOGGER.debug("list server: {} {} {}",server.getName(), server.getHost(), server.getId(), server.getUuid());
+    	}
+    	
         return osClient.compute().servers().get(serverId);
     }
 
