ec9509ed084c70785d6eedb8b00d2a3c71bb3ce7
Anonymous <anon@anon.com>
2019-07-17 17:32:04 +0200

    S1PRO-300: Trying to get persistence working

--- inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/Application.java
+++ inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/Application.java
@@ -3,12 +3,16 @@ package esa.s1pdgs.cpoc.inbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import esa.s1pdgs.cpoc.inbox.config.InboxPollingConfigurationProperties;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
+@EnableJpaRepositories("esa.s1pdgs.cpoc.inbox.polling.repo")
 @EnableConfigurationProperties(InboxPollingConfigurationProperties.class)
 public class Application {
     public static void main(String[] args) {
