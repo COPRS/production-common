2f164851e9dcb01e9adbfcd199d28c56cec9ab66
Anonymous <anon@anon.com>
2019-07-19 15:59:34 +0200

    fixed test

--- lib-commons/src/test/java/esa/s1pdgs/cpoc/common/errors/AbstractCodedExceptionTest.java
+++ lib-commons/src/test/java/esa/s1pdgs/cpoc/common/errors/AbstractCodedExceptionTest.java
@@ -13,7 +13,7 @@ public class AbstractCodedExceptionTest {
 
     @Test
     public void testEnumErroCode() {
-        assertEquals(60, ErrorCode.values().length);
+        assertEquals(61, ErrorCode.values().length);
         
         assertEquals(ErrorCode.ES_CREATION_ERROR,
                 ErrorCode.valueOf("ES_CREATION_ERROR"));
