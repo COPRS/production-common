be6c40a983bb54330c8900b81c79ea67211aa1f6
Anonymous <anon@anon.com>
2019-07-24 11:38:46 +0200

    S1PRO-269: unit tests for ValidationService

--- validation/src/test/java/esa/s1pdgs/cpoc/validation/service/ValidationServiceTest.java
+++ validation/src/test/java/esa/s1pdgs/cpoc/validation/service/ValidationServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -238,9 +239,11 @@ public class ValidationServiceTest {
 
 		ObsObject ob1 = new ObsObject("product1", obsFamily);
 		ObsObject ob2 = new ObsObject("product2", obsFamily);
+		ObsObject ob3 = new ObsObject("product3", obsFamily);
 		Map<String, ObsObject> obsResults = new HashMap<>();
 		obsResults.put("product1", ob1);
 		obsResults.put("product2", ob2);
+		obsResults.put("product3", ob3);
 
 		doReturn(metadataResults).when(metadataService).query(family, null, intervalStart, intervalStop);
 		doReturn(obsResults).when(obsClient).listInterval(family, startDate, stopDate);
@@ -285,7 +288,8 @@ public class ValidationServiceTest {
 	}
 
 	@Test
-	public void testCheckConsistencyForFamilyAndTimeFrameWhenX() throws SdkClientException, MetadataQueryException {
+	public void testCheckConsistencyForFamilyAndTimeFrameWhenDateTimeParseException()
+			throws SdkClientException, MetadataQueryException {
 
 		ProductFamily family = ProductFamily.L0_SLICE;
 		ObsFamily obsFamily = ObsFamily.L0_SLICE;
@@ -308,7 +312,80 @@ public class ValidationServiceTest {
 			validationService.checkConsistencyForFamilyAndTimeFrame(family, intervalStart, intervalStop);
 			fail("Exception expected");
 		} catch (DateTimeParseException e) {
-			// OK
+			// expected
+		}
+	}
+
+	@Test
+	public void testCheckConsistencyForFamilyAndTimeFrameWhenMetadataQueryException()
+			throws MetadataQueryException, SdkClientException {
+
+		ProductFamily family = ProductFamily.L0_SLICE;
+		ObsFamily obsFamily = ObsFamily.L0_SLICE;
+
+		String intervalStart = "2000-01-01T00:00:00.000000Z";
+		String intervalStop = "2000-01-03T00:00:00.000000Z";
+
+		LocalDateTime localDateTimeStart = LocalDateTime.parse(intervalStart, DateUtils.METADATA_DATE_FORMATTER);
+		LocalDateTime localDateTimeStop = LocalDateTime.parse(intervalStop, DateUtils.METADATA_DATE_FORMATTER);
+
+		Date startDate = Date.from(localDateTimeStart.atZone(ZoneId.of("UTC")).toInstant());
+		Date stopDate = Date.from(localDateTimeStop.atZone(ZoneId.of("UTC")).toInstant());
+
+		SearchMetadata md1 = new SearchMetadata();
+		md1.setKeyObjectStorage("product1");
+		List<SearchMetadata> metadataResults = new ArrayList<>();
+		metadataResults.add(md1);
+
+		ObsObject ob1 = new ObsObject("product1", obsFamily);
+		Map<String, ObsObject> obsResults = new HashMap<>();
+		obsResults.put("product1", ob1);
+
+		doThrow(new MetadataQueryException("test generated exception")).when(metadataService).query(family, null,
+				intervalStart, intervalStop);
+		doReturn(obsResults).when(obsClient).listInterval(family, startDate, stopDate);
+
+		try {
+			validationService.checkConsistencyForFamilyAndTimeFrame(family, intervalStart, intervalStop);
+			fail("Exception expected");
+		} catch (MetadataQueryException e) {
+			// expected
+		}
+	}
+
+	@Test
+	public void testCheckConsistencyForFamilyAndTimeFrameWhenSdkClientException()
+			throws SdkClientException, MetadataQueryException {
+
+		ProductFamily family = ProductFamily.L0_SLICE;
+		ObsFamily obsFamily = ObsFamily.L0_SLICE;
+
+		String intervalStart = "2000-01-01T00:00:00.000000Z";
+		String intervalStop = "2000-01-03T00:00:00.000000Z";
+
+		LocalDateTime localDateTimeStart = LocalDateTime.parse(intervalStart, DateUtils.METADATA_DATE_FORMATTER);
+		LocalDateTime localDateTimeStop = LocalDateTime.parse(intervalStop, DateUtils.METADATA_DATE_FORMATTER);
+
+		Date startDate = Date.from(localDateTimeStart.atZone(ZoneId.of("UTC")).toInstant());
+		Date stopDate = Date.from(localDateTimeStop.atZone(ZoneId.of("UTC")).toInstant());
+
+		SearchMetadata md1 = new SearchMetadata();
+		md1.setKeyObjectStorage("product1");
+		List<SearchMetadata> metadataResults = new ArrayList<>();
+		metadataResults.add(md1);
+
+		ObsObject ob1 = new ObsObject("product1", obsFamily);
+		Map<String, ObsObject> obsResults = new HashMap<>();
+		obsResults.put("product1", ob1);
+
+		doReturn(metadataResults).when(metadataService).query(family, null, intervalStart, intervalStop);
+		doThrow(new SdkClientException("test generated exception")).when(obsClient).listInterval(family, startDate, stopDate);
+
+		try {
+			validationService.checkConsistencyForFamilyAndTimeFrame(family, intervalStart, intervalStop);
+			fail("Exception expected");
+		} catch (SdkClientException e) {
+			// expected
 		}
 	}
 
