735910d7eae527c47391b9caf77810f9a28e7564
Anonymous <anon@anon.com>
2019-07-25 14:31:00 +0200

    added Bean annotation

--- inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/config/SchedulingConfiguration.java
+++ inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/config/SchedulingConfiguration.java
@@ -2,13 +2,14 @@ package esa.s1pdgs.cpoc.inbox.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import esa.s1pdgs.cpoc.inbox.InboxPollingService;
 import esa.s1pdgs.cpoc.inbox.PollingTrigger;
 
-//make scheduling configurable via property to allow disabling it in unit tests
+// make scheduling configurable via property to allow disabling it in unit tests
 @ConditionalOnProperty(value = "scheduling.enable", havingValue = "true", matchIfMissing = true)
 @EnableScheduling
 @Configuration
@@ -20,6 +21,7 @@ public class SchedulingConfiguration {
 		this.pollingService = pollingService;
 	}
 
+	@Bean
 	public PollingTrigger pollingTrigger()
 	{
 		return new PollingTrigger(pollingService);
