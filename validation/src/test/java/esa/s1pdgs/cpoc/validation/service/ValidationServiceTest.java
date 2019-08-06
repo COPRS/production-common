2e8e3755a8f7913ca0f4ea8eac703576c363d760
Anonymous <anon@anon.com>
2019-08-06 12:55:50 +0200

    * Attempt to make validation service useful again

--- validation/src/test/java/esa/s1pdgs/cpoc/validation/service/ValidationServiceTest.java
+++ validation/src/test/java/esa/s1pdgs/cpoc/validation/service/ValidationServiceTest.java
@@ -268,4 +268,128 @@ public class ValidationServiceTest {
 		int discrepancies = validationService.checkConsistencyForInterval(localDateTimeStart, localDateTimeStop);
 		assertEquals(2, discrepancies);
 	}
+	
+	@Test
+	public void testCheckConsistencyForAuxScenario()
+			throws SdkClientException, MetadataQueryException {
+
+		ProductFamily family = ProductFamily.AUXILIARY_FILE;
+
+		String intervalStart = "2000-01-01T00:00:00.000000Z";
+		String intervalStop = "2000-01-03T00:00:00.000000Z";
+
+		LocalDateTime localDateTimeStart = LocalDateTime.parse(intervalStart, DateUtils.METADATA_DATE_FORMATTER);
+		LocalDateTime localDateTimeStop = LocalDateTime.parse(intervalStop, DateUtils.METADATA_DATE_FORMATTER);
+
+		Date startDate = Date.from(localDateTimeStart.atZone(ZoneId.of("UTC")).toInstant());
+		Date stopDate = Date.from(localDateTimeStop.atZone(ZoneId.of("UTC")).toInstant());
+
+		SearchMetadata md1 = new SearchMetadata();
+		md1.setKeyObjectStorage("AUX_PP1_PRODUCT1");
+		SearchMetadata md2 = new SearchMetadata();
+		md2.setKeyObjectStorage("MPL_SF_PRODUCT2");
+		SearchMetadata md3 = new SearchMetadata();
+		md3.setKeyObjectStorage("AUX_MPL_PRODUCT3");
+
+		List<SearchMetadata> metadataResults = new ArrayList<>();
+		metadataResults.add(md1);
+		metadataResults.add(md2);
+		metadataResults.add(md3);
+
+		ObsObject ob1 = new ObsObject("AUX_PP1_PRODUCT1", family);
+		ObsObject ob2 = new ObsObject("MPL_SF_PRODUCT2", family);
+		
+		Map<String, ObsObject> obsResults = new HashMap<>();
+		obsResults.put("AUX_PP1/data.xsd", ob1);
+		obsResults.put("AUX_PP1/other.xsd", ob1);
+		
+		obsResults.put("MPL_SF/other.xsd", ob2);
+
+		doReturn(metadataResults).when(metadataService).query(family, localDateTimeStart, localDateTimeStop);
+		doReturn(obsResults).when(obsClient).listInterval(family, startDate, stopDate);
+
+		int discrepancies = validationService.checkConsistencyForInterval(localDateTimeStart, localDateTimeStop);
+		assertEquals(1, discrepancies);
+	}
+	
+	@Test
+	public void testCheckConsistencyForSessionScenario()
+			throws SdkClientException, MetadataQueryException {
+
+		ProductFamily family = ProductFamily.EDRS_SESSION;
+
+		String intervalStart = "2000-01-01T00:00:00.000000Z";
+		String intervalStop = "2000-01-03T00:00:00.000000Z";
+
+		LocalDateTime localDateTimeStart = LocalDateTime.parse(intervalStart, DateUtils.METADATA_DATE_FORMATTER);
+		LocalDateTime localDateTimeStop = LocalDateTime.parse(intervalStop, DateUtils.METADATA_DATE_FORMATTER);
+
+		Date startDate = Date.from(localDateTimeStart.atZone(ZoneId.of("UTC")).toInstant());
+		Date stopDate = Date.from(localDateTimeStop.atZone(ZoneId.of("UTC")).toInstant());
+
+		SearchMetadata md1 = new SearchMetadata();
+		md1.setKeyObjectStorage("S1A/L231232132/ch02/DCS_L231232132_ch2_DSDB_01921.raw");
+		SearchMetadata md2 = new SearchMetadata();
+		md2.setKeyObjectStorage("S1B/L231232132/ch01/DCS_L231232132_ch2_DSDB_01922.raw");
+		SearchMetadata md3 = new SearchMetadata();
+		md3.setKeyObjectStorage("S1C/notvalid");
+
+		List<SearchMetadata> metadataResults = new ArrayList<>();
+		metadataResults.add(md1);
+		metadataResults.add(md2);
+		metadataResults.add(md3);
+
+		ObsObject ob1 = new ObsObject("S1A/L231232132/ch02/DCS_L231232132_ch2_DSDB_01921.raw", family);
+		ObsObject ob2 = new ObsObject("S1B/L231232132/ch01/DCS_L231232132_ch2_DSDB_01922.raw", family);
+		
+		Map<String, ObsObject> obsResults = new HashMap<>();
+		obsResults.put("S1A/L231232132/ch02/DCS_L231232132_ch2_DSDB_01921.raw", ob1);
+		obsResults.put("S1B/L231232132/ch01/DCS_L231232132_ch2_DSDB_01922.raw", ob2);
+		
+
+		doReturn(metadataResults).when(metadataService).query(family, localDateTimeStart, localDateTimeStop);
+		doReturn(obsResults).when(obsClient).listInterval(family, startDate, stopDate);
+
+		int discrepancies = validationService.checkConsistencyForInterval(localDateTimeStart, localDateTimeStop);
+		assertEquals(1, discrepancies);
+	}
+	
+	@Test
+	public void testCheckConsistencyForSliceScenario()
+			throws SdkClientException, MetadataQueryException {
+
+		ProductFamily family = ProductFamily.L0_SLICE;
+
+		String intervalStart = "2000-01-01T00:00:00.000000Z";
+		String intervalStop = "2000-01-03T00:00:00.000000Z";
+
+		LocalDateTime localDateTimeStart = LocalDateTime.parse(intervalStart, DateUtils.METADATA_DATE_FORMATTER);
+		LocalDateTime localDateTimeStop = LocalDateTime.parse(intervalStop, DateUtils.METADATA_DATE_FORMATTER);
+
+		Date startDate = Date.from(localDateTimeStart.atZone(ZoneId.of("UTC")).toInstant());
+		Date stopDate = Date.from(localDateTimeStop.atZone(ZoneId.of("UTC")).toInstant());
+
+		SearchMetadata md1 = new SearchMetadata();
+		md1.setKeyObjectStorage("S1B_IW_RAW__0SDV_20181001T143515_20181001T143547_012960_017EFD_5A8C.SAFE");
+		SearchMetadata md2 = new SearchMetadata();
+		md2.setKeyObjectStorage("S1A_IW_RAW__0SDV_20181001T143515_20181001T143547_012960_017EFD_5A8C.SAFE");
+
+		List<SearchMetadata> metadataResults = new ArrayList<>();
+		metadataResults.add(md1);
+		metadataResults.add(md2);
+		
+		ObsObject ob1 = new ObsObject("S1B_IW_RAW__0SDV_20181001T143515_20181001T143547_012960_017EFD_5A8C.SAFE/manifest.safe", family);
+		ObsObject ob2 = new ObsObject("S1B_IW_RAW__0SDV_20181001T143515_20181001T143547_012960_017EFD_5A8C.SAFE/s1b-ew-raw-s-hh-20181001t142523-20181001t142631-012960-017efc-index.dat", family);
+		
+		Map<String, ObsObject> obsResults = new HashMap<>();
+		obsResults.put("S1B_IW_RAW__0SDV_20181001T143515_20181001T143547_012960_017EFD_5A8C.SAFE/manifest.safe", ob1);
+		obsResults.put("S1B_IW_RAW__0SDV_20181001T143515_20181001T143547_012960_017EFD_5A8C.SAFE/s1b-ew-raw-s-hh-20181001t142523-20181001t142631-012960-017efc-index.dat", ob2);
+		
+
+		doReturn(metadataResults).when(metadataService).query(family, localDateTimeStart, localDateTimeStop);
+		doReturn(obsResults).when(obsClient).listInterval(family, startDate, stopDate);
+
+		int discrepancies = validationService.checkConsistencyForInterval(localDateTimeStart, localDateTimeStop);
+		assertEquals(1, discrepancies);
+	}
 }
