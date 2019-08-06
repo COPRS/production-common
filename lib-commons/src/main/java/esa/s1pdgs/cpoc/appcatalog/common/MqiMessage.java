20dff1e252f627c41feaa4b1c15053acae3cfdf2
Anonymous <anon@anon.com>
2019-08-06 16:07:47 +0200

    trying to fix @Id problem

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/appcatalog/common/MqiMessage.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/appcatalog/common/MqiMessage.java
@@ -53,6 +53,14 @@ public class MqiMessage extends AbstractRequest {
         this.lastReadDate = lastReadDate;
     }
     
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	/**
 	 * @return the identifier
 	 */
