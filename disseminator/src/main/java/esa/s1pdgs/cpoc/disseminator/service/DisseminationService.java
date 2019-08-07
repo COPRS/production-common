f1557dda5acf733852dd7844d3884b5e93658f97
Anonymous <anon@anon.com>
2019-08-07 20:14:02 +0200

    S1PRO-423: Implemented local transfer

--- disseminator/src/main/java/esa/s1pdgs/cpoc/disseminator/service/DisseminationService.java
+++ disseminator/src/main/java/esa/s1pdgs/cpoc/disseminator/service/DisseminationService.java
@@ -18,6 +18,8 @@ import esa.s1pdgs.cpoc.common.errors.obs.ObsException;
 import esa.s1pdgs.cpoc.common.utils.LogUtils;
 import esa.s1pdgs.cpoc.disseminator.config.DisseminationProperties;
 import esa.s1pdgs.cpoc.disseminator.config.DisseminationProperties.DisseminationTypeConfiguration;
+import esa.s1pdgs.cpoc.disseminator.config.DisseminationProperties.OutboxConfiguration;
+import esa.s1pdgs.cpoc.disseminator.config.DisseminationProperties.OutboxConfiguration.Protocol;
 import esa.s1pdgs.cpoc.mqi.MqiConsumer;
 import esa.s1pdgs.cpoc.mqi.client.GenericMqiClient;
 import esa.s1pdgs.cpoc.mqi.model.queue.ProductDto;
@@ -67,20 +69,32 @@ public class DisseminationService {
     	}
     }
     
-	public void transferTo(final ProductFamily family, String keyObjectStorage, String target) {		
+	public final void transferTo(final ProductFamily family, String keyObjectStorage, String target) {		
 		try {
 			if (!obsClient.exist(family, keyObjectStorage)) {
 				throw new RuntimeException(String.format("OBS file %s (%s) does not exist", keyObjectStorage, family));
 			}
-			// TODO
+			final OutboxConfiguration config = getOutboxConfigurationFor(target);	
+			if (config.getProtocol() == Protocol.FILE) {				
+				obsClient.downloadFile(family, keyObjectStorage, config.getPath());
+			} else {
+				throw new UnsupportedOperationException(String.format("Protocol %s not implemented", config.getProtocol()));
+			}		
 		} catch (ObsException e) {
 			throw new RuntimeException(
 					String.format("Error downloading file %s (%s) from OBS: %s", keyObjectStorage, family, LogUtils.toString(e))
 			);
-		}
-		
+		}		
 	}
-    
-    
+	
+	final OutboxConfiguration getOutboxConfigurationFor(final String target) {
+		final OutboxConfiguration result = properties.getOutboxes().get(target);
+		if (result == null) {
+			throw new RuntimeException(
+					String.format("Missing outbox configuration %s. Available are: %s", target , properties.getOutboxes().keySet())
+			);
+		}
+		return result;
+	}
 }
 
