e93feb523fa522386052164f6e3f39e9a69ce6aa
Anonymous <anon@anon.com>
2019-08-08 19:56:38 +0200

    S1PRO-424: added new OBS API call for remote transfer

--- disseminator/src/main/java/esa/s1pdgs/cpoc/disseminator/DisseminationService.java
+++ disseminator/src/main/java/esa/s1pdgs/cpoc/disseminator/DisseminationService.java
@@ -1,8 +1,11 @@
 package esa.s1pdgs.cpoc.disseminator;
 
+import java.io.InputStream;
 import java.util.Date;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 import esa.s1pdgs.cpoc.common.ProductCategory;
 import esa.s1pdgs.cpoc.common.ProductFamily;
 import esa.s1pdgs.cpoc.common.errors.obs.ObsException;
+import esa.s1pdgs.cpoc.common.errors.obs.ObsUnknownObject;
 import esa.s1pdgs.cpoc.common.utils.LogUtils;
 import esa.s1pdgs.cpoc.disseminator.config.DisseminationProperties;
 import esa.s1pdgs.cpoc.disseminator.config.DisseminationProperties.DisseminationTypeConfiguration;
@@ -27,12 +31,15 @@ import esa.s1pdgs.cpoc.mqi.MqiConsumer;
 import esa.s1pdgs.cpoc.mqi.client.GenericMqiClient;
 import esa.s1pdgs.cpoc.mqi.model.queue.ProductDto;
 import esa.s1pdgs.cpoc.obs_sdk.ObsClient;
+import esa.s1pdgs.cpoc.obs_sdk.SdkClientException;
 import esa.s1pdgs.cpoc.report.LoggerReporting;
 import esa.s1pdgs.cpoc.report.Reporting;
 
 @Service
-public class DisseminationService {
+public class DisseminationService {	
 	private static final Logger LOG = LogManager.getLogger(DisseminationService.class);
+	
+	private static Set<Protocol> REMOTE_PROTOCOLS = EnumSet.of(Protocol.FTPS, Protocol.SFTP);
 	
 	private final GenericMqiClient client;
     private final ObsClient obsClient;
@@ -76,15 +83,20 @@ public class DisseminationService {
     							try {
 									transferTo(product.getFamily(), product.getKeyObjectStorage(), config.getTarget(), rf);								
     							} catch (Exception e) {
-    								final String message = (e instanceof DisseminationException) ? e.getMessage() : LogUtils.toString(e);    	
-									reporting.reportError("Error on dissemination of product to outbox {}: {}", config.getTarget(), message);									
+    								final String errMessage = (e instanceof DisseminationException) ? e.getMessage() : LogUtils.toString(e); 
+    								final String message = String.format(
+    										"Error on dissemination of product to outbox %s: %s", 
+    										config.getTarget(), 
+    										errMessage
+    								);
+									reporting.reportError(message);									
 									errorAppender.send(new FailedProcessingDto(
 											properties.getHostname(), 
 											new Date(), 
 											message, 
 											dto
 									));									
-									throw new RuntimeException(e);
+									throw new RuntimeException(message, e);
 								} 
     							reporting.reportStop("End dissemination of product to outbox " + config.getTarget());    							
     						}			
@@ -100,23 +112,42 @@ public class DisseminationService {
 		try {
 			if (!obsClient.exist(family, keyObjectStorage)) {
 				throw new RuntimeException(String.format("OBS file %s (%s) does not exist", keyObjectStorage, family));
-			}
-			final OutboxConfiguration config = getOutboxConfigurationFor(target);
+			}			
 			final Reporting reporting = reportingFactory.newReporting(1);
-			
-			if (config.getProtocol() == Protocol.FILE) {		
-				reporting.reportStart("Start downloading file from OBS " + keyObjectStorage + " to " + config.getPath());
-				obsClient.downloadFile(family, keyObjectStorage, config.getPath());
-				reporting.reportStop("End downloading file from OBS " + keyObjectStorage + " to " + config.getPath());
-			} else {
-				throw new UnsupportedOperationException(String.format("Protocol %s not implemented", config.getProtocol()));
-			}		
-		} catch (ObsException e) {
-			throw new RuntimeException(
+			reporting.reportStart("Start downloading file from OBS " + keyObjectStorage + " to " + target);
+			try {					
+				transferToOutbox(family, keyObjectStorage, getOutboxConfigurationFor(target));	
+				reporting.reportStop("End downloading file from OBS " + keyObjectStorage + " to " + target);
+			} catch (SdkClientException | ObsException e) {
+				reporting.reportError("Error downloading file from OBS {} to {}: {} ", keyObjectStorage, target, LogUtils.toString(e));
+				throw e;
+			}	
+		} catch (SdkClientException | ObsException e) {
+			throw new DisseminationException(
 					String.format("Error downloading file %s (%s) from OBS: %s", keyObjectStorage, family, LogUtils.toString(e))
 			);
 		}		
 	}
+	
+	final void transferToOutbox(final ProductFamily family, final String keyObjectStorage, final OutboxConfiguration config)
+			throws ObsException, ObsUnknownObject, SdkClientException {
+		if (config.getProtocol() == Protocol.FILE) {
+			obsClient.downloadFile(family, keyObjectStorage, config.getPath());
+		} else if (REMOTE_PROTOCOLS.contains(config.getProtocol())) {				
+			transferToRemoteOutbox(
+					obsClient.getAllAsInputStream(family, keyObjectStorage), 
+					keyObjectStorage, 
+					config
+			);
+		} else {
+			throw new UnsupportedOperationException(String.format("Protocol %s not implemented", config.getProtocol()));
+		}
+	}
+	
+	final void transferToRemoteOutbox(final Map<String, InputStream> obsContent, final String keyObjectStorage, final OutboxConfiguration config) {
+		// TODO
+	}
+
 	
 	final OutboxConfiguration getOutboxConfigurationFor(final String target) {
 		final OutboxConfiguration result = properties.getOutboxes().get(target);
