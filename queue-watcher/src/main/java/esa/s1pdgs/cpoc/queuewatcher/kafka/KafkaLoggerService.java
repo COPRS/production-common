be434d00eb5cb8ae776bbea6017a41c162cdf5fb
Anonymous <anon@anon.com>
2019-09-09 15:23:18 +0200

    * test kafka queue watcher

--- queue-watcher/src/main/java/esa/s1pdgs/cpoc/queuewatcher/kafka/KafkaLoggerService.java
+++ queue-watcher/src/main/java/esa/s1pdgs/cpoc/queuewatcher/kafka/KafkaLoggerService.java
@@ -1,49 +1,66 @@
 package esa.s1pdgs.cpoc.queuewatcher.kafka;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Service;
 
+import esa.s1pdgs.cpoc.queuewatcher.config.ApplicationProperties;
+
 @Service
 public class KafkaLoggerService {
 	private static final Logger LOGGER = LogManager.getLogger(KafkaLoggerService.class);
-	
+
 	private ConsumerFactory<String, String> factory;
-	private ConcurrentMessageListenerContainer container;
- 
-    @Autowired
-    public KafkaLoggerService(final ConsumerFactory<String, String> factory) {
-    	this.factory = factory;
-    }
-    
+	private ApplicationProperties properties;
+
+	@Autowired
+	public KafkaLoggerService(ApplicationProperties properties, final ConsumerFactory<String, String> factory) {
+		this.properties = properties;
+		this.factory = factory;
+	}
+
 	@PostConstruct
 	public void init() {
 		LOGGER.info("Starting kafka logger service...");
-		
-		ContainerProperties containerProperties = new ContainerProperties("t-pdgs-edrs-sessions");
-		containerProperties.setMessageListener((MessageListener<String, String>) record -> {
-		     //do something with received record
-			 System.out.println("Received Messasge in group foo: " + record.key()+" => "+record.value());
-		});
-		
-		container =
-		        new ConcurrentMessageListenerContainer<>(
-		                factory,
-		                containerProperties);
 
-		container.start();
-	}	
+		for (String topic : properties.getKafkaTopics()) {
+			LOGGER.info("Subscribing to kafka topic {}", topic);
+			ContainerProperties containerProperties = new ContainerProperties(topic);
+			containerProperties.setMessageListener((MessageListener<String, String>) record -> {
+				// do something with received record
+				LOGGER.debug("Received message from topic {}: {} ",topic, record.value());
+
+				String fileName = properties.getKafkaFolder() + "/kafka-" + topic;
+				FileWriter writer = null;
+				try {
+					writer = new FileWriter(new File(fileName));
+					writer.append(record.value());
+				} catch (IOException ex) {
+					LOGGER.error("An IO error occured while accessing {}", fileName);
+				} finally {
+					try {
+						writer.close();
+					} catch (IOException e) {
+						LOGGER.error("An IO error occured while closing {}", fileName);
+					}
+				}
+
+				ConcurrentMessageListenerContainer<String, String> container = new ConcurrentMessageListenerContainer<>(factory,
+						containerProperties);
+				container.start();
+			});
+
+		}
+	}
 }
