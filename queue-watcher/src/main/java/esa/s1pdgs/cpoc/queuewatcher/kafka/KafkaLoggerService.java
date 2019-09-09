b00be78688486377284c7027b14c944f45dd5cda
Anonymous <anon@anon.com>
2019-09-09 13:58:57 +0200

    * test kafka queue watcher

--- queue-watcher/src/main/java/esa/s1pdgs/cpoc/queuewatcher/kafka/KafkaLoggerService.java
+++ queue-watcher/src/main/java/esa/s1pdgs/cpoc/queuewatcher/kafka/KafkaLoggerService.java
@@ -1,11 +1,21 @@
 package esa.s1pdgs.cpoc.queuewatcher.kafka;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaLoggerService {
-	@KafkaListener(topics = "t-pdgs-compressed-products,t-pdgs-edrs-sessions", groupId = "foo")
+	private static final Logger LOGGER = LogManager.getLogger(KafkaLoggerService.class);
+	
+	@PostConstruct
+	public void init() {
+		LOGGER.info("Starting kafka logger service...");
+	}
+	@KafkaListener(topics = "t-pdgs-edrs-sessions", groupId = "foo")
 	public void listen(String message) {
 	    System.out.println("Received Messasge in group foo: " + message);
 	}
