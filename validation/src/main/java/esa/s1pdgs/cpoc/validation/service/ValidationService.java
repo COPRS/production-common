67778cabd464a4c5ca35e85335be8ea5acdf6bdd
Anonymous <anon@anon.com>
2019-09-11 17:36:50 +0200

    LS: added stacktrace dump to log in case of validation exceptions

--- validation/src/main/java/esa/s1pdgs/cpoc/validation/service/ValidationService.java
+++ validation/src/main/java/esa/s1pdgs/cpoc/validation/service/ValidationService.java
@@ -16,6 +16,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import esa.s1pdgs.cpoc.common.EdrsSessionFileType;
 import esa.s1pdgs.cpoc.common.ProductFamily;
 import esa.s1pdgs.cpoc.common.errors.processing.MetadataQueryException;
 import esa.s1pdgs.cpoc.common.utils.LogUtils;
@@ -144,10 +145,11 @@ public class ValidationService {
 					String key = smd.getKeyObjectStorage();					
 					if (family.name().endsWith("_ZIP")) {
 						key += ".zip";
-					}
+					} 
 					obsClient.validate(new ObsObject(family, key));
 				} catch (ObsServiceException | ObsValidationException ex) {
 					// Validation failed for that object.
+					LOGGER.debug(ex);
 					discrepancies.add(new Discrepancy(smd.getKeyObjectStorage(), ex.getMessage()));
 				}
 			}
@@ -156,7 +158,7 @@ public class ValidationService {
 			 * Step 3: After we know that the catalog data is valid within the OBS, we check if there are additional
 			 * products stored within that are not expects.
 			 */
-			Set<String> realKeys = extractRealKeys(obsResults.values());
+			Set<String> realKeys = extractRealKeys(obsResults.values(), family);
 			for (SearchMetadata smd : metadataResults) {
 				realKeys.remove(smd.getKeyObjectStorage());
 			}
@@ -189,12 +191,16 @@ public class ValidationService {
 		return 0;
 	}
 	
-	Set<String> extractRealKeys(Collection<ObsObject> obsResults) {
+	Set<String> extractRealKeys(Collection<ObsObject> obsResults, ProductFamily family) {
 		Set<String> realProducts = new HashSet<>();
 		for (ObsObject obsResult: obsResults) {
 			String key = obsResult.getKey();
 			int index = key.indexOf("/");
 			String realKey = null;
+//			
+//			if (family == ProductFamily.EDRS_SESSION) {
+//				realKey = key.substring(key.lastIndexOf('/'));
+//			}			
 			if (index != -1) {
 				realKey = key.substring(0, index);
 			} else {
