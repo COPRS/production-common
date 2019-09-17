ac1dc4d1c2d66a88f95a37f5868b4a5fd50d0931
Anonymous <anon@anon.com>
2019-09-17 13:17:00 +0200

    S1PRO-573: Removed duplication

--- metadata-catalog/src/main/java/esa/s1pdgs/cpoc/mdcatalog/rest/L0SliceMetadataController.java
+++ metadata-catalog/src/main/java/esa/s1pdgs/cpoc/mdcatalog/rest/L0SliceMetadataController.java
@@ -1,12 +1,11 @@
 package esa.s1pdgs.cpoc.mdcatalog.rest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,148 +15,79 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import esa.s1pdgs.cpoc.common.ProductFamily;
-import esa.s1pdgs.cpoc.common.errors.AbstractCodedException;
-import esa.s1pdgs.cpoc.common.errors.AbstractCodedException.ErrorCode;
 import esa.s1pdgs.cpoc.common.errors.processing.MetadataNotPresentException;
-import esa.s1pdgs.cpoc.common.utils.LogUtils;
 import esa.s1pdgs.cpoc.mdcatalog.es.EsServices;
 import esa.s1pdgs.cpoc.metadata.model.L0AcnMetadata;
 import esa.s1pdgs.cpoc.metadata.model.L0SliceMetadata;
 
 @RestController
 @RequestMapping(path = "/l0Slice")
-public class L0SliceMetadataController {
-
-	private static final Logger LOGGER = LogManager.getLogger(L0SliceMetadataController.class);
-
-	private final EsServices esServices;
-
+public class L0SliceMetadataController extends AbstractMetadataController<L0SliceMetadata> {
+	
 	@Autowired
-	public L0SliceMetadataController(final EsServices esServices) {
-		this.esServices = esServices;
+    public L0SliceMetadataController(final EsServices esServices) {
+		super(L0SliceMetadata.class, esServices);
 	}
 	
-    
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, path = "/{family}/{productName:.+}/seaCoverage")
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, path = "/{family}/{productName:.+}/seaCoverage")
     public ResponseEntity<Integer> getSeaCoverage(
             @PathVariable(name = "family") ProductFamily family,
-            @PathVariable(name = "productName") String productName) {
-        try {
-			return new ResponseEntity<>(esServices.getSeaCoverage(family, productName), HttpStatus.OK);
-        } catch (MetadataNotPresentException em) {
-            LOGGER.warn("[{}] [productName {}] [code {}] {}", family, productName, 
-            		em.getCode().getCode(), em.getLogMessage());            
-            return new ResponseEntity<Integer>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            LOGGER.error("[{}] [productName {}] [code {}] [msg {}]", family, productName, 
-            		ErrorCode.INTERNAL_ERROR.getCode(), LogUtils.toString(e));
-            return new ResponseEntity<Integer>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            @PathVariable(name = "productName") String productName) {		
+		return getResponse(productName, family, () -> esServices.getSeaCoverage(family, productName));
     }
 
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, path = "/{productName:.+}")
 	public ResponseEntity<L0SliceMetadata> get(@PathVariable(name = "productName") String productName) {
-		try {
-			final L0SliceMetadata response = esServices.getL0Slice(productName);
-			return new ResponseEntity<L0SliceMetadata>(response, HttpStatus.OK);
-			
-		} catch (MetadataNotPresentException em) {
-			LOGGER.warn("[L0_SLICE] [productName {}] [code {}] {}", productName,
-					em.getCode().getCode(), em.getLogMessage());
-			return new ResponseEntity<L0SliceMetadata>(HttpStatus.NOT_FOUND);
-		} catch (AbstractCodedException ace) {
-			LOGGER.error("[L0_SLICE] [productName {}] [code {}] {}", productName,
-					ace.getCode().getCode(), ace.getLogMessage());
-			return new ResponseEntity<L0SliceMetadata>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (Exception e) {
-			LOGGER.error("[L0_SLICE] [productName {}] [code {}] [msg {}]", productName,
-					ErrorCode.INTERNAL_ERROR.getCode(),   LogUtils.toString(e));
-			return new ResponseEntity<L0SliceMetadata>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}		
+		return getResponse(productName, ProductFamily.L0_SLICE, () -> esServices.getL0Slice(productName));	
 	}
-
+	
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, path = "/{productName:.+}/acns")
 	public ResponseEntity<List<L0AcnMetadata>> getAcns(
 			@PathVariable(name = "productName") String productName,
 			@RequestParam(name = "processMode", defaultValue = "NONE") String processMode,
 			@RequestParam(value = "mode", defaultValue = "ALL") String mode) {
-		try {
-			// Retrieve slice
-			L0SliceMetadata f = esServices.getL0Slice(productName);
-			if (f == null) {
-				LOGGER.warn("[L0_SLICE] [productName {}] Not found", productName);
-				return new ResponseEntity<List<L0AcnMetadata>>(HttpStatus.NOT_FOUND);
-			}
-			String productType = f.getProductType();
-			String productTypeWithoutLastChar = productType.substring(0, productType.length() - 1);
-
-			// Retrieve ACN
-			List<L0AcnMetadata> r = new ArrayList<>();
-
-			LOGGER.info("Call getACN for {}A {}", productTypeWithoutLastChar, f.getDatatakeId());
-			L0AcnMetadata l0a = esServices.getL0Acn(productTypeWithoutLastChar + "A", f.getDatatakeId(), processMode);
-			if (l0a != null) {
-				L0AcnMetadata l0aDto = new L0AcnMetadata(l0a.getProductName(), l0a.getProductType(),
-						l0a.getKeyObjectStorage(), l0a.getValidityStart(), l0a.getValidityStop(),
-						l0a.getMissionId(), l0a.getSatelliteId(), l0a.getStationCode(),
-						f.getInstrumentConfigurationId(), f.getNumberSlice(), f.getDatatakeId());
-				l0aDto.setInstrumentConfigurationId(l0a.getInstrumentConfigurationId());
-				l0aDto.setNumberOfSlices(l0a.getNumberOfSlices());
-				l0aDto.setDatatakeId(l0a.getDatatakeId());
-				r.add(l0aDto);
-				if ("ONE".equalsIgnoreCase(mode)) {
-					return new ResponseEntity<List<L0AcnMetadata>>(r, HttpStatus.OK);
+		
+		final Callable<List<L0AcnMetadata>> acnsSupplier = new Callable<List<L0AcnMetadata>>() {
+			@Override
+			public final List<L0AcnMetadata> call() throws Exception {
+				final L0SliceMetadata slice = esServices.getL0Slice(productName);
+				if (slice == null) {
+					throw new MetadataNotPresentException(productName);
 				}
-			}
-            LOGGER.info("Call getACN for {}C {}", productTypeWithoutLastChar, f.getDatatakeId());
-			L0AcnMetadata l0c = esServices.getL0Acn(productTypeWithoutLastChar + "C", f.getDatatakeId(), processMode);
-			if (l0c != null) {
-				L0AcnMetadata l0cDto = new L0AcnMetadata(l0c.getProductName(), l0c.getProductType(),
-						l0c.getKeyObjectStorage(), l0c.getValidityStart(), l0c.getValidityStop(),
-						l0c.getMissionId(), l0c.getSatelliteId(), l0c.getStationCode(),
-						f.getInstrumentConfigurationId(), f.getNumberSlice(), f.getDatatakeId());
-				l0cDto.setInstrumentConfigurationId(l0c.getInstrumentConfigurationId());
-				l0cDto.setNumberOfSlices(l0c.getNumberOfSlices());
-				l0cDto.setDatatakeId(l0c.getDatatakeId());
-				r.add(l0cDto);
-				if ("ONE".equalsIgnoreCase(mode)) {
-					return new ResponseEntity<List<L0AcnMetadata>>(r, HttpStatus.OK);
-				}
-			}
-            LOGGER.info("Call getACN for {}N {}", productTypeWithoutLastChar, f.getDatatakeId());
-			L0AcnMetadata l0n = esServices.getL0Acn(productTypeWithoutLastChar + "N", f.getDatatakeId(), processMode);
-			if (l0n != null) {
-				L0AcnMetadata l0nDto = new L0AcnMetadata(l0n.getProductName(), l0n.getProductType(),
-						l0n.getKeyObjectStorage(), l0n.getValidityStart(), l0n.getValidityStop(),
-						l0n.getMissionId(), l0n.getSatelliteId(), l0n.getStationCode(),
-						f.getInstrumentConfigurationId(), f.getNumberSlice(), f.getDatatakeId());
-				l0nDto.setInstrumentConfigurationId(l0n.getInstrumentConfigurationId());
-				l0nDto.setNumberOfSlices(l0n.getNumberOfSlices());
-				l0nDto.setDatatakeId(l0n.getDatatakeId());
-				r.add(l0nDto);
-				if ("ONE".equalsIgnoreCase(mode)) {
-					return new ResponseEntity<List<L0AcnMetadata>>(r, HttpStatus.OK);
-				}
-			}
-
-			if(l0a == null && l0c == null && l0n == null) {
-				return new ResponseEntity<List<L0AcnMetadata>>(HttpStatus.NOT_FOUND);
-			}
+				return getL0AcnFor(slice, mode);
+			}			
+		};
+		return getResponse(productName, ProductFamily.L0_ACN, acnsSupplier);
+	}
+		
+	private final List<L0AcnMetadata> getL0AcnFor(final L0SliceMetadata slice, final String mode) throws Exception {		
+		final List<L0AcnMetadata> result = new ArrayList<>();
 			
-			return new ResponseEntity<List<L0AcnMetadata>>(r, HttpStatus.OK);
-
-		} catch (MetadataNotPresentException em) {
-			LOGGER.warn("[L0_SLICE] [productName {}] [code {}] {}", productName,
-					em.getCode().getCode(), em.getLogMessage());
-			return new ResponseEntity<List<L0AcnMetadata>>(HttpStatus.NOT_FOUND);
-		} catch (AbstractCodedException ace) {
-			LOGGER.error("[L0_SLICE] [productName {}] [code {}] {}", productName,
-					ace.getCode().getCode(), ace.getLogMessage());
-			return new ResponseEntity<List<L0AcnMetadata>>(HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (Exception e) {
-			LOGGER.error("[L0_SLICE] [productName {}] [code {}] [msg {}]", productName,
-					ErrorCode.INTERNAL_ERROR.getCode(), LogUtils.toString(e));
-			return new ResponseEntity<List<L0AcnMetadata>>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		final String productType = slice.getProductType();
+		final String productTypeWithoutLastChar = productType.substring(0, productType.length() - 1);		
+		final String datatakeId = slice.getDatatakeId();
+		
+		// Retrieve ACN		
+		for (final String acnType : Arrays.asList("A", "C", "N")) {
+			logger.info("Call getACN for {}{} {} (mode: {})", productTypeWithoutLastChar, acnType, datatakeId, mode);
+			
+			final L0AcnMetadata l0acn = esServices.getL0Acn(productTypeWithoutLastChar + acnType, datatakeId, mode);
+			
+			if (l0acn != null) {
+				logger.debug("Got ACN for {}{} {} '{}' (mode: {})", productTypeWithoutLastChar, acnType, datatakeId, 
+						l0acn.getProductName(), mode);
+				result.add(l0acn);
+				
+				if ("ONE".equalsIgnoreCase(mode)) {
+					logger.debug("Got ACN for '{}' (mode: {}): {}", datatakeId, mode, result);
+					return result;
+				}
+			}
+		}		
+		if (result.isEmpty()) {
+			throw new MetadataNotPresentException("ACN for " + slice.getProductName());
+		}		
+		logger.debug("Got ACNs for '{}' (mode: {}): {}", datatakeId, mode, result);
+		return result;		
 	}
 }
