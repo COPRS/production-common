269a47d69bc1a1f4d3200b33469a0b95b2236c61
Anonymous <anon@anon.com>
2019-09-17 10:24:58 +0200

    S1PRO-476: Added 'creationDate' and 'hostname' fields to AbstractDto

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/mqi/model/queue/AbstractDto.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/mqi/model/queue/AbstractDto.java
@@ -1,13 +1,19 @@
 package esa.s1pdgs.cpoc.mqi.model.queue;
 
+import java.util.Date;
+
 import esa.s1pdgs.cpoc.common.ProductFamily;
 
 public abstract class AbstractDto {
 
     private String productName;
-    
+
     private ProductFamily family = ProductFamily.BLANK;
-    
+
+    private Date creationDate;
+
+    private String hostname;
+
 	public AbstractDto() {
 	}
 
@@ -30,5 +36,21 @@ public abstract class AbstractDto {
 
 	public void setFamily(ProductFamily family) {
 		this.family = family;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
 	}
 }
