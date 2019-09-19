bc3ccddd3a4779da1b4399dd3db734cbfbc9c3df
Anonymous <anon@anon.com>
2019-09-19 19:22:23 +0200

    removed ingestor

--- ingestor/config/start.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/ingestor/ -P /app --reject="index.html*"
-exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ingestor.jar --spring.config.location=/app/application.yml
--- ingestor/src/main/java/esa/s1pdgs/cpoc/ingestor/kafka/PublicationServices.java
+++
@@ -1,8 +0,0 @@
-package esa.s1pdgs.cpoc.ingestor.kafka;
-
-import esa.s1pdgs.cpoc.common.errors.mqi.MqiPublicationError;
-
-public interface PublicationServices<T> {
-
-	public void send(T obj) throws MqiPublicationError;
-}
