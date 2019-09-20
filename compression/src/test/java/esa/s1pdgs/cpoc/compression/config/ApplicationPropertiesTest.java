c19690be2b1ee248131ab08d18167d1ab721a23a
Anonymous <anon@anon.com>
2019-09-20 13:00:05 +0200

    fixes

--- compression/src/test/java/esa/s1pdgs/cpoc/compression/config/ApplicationPropertiesTest.java
+++ compression/src/test/java/esa/s1pdgs/cpoc/compression/config/ApplicationPropertiesTest.java
@@ -5,13 +5,17 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import esa.s1pdgs.cpoc.obs_sdk.ObsConfigurationProperties;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DirtiesContext
+@EnableConfigurationProperties({ObsConfigurationProperties.class, ApplicationProperties.class})
 public class ApplicationPropertiesTest {
     /**
      * Properties to test
