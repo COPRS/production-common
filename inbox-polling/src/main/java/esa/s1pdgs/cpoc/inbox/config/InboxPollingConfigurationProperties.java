4c94c90a1e4c4aff463988376ed0de45828be2b6
Anonymous <anon@anon.com>
2019-09-23 15:55:03 +0200

    trying to fix config resolution

--- inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/config/InboxPollingConfigurationProperties.java
+++ inbox-polling/src/main/java/esa/s1pdgs/cpoc/inbox/config/InboxPollingConfigurationProperties.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("inbox")
--- obs-sdk/src/main/java/esa/s1pdgs/cpoc/obs_sdk/ObsConfigurationProperties.java
+++ obs-sdk/src/main/java/esa/s1pdgs/cpoc/obs_sdk/ObsConfigurationProperties.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
