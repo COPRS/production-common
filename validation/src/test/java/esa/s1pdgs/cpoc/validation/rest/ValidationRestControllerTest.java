15cd7fd845b87a7a7ae5c9939aaa172187102cf1
Anonymous <anon@anon.com>
2019-10-01 12:56:05 +0200

    fixed test

--- validation/src/test/java/esa/s1pdgs/cpoc/validation/rest/ValidationRestControllerTest.java
+++ validation/src/test/java/esa/s1pdgs/cpoc/validation/rest/ValidationRestControllerTest.java
@@ -6,9 +6,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,7 +25,6 @@ import esa.s1pdgs.cpoc.validation.service.ValidationService;
 @RunWith(SpringRunner.class)
 @WebMvcTest(ValidationRestController.class)
 public class ValidationRestControllerTest {
-
 	@MockBean
 	private ValidationService validationService;
 
@@ -27,11 +33,14 @@ public class ValidationRestControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private ValidationRestControllerTestConfig config;
 
 	@Test
 	public void testValidate200() throws Exception {
-
-		mockMvc.perform(post("/api/v1/validate").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+		mockMvc.perform(post("/api/v1/validate").contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
 	}
 
 }
