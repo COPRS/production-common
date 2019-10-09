06455511f46c76f4c534c0bb5ea37964fbc3dc62
Anonymous <anon@anon.com>
2019-10-09 15:25:14 +0200

    S1PRO-494: add elastic search implementation of PripMetadataRepository

---
+++ prip/src/main/java/esa/s1pdgs/cpoc/prip/service/metadata/PripElasticSearchMetadataRepo.java
@@ -0,0 +1,26 @@
+package esa.s1pdgs.cpoc.prip.service.metadata;
+
+import java.util.List;
+
+import esa.s1pdgs.cpoc.prip.model.PripMetadata;
+
+public class PripElasticSearchMetadataRepo implements PripMetadataRepository {
+
+	@Override
+	public void save(PripMetadata pripMetadata) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<PripMetadata> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PripMetadata findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
