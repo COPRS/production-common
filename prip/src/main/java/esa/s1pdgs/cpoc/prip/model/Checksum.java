13b3ca5fb01cdef2e2e65b1500f7ab1777b4e096
Anonymous <anon@anon.com>
2019-10-09 10:08:42 +0200

    S1PRO-494: add PRIP model classes

---
+++ prip/src/main/java/esa/s1pdgs/cpoc/prip/model/Checksum.java
@@ -0,0 +1,21 @@
+package esa.s1pdgs.cpoc.prip.model;
+
+public class Checksum {
+	
+	private String algorithm;
+	private String value;
+	
+	public String getAlgorithm() {
+		return algorithm;
+	}
+	public void setAlgorithm(String algorithm) {
+		this.algorithm = algorithm;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
---
+++ prip/src/main/java/esa/s1pdgs/cpoc/prip/model/PripMetadata.java
@@ -0,0 +1,91 @@
+package esa.s1pdgs.cpoc.prip.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class PripMetadata {
+
+	private long id;
+
+	private String obsKey;
+
+	private String name;
+
+	private String contentType;
+
+	private long contentLength;
+
+	private Date creationDate;
+
+	private Date evictionDate;
+
+	private List<Checksum> checksums;
+
+	public PripMetadata() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getObsKey() {
+		return obsKey;
+	}
+
+	public void setObsKey(String obsKey) {
+		this.obsKey = obsKey;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public long getContentLength() {
+		return contentLength;
+	}
+
+	public void setContentLength(long contentLength) {
+		this.contentLength = contentLength;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getEvictionDate() {
+		return evictionDate;
+	}
+
+	public void setEvictionDate(Date evictionDate) {
+		this.evictionDate = evictionDate;
+	}
+
+	public List<Checksum> getChecksums() {
+		return checksums;
+	}
+
+	public void setChecksums(List<Checksum> checksums) {
+		this.checksums = checksums;
+	}
+
+}
