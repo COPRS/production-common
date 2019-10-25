c64dc21d49426e10cfe6ae84023d79582b537cd6
Anonymous <anon@anon.com>
2019-10-25 15:00:24 +0200

    S1PRO-752 (hotfix): when reporting an "end" event, check if the duration can be calculated

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/report/LoggerReporting.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/report/LoggerReporting.java
@@ -80,7 +80,10 @@ public final class LoggerReporting implements Reporting  {
 	
 	@Override
 	public final void end(final ReportingOutput out, final ReportingMessage reportingMessage) {
-		final long deltaTMillis =  System.currentTimeMillis() - actionStart;		
+	    long deltaTMillis = 0;
+		if (actionStart != 0L) {
+			deltaTMillis =  System.currentTimeMillis() - actionStart;		
+		}
 		report(Level.INFO, Event.end, additionalEndJsonFields(0, deltaTMillis, reportingMessage.getTransferAmount(), out), reportingMessage);	
 	}
 
