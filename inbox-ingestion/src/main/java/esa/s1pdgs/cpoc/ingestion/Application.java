ce6e5bd37e2543bd32f9159bb1e29368b043f63b
Anonymous <anon@anon.com>
2019-10-28 17:37:57 +0100

    S1PRO-789: Setting random port for inbox ingestion spring boot app

--- inbox-ingestion/src/main/java/esa/s1pdgs/cpoc/ingestion/Application.java
+++ inbox-ingestion/src/main/java/esa/s1pdgs/cpoc/ingestion/Application.java
@@ -1,5 +1,7 @@
 package esa.s1pdgs.cpoc.ingestion;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,6 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
     public static void main(String[] args) {
+    	Random rand = new Random();
+    	int port = 9000 + rand.nextInt(50000);
+    	System.getProperties().put("server.port", port);
         SpringApplication.run(Application.class, args);
     }
 }
