d8c399272b88eb2211ce5f9d6800a9de21ae28ba
Anonymous <anon@anon.com>
2019-11-06 11:38:06 +0100

    * fixing application location in l0 wrapper scripts

--- mqi-server/config/start.sh
+++ mqi-server/config/start.sh
@@ -4,4 +4,4 @@ if [[ ${HOSTNAME} == inbox-ingestion* ]]
 then
 	sed -i "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" /app/application.yml routing-files/outputs.xml
 fi
-exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/application.yml
+exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/config/application.yml
