580a703e9c8204cf4402fc83345717e36915e866
Anonymous <anon@anon.com>
2019-11-07 15:53:51 +0100

    * adding l2 scaler to helm

--- scaler/config/start.sh
+++ scaler/config/start.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-wget --cut-dirs=1 --no-parent -nH -r $externalconf_host/$externalconf_dir/ -P /app --reject="index.html*"
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-scaler.jar --spring.config.location=/app/config/application.yml
