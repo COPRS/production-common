dd91911729eaa1ccf01fb343eb3503b54f316585
Anonymous <anon@anon.com>
2019-11-08 17:13:03 +0100

    * Set ComponentScan to "esa.s1pdgs.cpoc"

--- mqi-server/src/main/java/esa/s1pdgs/cpoc/mqi/server/Application.java
+++ mqi-server/src/main/java/esa/s1pdgs/cpoc/mqi/server/Application.java
@@ -2,6 +2,7 @@ package esa.s1pdgs.cpoc.mqi.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
 	
     /**
--- request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/Application.java
+++ request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/Application.java
@@ -2,11 +2,13 @@ package esa.s1pdgs.cpoc.reqrepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
 @EnableMongoRepositories
+@ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
