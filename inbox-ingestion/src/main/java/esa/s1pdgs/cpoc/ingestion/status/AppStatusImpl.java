50284918e028a750eb0fc9c23c01687c7527f544
Anonymous <anon@anon.com>
2019-11-11 13:52:13 +0100

    S1PRO-556: Added AppStatus to inbox-ingestion

---
+++ inbox-ingestion/src/main/java/esa/s1pdgs/cpoc/ingestion/status/AppStatusImpl.java
@@ -0,0 +1,38 @@
+package esa.s1pdgs.cpoc.ingestion.status;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import esa.s1pdgs.cpoc.status.AbstractAppStatus;
+import esa.s1pdgs.cpoc.status.Status;
+
+@Component
+public class AppStatusImpl extends AbstractAppStatus {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOGGER = LogManager.getLogger(AppStatusImpl.class);
+
+    @Autowired
+    public AppStatusImpl(
+            @Value("${status.max-error-counter-processing:100}") final int maxErrorCounterProcessing,
+            @Value("${status.max-error-counter-mqi:100}") final int maxErrorCounterNextMessage) {
+    	super(new Status(maxErrorCounterProcessing, maxErrorCounterNextMessage));
+    }    
+
+    /**
+     * Stop the application if someone asks for forcing stop
+     */
+    @Override
+	@Scheduled(fixedDelayString = "${status.delete-fixed-delay-ms:3000}")
+    public void forceStopping() {
+        if (isShallBeStopped()) {
+            System.exit(0);
+        }
+    }
+}
