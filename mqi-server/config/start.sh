403ab15f93e2ebe9c3539ab696ecc712162566eb
Anonymous <anon@anon.com>
2019-11-13 08:58:29 +0100

    * Using for ingestion workaround /app/config as well

--- mqi-server/config/start.sh
+++ mqi-server/config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ ${HOSTNAME} == inbox-ingestion* ]]
 then
-	sed -i "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" /app/application.yml routing-files/outputs.xml
+	sed -i "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" /app/config/application.yml routing-files/outputs.xml
 fi
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/config/application.yml
