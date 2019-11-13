11b2f52814d8a16df63642bffd3f4ff4e0ebf742
Anonymous <anon@anon.com>
2019-11-13 11:13:28 +0100

    This is a work around for the ingestion pods as the /app/config directory is mounted as a read-only filesystem

--- mqi-server/config/start.sh
+++ mqi-server/config/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [[ ${HOSTNAME} == inbox-ingestion* ]]
 then
-	sed -i "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" /app/config/application.yml routing-files/outputs.xml
+    # This is a work around for the ingestion pods as the /app/config directory is mounted as a read-only filesystem
+    cat /app/config/application.yml  | sed "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" "s;config/outputs.xml;outputs.xml;g" > /app/application.yml
+    cat /app/config/outputs.xml  | sed "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" > /app/outputs.xml
+    exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/application.yml
 fi
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/config/application.yml
