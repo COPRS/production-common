b8f40c349b213253239a3a107c7c03d88b3ac7f7
Anonymous <anon@anon.com>
2019-11-18 15:23:03 +0100

    S1PRO-864: Added equals/hashCode/toString to CatalogEvent

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/mqi/model/queue/CatalogEvent.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/mqi/model/queue/CatalogEvent.java
@@ -102,5 +102,92 @@ public class CatalogEvent extends AbstractDto {
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
+	@Override
+	public String toString() {
+		return "CatalogEvent [productType=" + productType + ", missionId=" + missionId + ", satelliteId=" + satelliteId
+				+ ", creationTime=" + creationTime + ", insertionTime=" + insertionTime + ", validityStartTime="
+				+ validityStartTime + ", validityStopTime=" + validityStopTime + ", instrumentConfigurationId="
+				+ instrumentConfigurationId + ", site=" + site + ", url=" + url + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creationTime == null) ? 0 : creationTime.hashCode());
+		result = prime * result + ((insertionTime == null) ? 0 : insertionTime.hashCode());
+		result = prime * result + ((instrumentConfigurationId == null) ? 0 : instrumentConfigurationId.hashCode());
+		result = prime * result + ((missionId == null) ? 0 : missionId.hashCode());
+		result = prime * result + ((productType == null) ? 0 : productType.hashCode());
+		result = prime * result + ((satelliteId == null) ? 0 : satelliteId.hashCode());
+		result = prime * result + ((site == null) ? 0 : site.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		result = prime * result + ((validityStartTime == null) ? 0 : validityStartTime.hashCode());
+		result = prime * result + ((validityStopTime == null) ? 0 : validityStopTime.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CatalogEvent other = (CatalogEvent) obj;
+		if (creationTime == null) {
+			if (other.creationTime != null)
+				return false;
+		} else if (!creationTime.equals(other.creationTime))
+			return false;
+		if (insertionTime == null) {
+			if (other.insertionTime != null)
+				return false;
+		} else if (!insertionTime.equals(other.insertionTime))
+			return false;
+		if (instrumentConfigurationId == null) {
+			if (other.instrumentConfigurationId != null)
+				return false;
+		} else if (!instrumentConfigurationId.equals(other.instrumentConfigurationId))
+			return false;
+		if (missionId == null) {
+			if (other.missionId != null)
+				return false;
+		} else if (!missionId.equals(other.missionId))
+			return false;
+		if (productType == null) {
+			if (other.productType != null)
+				return false;
+		} else if (!productType.equals(other.productType))
+			return false;
+		if (satelliteId == null) {
+			if (other.satelliteId != null)
+				return false;
+		} else if (!satelliteId.equals(other.satelliteId))
+			return false;
+		if (site == null) {
+			if (other.site != null)
+				return false;
+		} else if (!site.equals(other.site))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		if (validityStartTime == null) {
+			if (other.validityStartTime != null)
+				return false;
+		} else if (!validityStartTime.equals(other.validityStartTime))
+			return false;
+		if (validityStopTime == null) {
+			if (other.validityStopTime != null)
+				return false;
+		} else if (!validityStopTime.equals(other.validityStopTime))
+			return false;
+		return true;
+	}
 	
 }
