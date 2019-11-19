6e1faa7d76ce064057f0261922d5cad8a4c6ad61
Anonymous <anon@anon.com>
2019-11-19 10:21:56 +0100

    * Fixing some jar namings

--- ingestion-trigger/config/start.sh
+++ ingestion-trigger/config/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-inbox-polling.jar --spring.config.location=/app/config/application.yml
+exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ingestion-trigger.jar --spring.config.location=/app/config/application.yml
--- ingestion-worker/config/start.sh
+++ ingestion-worker/config/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-inbox-ingestion.jar --spring.config.location=/app/config/application.yml
+exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ingestion-worker.jar --spring.config.location=/app/config/application.yml
