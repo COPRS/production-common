600a6b93a282707401b937ded42a073141ae0172
Anonymous <anon@anon.com>
2019-11-22 15:45:36 +0100

    S1PRO-839: adds unit test for testing application context

---
+++ compression-trigger/src/test/java/esa/s1pdgs/cpoc/compression/trigger/TestApplication.java
@@ -0,0 +1,19 @@
+package esa.s1pdgs.cpoc.compression.trigger;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext
+public class TestApplication {
+
+	@Test
+	public void applicationContextTest() throws InterruptedException {
+		Application.main(new String[] {});
+	}
+}
