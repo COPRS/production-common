796e6a697665875e4362a349b3e1b5149f62655b
Anonymous <anon@anon.com>
2019-11-29 12:28:29 +0100

    AppStatus for CatalogTrigger

---
+++ metadata-catalog-trigger/src/main/java/esa/s1pdgs/cpoc/mdc/trigger/status/AppStatusImpl.java
@@ -0,0 +1,41 @@
+package esa.s1pdgs.cpoc.mdc.trigger.status;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import esa.s1pdgs.cpoc.appstatus.AbstractAppStatus;
+import esa.s1pdgs.cpoc.appstatus.Status;
+
+@Component
+public class AppStatusImpl extends AbstractAppStatus {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOGGER = LogManager.getLogger(AppStatusImpl.class);
+
+	/**
+     * Constructor
+     * 
+     * @param maxErrorCounter
+     */
+    @Autowired
+    public AppStatusImpl(@Value("${status.max-error-counter-processing:100}") final int maxErrorCounterProcessing,
+    		@Value("${status.max-error-counter-mqi:100}") final int maxErrorCounterMqi) {
+    	super(new Status(maxErrorCounterProcessing, maxErrorCounterMqi));
+    }
+	
+    /**
+     * Stop the application if someone asks for forcing stop
+     */
+    @Scheduled(fixedDelayString = "${status.delete-fixed-delay-ms:3000}")
+    public void forceStopping() {
+        if (isShallBeStopped()) {
+            System.exit(0);
+        }
+    }
+}
