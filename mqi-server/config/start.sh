a78567ced467d928e81aac7061165009390e322c
Anonymous <anon@anon.com>
2019-12-05 18:21:54 +0100

    fixed hostname evaluation

--- mqi-server/config/start.sh
+++ mqi-server/config/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ ${HOSTNAME} == ingestion-worker* ]]
+if [[ ${HOSTNAME} == ingestion* ]]
 then
     # This is a work around for the ingestion pods as the /app/config directory is mounted as a read-only filesystem
     cat /app/config/application.yml  | sed -e "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" -e "s;config/outputs.xml;outputs.xml;g" > /app/application.yml
