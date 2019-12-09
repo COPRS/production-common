c24d316190197d14e4a4f3004789be9cbff1a061
Anonymous <anon@anon.com>
2019-12-09 17:36:38 +0100

    fix hostname evaluation

--- mqi-server/config/start.sh
+++ mqi-server/config/start.sh
@@ -2,8 +2,8 @@
 if [[ ${HOSTNAME} == s1pro-ingestion* ]]
 then
     # This is a work around for the ingestion pods as the /app/config directory is mounted as a read-only filesystem
-    cat /app/config/application.yml  | sed -e "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" -e "s;config/outputs.xml;outputs.xml;g" > /app/application.yml
-    cat /app/config/outputs.xml  | sed -e "s;t-pdgs-ingestion;t-pdgs-ingestion-${HOSTNAME};g" > /app/outputs.xml
+    cat /app/config/application.yml  | sed -e "s;t-pdgs-ingestion-jobs;t-pdgs-ingestion-jobs-${HOSTNAME};g" -e "s;config/outputs.xml;outputs.xml;g" > /app/application.yml
+    cat /app/config/outputs.xml  | sed -e "s;t-pdgs-ingestion-jobs;t-pdgs-ingestion-jobs-${HOSTNAME};g" > /app/outputs.xml
     exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/application.yml
 fi
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/mqi-server.jar --spring.config.location=/app/config/application.yml
