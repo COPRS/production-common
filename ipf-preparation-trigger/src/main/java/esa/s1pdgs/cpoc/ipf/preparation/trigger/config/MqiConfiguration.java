b0fe0047a2c8ba15038a0d4b0bb190e8b0d148c0
Anonymous <anon@anon.com>
2019-12-12 14:23:08 +0100

    * Fixing issue with not created GenericMqiClient bean

--- ipf-preparation-trigger/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/trigger/config/MqiConfiguration.java
+++ ipf-preparation-trigger/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/trigger/config/MqiConfiguration.java
@@ -8,56 +8,29 @@ import org.springframework.context.annotation.Configuration;
 
 import esa.s1pdgs.cpoc.mqi.client.GenericMqiClient;
 import esa.s1pdgs.cpoc.mqi.client.MqiClientFactory;
-import esa.s1pdgs.cpoc.mqi.client.StatusService;
 
 /**
- * Configuration of MQI client.<br/>
- * Creation of 3 services: LEVEL_PRODUCTS, LEVEL_REPORTS, LEVEL_JOBS
+ * Configuration of MQI client.
  * 
- * @author Viveris Technologies
+ * @author Faisal Rafi
  */
 @Configuration
 public class MqiConfiguration {
+	
 	private final MqiClientFactory mqiClientFactory;
 
-    /**
-     * Constructor
-     * 
-     * @param hostUri
-     * @param maxRetries
-     * @param tempoRetryMs
-     */
     @Autowired
     public MqiConfiguration(
-            @Value("${process.mqi.host-uri}") final String hostUri,
-            @Value("${process.mqi.max-retries}") final int maxRetries,
-            @Value("${process.mqi.tempo-retry-ms}") final int tempoRetryMs,
-            final RestTemplateBuilder builder
-    ) {
+    		@Value("${mqi.host-uri}") final String hostUri,
+            @Value("${mqi.max-retries}") final int maxRetries,
+            @Value("${mqi.tempo-retry-ms}") final int tempoRetryMs,
+            final RestTemplateBuilder builder) {
     	mqiClientFactory = new MqiClientFactory(hostUri, maxRetries, tempoRetryMs)
     			.restTemplateSupplier(builder::build);
     }
 
-    /**
-     * Service for querying MQI 
-     * 
-     * @param builder
-     * @return
-     */
     @Bean
-    public GenericMqiClient genericMqiClient() {
+    public GenericMqiClient genericService() { 
     	return mqiClientFactory.newGenericMqiService();
-    }
-
-
-    /**
-     * Service for stopping MQI server
-     * 
-     * @param builder
-     * @return
-     */
-    @Bean
-    public StatusService mqiServiceForStatus() {
-    	return mqiClientFactory.newStatusService();
     }
 }
