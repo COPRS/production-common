f767e25e266f2fdd4270df45cb2aaf8974d64d80
Anonymous <anon@anon.com>
2019-12-18 17:13:07 +0100

    fixed message field (it is called 'content')

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/report/message/Message.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/report/message/Message.java
@@ -1,20 +1,20 @@
 package esa.s1pdgs.cpoc.report.message;
 
 public class Message {
-	private String message;
+	private String content;
 	
 	public Message() {
 	}
 	
-	public Message(final String message) {
-		this.message = message;
+	public Message(final String content) {
+		this.content = content;
 	}
 
-	public String getMessage() {
-		return message;
+	public String getContent() {
+		return content;
 	}
 
-	public void setMessage(final String message) {
-		this.message = message;
-	}
+	public void setContent(String content) {
+		this.content = content;
+	}	
 }
