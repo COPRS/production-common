c82ef36f0ee046b3cc380d42fdfefcdef2e23737
Anonymous <anon@anon.com>
2020-01-06 16:38:55 +0100

    S1PRO-948: Refactoring

--- ipf-execution-worker/config/start/start_l0_segment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/var/tmp/conf.sh
-exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ipf-execution-worker.jar --spring.config.location=/app/config/application.yml
--- ipf-execution-worker/config/start/start_l2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/var/tmp/configure_pdgs_custom.sh pac1 8 && ulimit -s unlimited
-exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ipf-execution-worker.jar --spring.config.location=/app/config/application.yml
