e9cf41f2fa67d0c6f15a15a1adaad167b7ef0a30
Anonymous <anon@anon.com>
2020-01-21 11:27:37 +0100

    S1PRO-918: Fixed test

--- obs-sdk/src/test/java/esa/s1pdgs/cpoc/obs_sdk/ObsUploadCallableTest.java
+++ obs-sdk/src/test/java/esa/s1pdgs/cpoc/obs_sdk/ObsUploadCallableTest.java
@@ -18,6 +18,7 @@ import org.mockito.MockitoAnnotations;
 import esa.s1pdgs.cpoc.common.ProductFamily;
 import esa.s1pdgs.cpoc.common.errors.AbstractCodedException;
 import esa.s1pdgs.cpoc.common.utils.FileUtils;
+import esa.s1pdgs.cpoc.report.Reporting;
 
 /**
  * Test the class ObsUploadCallable
@@ -83,7 +84,7 @@ public class ObsUploadCallableTest {
     @Test
     public void testNominalCall()
             throws ObsServiceException, SdkClientException, AbstractCodedException, ObsEmptyFileException {
-        callable = new ObsUploadCallable(obsClient, object);
+        callable = new ObsUploadCallable(obsClient, object, Reporting.ChildFactory.NULL);
         callable.call();
         verify(obsClient, times(1)).upload(Mockito.eq(Arrays.asList(object)), Mockito.any());
     }
