0d218d3161c0c51e1a5545ec52ad96de700aed80
Anonymous <anon@anon.com>
2020-01-21 18:01:07 +0100

    S1PRO-918: Removed Compression REPORT layer

--- compression-worker/src/main/java/esa/s1pdgs/cpoc/compression/worker/process/CompressExecutorCallable.java
+++ compression-worker/src/main/java/esa/s1pdgs/cpoc/compression/worker/process/CompressExecutorCallable.java
@@ -16,7 +16,6 @@ import esa.s1pdgs.cpoc.common.errors.InternalErrorException;
 import esa.s1pdgs.cpoc.compression.worker.config.ApplicationProperties;
 import esa.s1pdgs.cpoc.mqi.model.queue.CompressionJob;
 import esa.s1pdgs.cpoc.report.Reporting;
-import esa.s1pdgs.cpoc.report.ReportingMessage;
 
 public class CompressExecutorCallable implements Callable<Void> {
 
@@ -68,11 +67,8 @@ public class CompressExecutorCallable implements Callable<Void> {
 	
 	public TaskResult execute(final String binaryPath, final String inputPath, final String outputPath,
             final String workDirectory) throws InternalErrorException {
-		
-		final Reporting reporting = reportingChildFactory.newChild("Compression");
-		
+				
 		LOGGER.info("Starting compression task using '{}' with input {} and output {} in {}", binaryPath, inputPath, outputPath, workDirectory);
-        reporting.begin(new ReportingMessage("Start Task {}", binaryPath));
         
         final Consumer<String> stdOutConsumer = DEFAULT_OUTPUT_CONSUMER;
         final Consumer<String> stdErrConsumer = DEFAULT_OUTPUT_CONSUMER;
@@ -99,23 +95,17 @@ public class CompressExecutorCallable implements Callable<Void> {
 			err.get();
 
 		} catch (final InterruptedException ie) {
-			reporting.error(new ReportingMessage("Interrupted Task {}", binaryPath));
 			LOGGER.warn("[task {}] [workDirectory {}]  InterruptedException", binaryPath, workDirectory);
 			Thread.currentThread().interrupt();
 		} catch (final IOException ioe) {
-			final InternalErrorException ex = new InternalErrorException("Cannot build the command for the task " + binaryPath, ioe);
-			reporting.error(new ReportingMessage("[code {}] {}", ex.getCode().getCode(), ex.getLogMessage()));
-			throw ex;
+			throw new InternalErrorException("Cannot build the command for the task " + binaryPath, ioe);
 		} catch (final ExecutionException e) {
-			final InternalErrorException ex =  new InternalErrorException("Error on consuming stdout/stderr of task " + binaryPath, e);
-			reporting.error(new ReportingMessage("[code {}] {}", ex.getCode().getCode(), ex.getLogMessage()));
-			throw ex;
+			throw new InternalErrorException("Error on consuming stdout/stderr of task " + binaryPath, e);
 		} finally {
 			if (process != null) {
 				process.destroy();
 			}
 		}        
-        reporting.end(new ReportingMessage("End Task {} with exit code {}", binaryPath, r));
 
         return new TaskResult(binaryPath, r);
     }
