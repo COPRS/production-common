82ddef6c4afb276a3cbbf6a95ed75236181d3636
Anonymous <anon@anon.com>
2020-01-22 18:33:03 +0100

    S1PRO-1016: Bugfix for DateTimeOffset decimal places

---
+++ prip-frontend/src/test/java/standalone/prip/frontend/service/processor/vistor/ProductsFilterVisitorTest.java
@@ -0,0 +1,29 @@
+package standalone.prip.frontend.service.processor.vistor;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitException;
+import org.junit.Test;
+
+import esa.s1pdgs.cpoc.common.utils.DateUtils;
+import esa.s1pdgs.cpoc.prip.frontend.service.processor.visitor.ProductsFilterVisitor;
+
+public class ProductsFilterVisitorTest {
+
+	@Test
+	public void testConvertToLocalDateTime() throws ExpressionVisitException {
+		final String datetime2 = "2012-01-01T00:00:00.12Z";
+		LocalDateTime result2 = ProductsFilterVisitor.convertToLocalDateTime(datetime2);
+		assertEquals(LocalDateTime.ofInstant(Instant.parse(datetime2), ZoneOffset.UTC), result2);		
+
+		final String datetime3 = "2012-01-01T00:00:00.123Z";		
+		LocalDateTime result3 = ProductsFilterVisitor.convertToLocalDateTime(datetime3);
+		assertEquals(LocalDateTime.ofInstant(Instant.parse(datetime3), ZoneOffset.UTC), result3);
+		assertEquals(DateUtils.parse(datetime3), result3); // reference value from previous calculation method		
+	}
+
+}
