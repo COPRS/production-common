9e63eed319301a0b1e543d48b561e749a66d0bbe
Anonymous <anon@anon.com>
2020-02-04 19:19:25 +0100

    formatting

--- request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/service/RequestRepository.java
+++ request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/service/RequestRepository.java
@@ -9,13 +9,32 @@ import esa.s1pdgs.cpoc.common.MessageState;
 import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessingDto;
 
 public interface RequestRepository {	
-	public static final List<String> PROCESSING_TYPES_LIST = Arrays.asList("t-pdgs-session-file-ingestion-events", "t-pdgs-aux-ingestion-events", "t-pdgs-aio-execution-jobs",
-			"t-pdgs-l0asp-execution-jobs", "t-pdgs-aio-l0-segment-production-events", "t-pdgs-aio-l0-slice-production-events-nrt", "t-pdgs-aio-l0-acn-production-events-nrt",
-			"t-pdgs-l0asp-l0-slice-production-events-fast", "t-pdgs-l0asp-l0-acn-production-events-fast", "t-pdgs-aio-production-report-events", "t-pdgs-l0asp-production-report-events",
-			"t-pdgs-aio-l0-blank-production-events", "t-pdgs-l1-slices-production-events-nrt", "t-pdgs-l1-acn-production-events-nrt", "t-pdgs-l1-slices-production-events-fast",
-			"t-pdgs-l1-acn-production-events-fast", "t-pdgs-l1-production-report-events", "t-pdgs-l1-execution-jobs-nrt", "t-pdgs-l1-execution-jobs-fast",
-			"t-pdgs-l2-acn-production-events-fast", "t-pdgs-l2-slices-production-events-fast", "t-pdgs-l2-execution-jobs-fast", "t-pdgs-l2-production-report-events",
-			"t-pdgs-compression-events");
+	public static final List<String> PROCESSING_TYPES_LIST = Arrays.asList(
+			"t-pdgs-session-file-ingestion-events", 
+			"t-pdgs-aux-ingestion-events", 
+			"t-pdgs-aio-execution-jobs",
+			"t-pdgs-l0asp-execution-jobs", 
+			"t-pdgs-aio-l0-segment-production-events", 
+			"t-pdgs-aio-l0-slice-production-events-nrt", 
+			"t-pdgs-aio-l0-acn-production-events-nrt",
+			"t-pdgs-l0asp-l0-slice-production-events-fast", 
+			"t-pdgs-l0asp-l0-acn-production-events-fast", 
+			"t-pdgs-aio-production-report-events", 
+			"t-pdgs-l0asp-production-report-events",
+			"t-pdgs-aio-l0-blank-production-events", 
+			"t-pdgs-l1-slices-production-events-nrt", 
+			"t-pdgs-l1-acn-production-events-nrt", 
+			"t-pdgs-l1-slices-production-events-fast",
+			"t-pdgs-l1-acn-production-events-fast", 
+			"t-pdgs-l1-production-report-events", 
+			"t-pdgs-l1-execution-jobs-nrt", 
+			"t-pdgs-l1-execution-jobs-fast",
+			"t-pdgs-l2-acn-production-events-fast", 
+			"t-pdgs-l2-slices-production-events-fast", 
+			"t-pdgs-l2-execution-jobs-fast", 
+			"t-pdgs-l2-production-report-events",
+			"t-pdgs-compression-events"
+	);
 	
 	public static final List<MessageState> PROCESSING_STATE_LIST = Arrays.asList(MessageState.values());
 	
