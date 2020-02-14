62a47792736bc45ad345e7cafc8b72999731fb05
Anonymous <anon@anon.com>
2020-02-14 14:54:26 +0100

    S1PRO-1067: Reworking ingestion-trigger reporting

--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Inbox.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Inbox.java
@@ -3,18 +3,24 @@ package esa.s1pdgs.cpoc.ingestion.trigger;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
+import esa.s1pdgs.cpoc.common.utils.LogUtils;
 import esa.s1pdgs.cpoc.ingestion.trigger.entity.InboxEntry;
 import esa.s1pdgs.cpoc.ingestion.trigger.filter.InboxFilter;
 import esa.s1pdgs.cpoc.ingestion.trigger.kafka.producer.SubmissionClient;
+import esa.s1pdgs.cpoc.ingestion.trigger.report.IngestionTriggerReportingInput;
+import esa.s1pdgs.cpoc.ingestion.trigger.report.IngestionTriggerReportingOutput;
 import esa.s1pdgs.cpoc.mqi.model.queue.IngestionJob;
+import esa.s1pdgs.cpoc.report.Reporting;
 import esa.s1pdgs.cpoc.report.ReportingMessage;
 import esa.s1pdgs.cpoc.report.ReportingUtils;
 
@@ -78,19 +84,43 @@ public final class Inbox {
 				
 				// all products not stored in the repo are considered new and shall be added to
 				// the configured queue.
-				for (final InboxEntry e : newElements) {
-					final File file = Paths.get(e.getPickupPath(), e.getRelativePath()).toFile();
-					if(org.apache.commons.io.FileUtils.sizeOf(file) == 0) {						
-						final String errorMessage = "Empty file detected, skipping: " + file.getName();
-						ReportingUtils.newReportingBuilder().newEventReporting(new ReportingMessage(errorMessage));
+				for (final InboxEntry entry : newElements) {
+					// TODO add retries					
+					final File file = Paths.get(entry.getPickupPath(), entry.getRelativePath()).toFile();
+					final String filename = file.getName();
+					
+					final Reporting reporting = ReportingUtils.newReportingBuilder()
+							.newReporting("IngestionTrigger");
 						
+					reporting.begin(
+							new IngestionTriggerReportingInput(file.getName(), new Date(), new Date(file.lastModified())),
+							new ReportingMessage("New file detected %s", filename)
+					);
+					
+					if (FileUtils.sizeOf(file) == 0) {	
+						reporting.error(new ReportingMessage("File %s is empty, ignored.", filename));						
 						// ensure that the empty file is persisted so it is not found again
-						final InboxEntry persisted = ingestionTriggerServiceTransactional.add(e);
+						final InboxEntry persisted = ingestionTriggerServiceTransactional.add(entry);
 						LOG.debug("Added {} to persistence", persisted);
 						continue;
 					}
-					handleNew(e);
-					ReportingUtils.newReportingBuilder().newEventReporting(new ReportingMessage("Handled file: " + file.getName()));
+					
+					try {
+						LOG.debug("Publishing new entry to kafka queue: {}", entry);					
+						final IngestionJob dto = new IngestionJob(entry.getName());
+					    dto.setRelativePath(entry.getRelativePath());
+					    dto.setPickupPath(entry.getPickupPath());
+					    dto.setUid(reporting.getUid());
+						client.publish(dto);
+						persist(entry);		
+						reporting.end(
+								new IngestionTriggerReportingOutput("file://" + file.getAbsolutePath()), 
+								new ReportingMessage("File %s created IngestionJob", filename)
+						);
+					} catch (final Exception e) {
+						reporting.error(new ReportingMessage("File %s could not be handled: %s", filename, LogUtils.toString(e)));
+						LOG.error(String.format("Error on handling %s in %s: %s", entry, description(), LogUtils.toString(e)));
+					}
 				}
 						
 			}
@@ -101,6 +131,8 @@ public final class Inbox {
 			LOG.error(String.format("Error on polling %s", description()), e);
 		}
 	}
+	
+
 
 	public final String description() {
 		return inboxAdapter.description() + " (" + filter + ")";
@@ -110,19 +142,11 @@ public final class Inbox {
 	public final String toString() {
 		return "Inbox [inboxAdapter=" + inboxAdapter + ", filter=" + filter + ", client=" + client + "]";
 	}
-
-	private void handleNew(final InboxEntry entry) {
-		try {
-			LOG.debug("Publishing new entry to kafka queue: {}", entry);
-			final IngestionJob dto = new IngestionJob(entry.getName());
-		    dto.setRelativePath(entry.getRelativePath());
-		    dto.setPickupPath(entry.getPickupPath());
-			client.publish(dto);
-			final InboxEntry persisted = ingestionTriggerServiceTransactional.add(entry);
-			LOG.debug("Added {} to persistence", persisted);
-		} catch (final Exception e) {
-			LOG.error(String.format("Error on handling %s in %s", entry, description()), e);
-		}
+	
+	private final InboxEntry persist(final InboxEntry toBePersisted) {
+		final InboxEntry persisted = ingestionTriggerServiceTransactional.add(toBePersisted);
+		LOG.debug("Added {} to persistence", persisted);
+		return persisted;
 	}
 	
 	private final String summarize(final Collection<InboxEntry> entries) {
---
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/report/IngestionTriggerReportingInput.java
@@ -0,0 +1,46 @@
+package esa.s1pdgs.cpoc.ingestion.trigger.report;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import esa.s1pdgs.cpoc.report.message.input.FilenameReportingInput;
+
+public class IngestionTriggerReportingInput extends FilenameReportingInput {
+	
+	@JsonProperty("pickup_point_detection_date")
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'000Z'", timezone="UTC")
+	private Date pollingDate = new Date();
+	
+	@JsonProperty("pickup_point_available_date")
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'000Z'", timezone="UTC")
+	private Date availDate = new Date();
+	
+	public IngestionTriggerReportingInput()
+	{
+		super();
+	}
+
+	public IngestionTriggerReportingInput(final String filename, final Date pollingDate, final Date availDate) {
+		super(filename);
+		this.pollingDate = pollingDate;
+		this.availDate = availDate;
+	}
+
+	public Date getPollingDate() {
+		return pollingDate;
+	}
+
+	public void setPollingDate(final Date pollingDate) {
+		this.pollingDate = pollingDate;
+	}
+
+	public Date getAvailDate() {
+		return availDate;
+	}
+
+	public void setAvailDate(final Date availDate) {
+		this.availDate = availDate;
+	}
+}
