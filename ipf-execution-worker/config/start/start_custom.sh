c3e4869eae468cdac55bb33e50ad18f89570cf0b
Anonymous <anon@anon.com>
2020-03-03 09:16:17 +0100

    S1PRO-1174 & S1PRO-1190: Taking environment setup from airbus out of the start_custom.sh script

--- ipf-execution-worker/config/start/start_custom.sh
+++ ipf-execution-worker/config/start/start_custom.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-/app/airbus/entrypoint.sh pac1 8 && ulimit -s unlimited
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ipf-execution-worker.jar --spring.config.location=/app/config/application.yml
