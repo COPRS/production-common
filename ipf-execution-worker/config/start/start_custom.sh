3030b5d47291c2acd75e75898740553982f7f727
Anonymous <anon@anon.com>
2020-03-04 09:26:10 +0100

    * reverting workarounds for root permissions as they generate a segfault

--- ipf-execution-worker/config/start/start_custom.sh
+++ ipf-execution-worker/config/start/start_custom.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+/app/airbus/entrypoint.sh pac1 8 && ulimit -s unlimited
 exec java -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ipf-execution-worker.jar --spring.config.location=/app/config/application.yml
