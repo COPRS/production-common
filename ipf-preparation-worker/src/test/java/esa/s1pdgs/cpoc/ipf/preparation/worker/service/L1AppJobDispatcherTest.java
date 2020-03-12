6eb4cd65aee234df78e0dbbcb11887be18036086
Anonymous <anon@anon.com>
2020-03-12 12:42:14 +0100

    S1PRO-1181: Fixed test

--- ipf-preparation-worker/src/test/java/esa/s1pdgs/cpoc/ipf/preparation/worker/service/L1AppJobDispatcherTest.java
+++ ipf-preparation-worker/src/test/java/esa/s1pdgs/cpoc/ipf/preparation/worker/service/L1AppJobDispatcherTest.java
@@ -268,17 +268,17 @@ public class L1AppJobDispatcherTest {
             verify(jobGenerationTaskScheduler, times(this.nbTaskTables))
                     .scheduleWithFixedDelay(any(), eq(2000L));
             verify(jobsGeneratorFactory, never())
-                    .newJobGenerator(any(), any(), JobGenType.LEVEL_0);
+                    .newJobGenerator(any(), any(), eq(JobGenType.LEVEL_0));
             verify(jobsGeneratorFactory, times(this.nbTaskTables))
-                    .newJobGenerator(any(), any(), JobGenType.LEVEL_PRODUCT);
+                    .newJobGenerator(any(), any(), eq(JobGenType.LEVEL_PRODUCT));
             verify(jobsGeneratorFactory, times(1))
-                    .newJobGenerator(eq(taskTable1), any(), JobGenType.LEVEL_PRODUCT);
+                    .newJobGenerator(eq(taskTable1), any(), eq(JobGenType.LEVEL_PRODUCT));
             verify(jobsGeneratorFactory, times(1))
-                    .newJobGenerator(eq(taskTable2), any(), JobGenType.LEVEL_PRODUCT);
+                    .newJobGenerator(eq(taskTable2), any(), eq(JobGenType.LEVEL_PRODUCT));
             verify(jobsGeneratorFactory, times(1))
-                    .newJobGenerator(eq(taskTable3), any(), JobGenType.LEVEL_PRODUCT);
+                    .newJobGenerator(eq(taskTable3), any(), eq(JobGenType.LEVEL_PRODUCT));
             verify(jobsGeneratorFactory, times(1))
-                    .newJobGenerator(eq(taskTable4), any(), JobGenType.LEVEL_PRODUCT);
+                    .newJobGenerator(eq(taskTable4), any(), eq(JobGenType.LEVEL_PRODUCT));
             assertTrue(dispatcher.getGenerators().size() == this.nbTaskTables);
             assertTrue(dispatcher.getGenerators()
                     .containsKey(taskTable1.getName()));
