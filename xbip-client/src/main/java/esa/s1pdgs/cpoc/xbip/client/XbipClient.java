9a201f1a4bb3ded8ac844616867951ef8afeabf7
Anonymous <anon@anon.com>
2020-03-25 15:00:24 +0100

    S1PRO-1306: provide factory method an null object

--- xbip-client/src/main/java/esa/s1pdgs/cpoc/xbip/client/XbipClient.java
+++ xbip-client/src/main/java/esa/s1pdgs/cpoc/xbip/client/XbipClient.java
@@ -1,8 +1,22 @@
 package esa.s1pdgs.cpoc.xbip.client;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 public interface XbipClient {	
+	static final XbipClient NULL = new XbipClient() {
+		@Override
+		public final List<XbipEntry> list(final XbipEntryFilter filter) throws IOException {
+			return Collections.emptyList();
+		}		
+	};
+	
+	static XbipClient newXbipClient(final URI serverUrl) {
+		// TODO
+		return NULL;
+	}
+	
 	List<XbipEntry> list(XbipEntryFilter filter) throws IOException;
 }
