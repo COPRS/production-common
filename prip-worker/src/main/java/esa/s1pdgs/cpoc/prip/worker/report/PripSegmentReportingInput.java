d2b5c8a0e521b6b777275c956ac869ea25773ddb
Anonymous <anon@anon.com>
2020-03-25 18:35:16 +0100

    S1PRO-1286: Fixes according feedback from Pierre

--- prip-worker/src/main/java/esa/s1pdgs/cpoc/prip/worker/report/PripSegmentReportingInput.java
+++ prip-worker/src/main/java/esa/s1pdgs/cpoc/prip/worker/report/PripSegmentReportingInput.java
@@ -5,14 +5,14 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import esa.s1pdgs.cpoc.report.message.input.FilenameReportingInput;
+import esa.s1pdgs.cpoc.report.message.input.SegmentReportingInput;
 
-public class PripReportingInput extends FilenameReportingInput {	
+public class PripSegmentReportingInput extends SegmentReportingInput {	
 	@JsonProperty("prip_storage_date")
 	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'000Z'", timezone="UTC")
 	private Date storeDate;
 
-	public PripReportingInput(final String filename, final Date storeDate) {
+	public PripSegmentReportingInput(final String filename, final Date storeDate) {
 		super(filename);
 		this.storeDate = storeDate;
 	}
