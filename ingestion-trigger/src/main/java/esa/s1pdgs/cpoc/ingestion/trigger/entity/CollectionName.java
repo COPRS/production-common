10f5861b8cc52068d65133ad1c5efa39088f22d8
Anonymous <anon@anon.com>
2020-04-05 12:00:50 +0200

    S1PRO-809: use ${spring.application.name} for mongodb collection name (tbc.)

---
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/entity/CollectionName.java
@@ -0,0 +1,20 @@
+package esa.s1pdgs.cpoc.ingestion.trigger.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Determines the mongodb collection name used for {@link InboxEntry} by this ingestion-trigger app.
+ * see <a href="https://jira.spring.io/browse/DATAMONGO-525?focusedCommentId=89157&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-89157">https://jira.spring.io/browse/DATAMONGO-525</a>
+ */
+@Component
+public class CollectionName {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public String getName() {
+        return "appName";
+    }
+
+}
--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/entity/InboxEntry.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/entity/InboxEntry.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.Objects;
 
-@Document
+@Document(collection = "#{@collectionName.name}")
 public class InboxEntry {
 
 	@Transient
