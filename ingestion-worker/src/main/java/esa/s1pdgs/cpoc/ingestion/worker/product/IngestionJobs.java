3d66bbb48f04bc165048c280b720886291a46806
Anonymous <anon@anon.com>
2020-04-06 10:56:28 +0200

    S1PRO-1142: Extended tests

--- ingestion-worker/src/main/java/esa/s1pdgs/cpoc/ingestion/worker/product/IngestionJobs.java
+++ ingestion-worker/src/main/java/esa/s1pdgs/cpoc/ingestion/worker/product/IngestionJobs.java
@@ -11,6 +11,16 @@ import esa.s1pdgs.cpoc.mqi.model.queue.IngestionJob;
 public class IngestionJobs {
 	public static final URI toUri(final IngestionJob ingestion) throws InternalErrorException {
 		try {
+			System.out.println(ingestion.getPickupBaseURL());
+			System.out.println(ingestion.getRelativePath());
+			
+			System.out.println("OLD: " + new URIBuilder(ingestion.getPickupBaseURL() + ingestion.getRelativePath()).build(););
+			System.out.println("new: " + new URIBuilder(ingestion.getPickupBaseURL() + ("/" + ingestion.getRelativePath()).replaceAll("//", "/")).build());
+			System.out.println("NEW: " + new URIBuilder(ingestion.getPickupBaseURL() + ("/" + ingestion.getRelativePath()).replaceAll("//", "/")).build());
+			System.out.println(ingestion.getPickupBaseURL());
+			System.out.println(ingestion.getRelativePath());
+			
+			//return new URIBuilder(ingestion.getPickupBaseURL() + ("/" + ingestion.getRelativePath()).replaceFirst("//", "/")).build();
 			return new URIBuilder(ingestion.getPickupBaseURL() + ingestion.getRelativePath()).build();
 		} catch (final URISyntaxException e) {
 			throw new InternalErrorException(
