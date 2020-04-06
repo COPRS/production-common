d1d584b04a18ce704bcdcce71227bd587c9303c9
Anonymous <anon@anon.com>
2020-04-06 12:20:39 +0200

    fixed missing path element

--- ingestion-worker/src/main/java/esa/s1pdgs/cpoc/ingestion/worker/product/IngestionJobs.java
+++ ingestion-worker/src/main/java/esa/s1pdgs/cpoc/ingestion/worker/product/IngestionJobs.java
@@ -2,6 +2,8 @@ package esa.s1pdgs.cpoc.ingestion.worker.product;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.http.client.utils.URIBuilder;
 
@@ -11,8 +13,12 @@ import esa.s1pdgs.cpoc.mqi.model.queue.IngestionJob;
 public class IngestionJobs {
 	public static final URI toUri(final IngestionJob ingestion) throws InternalErrorException {
 		try {
-			return new URIBuilder(ingestion.getPickupBaseURL())
-					.setPath(ingestion.getRelativePath())
+			final URI baseUri = new URI(ingestion.getPickupBaseURL());			
+			final Path resultPath = Paths.get(baseUri.getPath())
+					.resolve(ingestion.getRelativePath());			
+			
+			return new URIBuilder(baseUri)
+					.setPath(resultPath.toAbsolutePath().toString())
 					.build();
 		} catch (final URISyntaxException e) {
 			throw new InternalErrorException(
