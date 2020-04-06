b959340b8ff560a7448ed88d5eefd458857e8178
Anonymous <anon@anon.com>
2020-04-06 10:59:37 +0200

    S1PRO-1142: Extended tests

--- ingestion-worker/src/test/java/esa/s1pdgs/cpoc/ingestion/worker/product/TestIngestionJobs.java
+++ ingestion-worker/src/test/java/esa/s1pdgs/cpoc/ingestion/worker/product/TestIngestionJobs.java
@@ -13,13 +13,24 @@ import esa.s1pdgs.cpoc.mqi.model.queue.IngestionJob;
 public class TestIngestionJobs {
 
 	@Test
-	public void testToUri() throws InternalErrorException, URISyntaxException {
+	public void testToUri_OnFile() throws InternalErrorException, URISyntaxException {
 		IngestionJob ingestionJob = new IngestionJob();
 		ingestionJob.setProductName("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
 		ingestionJob.setRelativePath("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
 		ingestionJob.setPickupBaseURL("file:///data/inbox/AUX/");
 		URI result = IngestionJobs.toUri(ingestionJob);
 		URI expected = new URI("file:///data/inbox/AUX/S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testToUri_OnXBIP() throws InternalErrorException, URISyntaxException {
+		IngestionJob ingestionJob = new IngestionJob();
+		ingestionJob.setProductName("DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");
+		ingestionJob.setRelativePath("S1A/DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");
+		ingestionJob.setPickupBaseURL("https://cgs03.sentinel1.eo.esa.int/NOMINAL");
+		URI result = IngestionJobs.toUri(ingestionJob);
+		URI expected = new URI("https://cgs03.sentinel1.eo.esa.int/NOMINAL/S1A/DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");
 		assertEquals(expected, result);
 	}
 
