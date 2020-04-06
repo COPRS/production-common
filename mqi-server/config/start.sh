8e340b02eac1c9d00f04baca92cc31d54e3d1409
Anonymous <anon@anon.com>
2020-04-06 20:45:51 +0200

    adapt workaround not to match for xbip

--- mqi-server/config/start.sh
+++ mqi-server/config/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ ${HOSTNAME} == s1pro-ingestion* ]]
+if [[ ${HOSTNAME} == s1pro-ingestion-[0-9]* ]]
 then
     # This is a work around for the ingestion pods as the /app/config directory is mounted as a read-only filesystem
     cat /app/config/application.yml  | sed -e "s;t-pdgs-ingestion-jobs;t-pdgs-ingestion-jobs-${HOSTNAME};g" -e "s;config/outputs.xml;outputs.xml;g" > /app/application.yml
