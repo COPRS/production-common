0bf004346204cba98109c1e05dd01caefcc65205
Anonymous <anon@anon.com>
2020-04-08 19:51:53 +0200

    fix TestApplication (only persistent Entries for current station are deleted in Inbox)

--- ingestion-trigger/src/test/java/esa/s1pdgs/cpoc/ingestion/trigger/TestApplication.java
+++ ingestion-trigger/src/test/java/esa/s1pdgs/cpoc/ingestion/trigger/TestApplication.java
@@ -1,7 +1,19 @@
 package esa.s1pdgs.cpoc.ingestion.trigger;
 
-import static org.junit.Assert.assertEquals;
+import esa.s1pdgs.cpoc.ingestion.trigger.config.IngestionTriggerConfigurationProperties;
+import esa.s1pdgs.cpoc.ingestion.trigger.entity.InboxEntry;
+import esa.s1pdgs.cpoc.ingestion.trigger.entity.InboxEntryRepository;
+import esa.s1pdgs.cpoc.ingestion.trigger.fs.FilesystemInboxEntryFactory;
+import esa.s1pdgs.cpoc.ingestion.trigger.service.IngestionTriggerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
@@ -11,21 +23,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import javax.transaction.Transactional;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import esa.s1pdgs.cpoc.ingestion.trigger.config.IngestionTriggerConfigurationProperties;
-import esa.s1pdgs.cpoc.ingestion.trigger.entity.InboxEntry;
-import esa.s1pdgs.cpoc.ingestion.trigger.entity.InboxEntryRepository;
-import esa.s1pdgs.cpoc.ingestion.trigger.fs.FilesystemInboxEntryFactory;
-import esa.s1pdgs.cpoc.ingestion.trigger.service.IngestionTriggerService;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -43,7 +41,7 @@ public class TestApplication {
 	private InboxEntryRepository repo;
 
 	@Test
-	public void testPollAll_OnEmptyInboxAndPersistedEntries_ShallDeletePersistedEntries() throws InterruptedException, URISyntaxException {		
+	public void testPollAll_OnEmptyInboxAndPersistedEntries_ShallDeletePersistedEntriesForInboxStation() throws URISyntaxException {
 		
 		final URI inboxURL = new URI(props.getPolling().get(0).getDirectory());
 		final Path inboxPath = Paths.get(inboxURL.getPath());
@@ -52,30 +50,34 @@ public class TestApplication {
 			inboxPath.toFile().mkdirs();
 		}
 		
-		final FilesystemInboxEntryFactory fact = new FilesystemInboxEntryFactory();	
+		final FilesystemInboxEntryFactory fact = new FilesystemInboxEntryFactory();
 		final InboxEntry content = fact.newInboxEntry(
-				inboxURL, 
-				inboxPath.resolve("WILE/S1B/L20180724144436762001030/ch01/DCS_02_L20180724144436762001030_ch1_DSIB.xml"), 
-				2, 
-				new Date(), 
+				inboxURL,
+				inboxPath.resolve("WILE/S1B/L20180724144436762001030/ch01/DCS_02_L20180724144436762001030_ch1_DSIB.xml"),
+				2,
+				new Date(),
 				0,
 				"WILE"
 		);
 		final InboxEntry content2 = fact.newInboxEntry(
-				inboxURL, 
-				inboxPath.resolve("AUX/S1__AUX_ICE_V20160501T120000_G20160502T043607.SAFE"), 
-				1, 
+				inboxURL,
+				inboxPath.resolve("AUX/S1__AUX_ICE_V20160501T120000_G20160502T043607.SAFE"),
+				1,
 				new Date(),
 				0,
 				null
 		);
-		
+
 		repo.save(content);
 		repo.save(content2);
+		assertEquals(2, read().size());
+
 		service.pollAll();
-		
-		final List<InboxEntry> actual = read();
-		assertEquals(0, actual.size());
+
+		final List<InboxEntry> inboxEntries = read();
+		assertEquals(1, inboxEntries.size());
+		assertEquals(content, inboxEntries.get(0));
+
 	}
 
 	private List<InboxEntry> read() {
