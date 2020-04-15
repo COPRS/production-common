389dad5e7df3bb78cdabb7bb12f40060888158ba
Anonymous <anon@anon.com>
2020-04-15 15:46:02 +0200

    S1PRO-1373: extracted class and cleanup

--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/inbox/Inbox.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/inbox/Inbox.java
@@ -2,13 +2,8 @@ package esa.s1pdgs.cpoc.ingestion.trigger.inbox;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -29,16 +24,7 @@ import esa.s1pdgs.cpoc.report.Reporting;
 import esa.s1pdgs.cpoc.report.ReportingMessage;
 import esa.s1pdgs.cpoc.report.ReportingUtils;
 
-public final class Inbox {
-	// just make sure top level dirs are handled first
-	static final Comparator<InboxEntry> COMP = new Comparator<InboxEntry>() {
-		@Override
-		public final int compare(final InboxEntry o1, final InboxEntry o2) {			
-			return Paths.get(o1.getRelativePath()).getNameCount() - Paths.get(o2.getRelativePath()).getNameCount();
-		}		
-	};
-	
-	
+public final class Inbox {	
 	private final Logger log;
 	
 	private final InboxAdapter inboxAdapter;
@@ -68,104 +54,6 @@ public final class Inbox {
 		this.log = LoggerFactory.getLogger(String.format("%s (%s) for %s", getClass().getName(), stationName, family));
 	}
 	
-	static final class PollingRun {
-		private final Set<InboxEntry> persistedContent;
-		private final Set<InboxEntry> pickupContent;
-		private final Set<InboxEntry> finishedElements;
-		private final List<InboxEntry> newElements;
-		
-		PollingRun(
-				final Set<InboxEntry> persistedContent, 
-				final Set<InboxEntry> pickupContent,
-				final Set<InboxEntry> finishedElements, 
-				final List<InboxEntry> newElements
-		) {
-			this.persistedContent = persistedContent;
-			this.pickupContent = pickupContent;
-			this.finishedElements = finishedElements;
-			this.newElements = newElements;
-		}
-		
-		static PollingRun newInstance(final Set<InboxEntry> persistedContent, final List<InboxEntry> pickupContent) {
-			// determine the entries that have been deleted from inbox to remove them from persistence
-			final Set<InboxEntry> finishedElements = new HashSet<>(persistedContent);
-			finishedElements.removeAll(pickupContent);
-			
-			// detect all elements that are considered "new" on the inbox
-			final List<InboxEntry> newElements = new ArrayList<>(pickupContent);
-			newElements.removeAll(persistedContent);
-			Collections.sort(newElements, COMP);
-			
-			return new PollingRun(
-					persistedContent, 
-					new HashSet<>(pickupContent), 
-					finishedElements, 
-					newElements
-			);
-		}
-
-		public Set<InboxEntry> finishedElements() {
-			return finishedElements;
-		}		
-		
-		public List<InboxEntry> newElements() {				
-			return newElements;
-		}	
-		
-		private final void dumpTo(final Set<InboxEntry> handledElements, final Logger log) {
-			final StringBuilder logMessage = new StringBuilder();
-			
-			if (!finishedElements.isEmpty()) {			
-				logMessage.append("Handled ")
-					.append(finishedElements.size())
-					.append(" finished elements");
-				
-				dumpToDebugLog(log, "deleting {} from persistence",finishedElements);			
-			}
-			
-			if (!newElements.isEmpty()) {
-				final Set<InboxEntry> ignoredElements = new HashSet<>(newElements);
-				ignoredElements.removeAll(handledElements);
-				
-				if (logMessage.length() == 0) {
-					logMessage.append("Handled ");
-				} else {
-					logMessage.append(" and ");
-				}	
-				logMessage.append(newElements.size())
-					.append(" new elements (processed: ")
-					.append(handledElements.size())
-					.append(" / ignored: ")
-					.append(ignoredElements.size())
-					.append(").");
-				
-				if (!handledElements.isEmpty()) {
-					dumpToDebugLog(log, "processed {} from inbox", handledElements);		
-				}				
-				if (!ignoredElements.isEmpty()) {
-					dumpToDebugLog(log, "ignored {} in inbox", ignoredElements);		
-				}
-			}		
-			if (logMessage.length() != 0) {
-				log.info(logMessage.toString());
-			}
-		}
-		
-		private final void dumpToDebugLog(final Logger log, final String logTemplate, final Collection<InboxEntry> entries) {
-			if (log.isDebugEnabled()) {
-				entries.stream()
-					.map(p -> p.getRelativePath())
-					.forEach(e -> log.debug("PickupAction - " + logTemplate, e));
-			}			
-		}
-
-		@Override
-		public String toString() {
-			return "PollingRun [persistedContent=" + persistedContent.size() + ", pickupContent=" + pickupContent.size()
-					+ ", finishedElements=" + finishedElements.size() + ", newElements=" + newElements.size() + "]";
-		}
-	}
-	
 	public final void poll() {	
 		try {
 			final PollingRun pollingRun = PollingRun.newInstance(
@@ -188,13 +76,21 @@ public final class Inbox {
 				persist(newEntry);
 			}		
 			pollingRun.dumpTo(handledElements, log);
-			log.debug(pollingRun.toString());
+			log.trace(pollingRun.toString());
 		} catch (final Exception e) {			
 			// thrown on error reading the Inbox. No real retry here as it will be retried on next polling attempt anyway	
 			log.error(String.format("Error on polling %s", description()), e);
 		}
 	}
-	
+	public final String description() {
+		return inboxAdapter.description() + " for productFamily " + family + "";
+	}
+
+	@Override
+	public final String toString() {
+		return "Inbox [inboxAdapter=" + inboxAdapter + ", filter=" + filter + ", client=" + client + "]";
+	}
+		
 	private final boolean isChildOf(final InboxEntry entry, final Set<InboxEntry> handledElements) {
 		final Path thisPath = Paths.get(entry.getRelativePath());
 		
@@ -205,15 +101,6 @@ public final class Inbox {
 			}			
 		}
 		return false;		
-	}
-
-	public final String description() {
-		return inboxAdapter.description() + " for productFamily " + family + "";
-	}
-
-	@Override
-	public final String toString() {
-		return "Inbox [inboxAdapter=" + inboxAdapter + ", filter=" + filter + ", client=" + client + "]";
 	}
 
 	private final Optional<InboxEntry> handleEntry(final InboxEntry entry) {				
