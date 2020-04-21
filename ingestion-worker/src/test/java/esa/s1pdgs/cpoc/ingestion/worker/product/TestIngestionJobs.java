e01a32df4fa3ec2d6f2b31edfa984155dff5079e
Anonymous <anon@anon.com>
2020-04-21 22:49:37 +0200

    S1PRO-1274: simplified unit test

--- ingestion-worker/src/test/java/esa/s1pdgs/cpoc/ingestion/worker/product/TestIngestionJobs.java
+++ ingestion-worker/src/test/java/esa/s1pdgs/cpoc/ingestion/worker/product/TestIngestionJobs.java
@@ -37,31 +37,21 @@ public class TestIngestionJobs {
 	@Test
 	public final void testFilename_OnSlashes_ShallReturnLastElement() {
 		final IngestionJob ingestionJob = new IngestionJob();
-		ingestionJob.setKeyObjectStorage("DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");
-		ingestionJob.setProductName("DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");
-		ingestionJob.setRelativePath("S1A/DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");
-		ingestionJob.setPickupBaseURL("https://cgs03.sentinel1.eo.esa.int/NOMINAL");
-		
+		ingestionJob.setKeyObjectStorage("DCS_01_20200404173230031981_dat/ch_1/DCS_01_20200404173230031981_ch1_DSDB_00021.raw");		
 		assertEquals("DCS_01_20200404173230031981_ch1_DSDB_00021.raw", IngestionJobs.filename(ingestionJob));
 	}
 	
 	@Test
 	public final void testFilename_OnPlainFilename_ShallReturnLastElement() {
 		final IngestionJob ingestionJob = new IngestionJob();
-		ingestionJob.setKeyObjectStorage("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
-		ingestionJob.setProductName("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
-		ingestionJob.setRelativePath("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
-		ingestionJob.setPickupBaseURL("file:///data/inbox/AUX/");		
+		ingestionJob.setKeyObjectStorage("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");	
 		assertEquals("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF", IngestionJobs.filename(ingestionJob));
 	}
 	
 	@Test
 	public final void testFilename_OnTrailingSlash_ShallIgnoreTrailingSlash() {
 		final IngestionJob ingestionJob = new IngestionJob();
-		ingestionJob.setKeyObjectStorage("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.SAFE/");
-		ingestionJob.setProductName("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
-		ingestionJob.setRelativePath("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.EOF");
-		ingestionJob.setPickupBaseURL("file:///data/inbox/AUX/");		
+		ingestionJob.setKeyObjectStorage("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.SAFE/");		
 		assertEquals("S1__OPER_MSK__LAND__V20140403T210200_G20190711T113000.SAFE", IngestionJobs.filename(ingestionJob));
 	}
 
