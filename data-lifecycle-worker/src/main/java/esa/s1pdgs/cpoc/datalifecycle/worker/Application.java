a12671497b5d2c3f008d0d76e889ee7edbe0595d
Anonymous <anon@anon.com>
2020-05-13 14:30:37 +0200

    S1PRO-898: created s1pro-data-lifecycle-worker skeleton

---
+++ data-lifecycle-worker/src/main/java/esa/s1pdgs/cpoc/datalifecycle/worker/Application.java
@@ -0,0 +1,21 @@
+package esa.s1pdgs.cpoc.datalifecycle.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@ComponentScan("esa.s1pdgs.cpoc")
+public class Application {
+
+    /**
+     * Main application
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
