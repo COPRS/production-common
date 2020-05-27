d9dc91fcb601fcb0ee9a1dd68443f3c0cd9f54ce
Anonymous <anon@anon.com>
2020-05-27 12:04:32 +0200

    formatting and removed code duplication

--- data-lifecycle-trigger/src/main/java/esa/s1pdgs/cpoc/datalifecycle/trigger/service/DataLifecycleTriggerService.java
+++ data-lifecycle-trigger/src/main/java/esa/s1pdgs/cpoc/datalifecycle/trigger/service/DataLifecycleTriggerService.java
@@ -1,12 +1,12 @@
 package esa.s1pdgs.cpoc.datalifecycle.trigger.service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,16 +18,17 @@ import esa.s1pdgs.cpoc.datalifecycle.trigger.config.ProcessConfiguration;
 import esa.s1pdgs.cpoc.errorrepo.ErrorRepoAppender;
 import esa.s1pdgs.cpoc.mqi.client.MqiClient;
 import esa.s1pdgs.cpoc.mqi.client.MqiConsumer;
-import esa.s1pdgs.cpoc.mqi.model.queue.CompressionEvent;
-import esa.s1pdgs.cpoc.mqi.model.queue.IngestionEvent;
-import esa.s1pdgs.cpoc.mqi.model.queue.LtaDownloadEvent;
-import esa.s1pdgs.cpoc.mqi.model.queue.ProductionEvent;
+import esa.s1pdgs.cpoc.mqi.model.queue.AbstractMessage;
 
 @Service
 public class DataLifecycleTriggerService {
-
-	private static final Logger LOG = LogManager.getLogger(DataLifecycleTriggerService.class);
-
+	private static final List<ProductCategory> CATEGORIES = Arrays.asList(
+			ProductCategory.INGESTION_EVENT,
+			ProductCategory.PRODUCTION_EVENT,
+			ProductCategory.COMPRESSED_PRODUCTS,
+			ProductCategory.LTA_DOWNLOAD_EVENT
+	);
+	
 	private final DataLifecycleTriggerConfigurationProperties configurationProperties;
 	private final MqiClient mqiClient;
 	private final AppStatus appStatus;
@@ -35,60 +36,44 @@ public class DataLifecycleTriggerService {
 	private final ProcessConfiguration processConfig;
 
 	@Autowired
-	public DataLifecycleTriggerService(final DataLifecycleTriggerConfigurationProperties configurationProperties,
-			final MqiClient mqiClient, final AppStatus appStatus, final ErrorRepoAppender errorRepoAppender,
-			final ProcessConfiguration processConfig) {
+	public DataLifecycleTriggerService(
+			final DataLifecycleTriggerConfigurationProperties configurationProperties,
+			final MqiClient mqiClient, 
+			final AppStatus appStatus, 
+			final ErrorRepoAppender errorRepoAppender,
+			final ProcessConfiguration processConfig
+	) {
 		this.configurationProperties = configurationProperties;
 		this.mqiClient = mqiClient;
 		this.appStatus = appStatus;
 		this.errorRepoAppender = errorRepoAppender;
 		this.processConfig = processConfig;
 	}
-
+	
 	@PostConstruct
-	public void initService() {
-
-		CategoryConfig ingestionEventCategoryConfig = configurationProperties.getProductCategories()
-				.get(ProductCategory.INGESTION_EVENT);
-		MqiConsumer<IngestionEvent> ingestionEventConsumer = new MqiConsumer<IngestionEvent>(mqiClient,
-				ProductCategory.INGESTION_EVENT,
-				new DataLifecycleTriggerListener<>(mqiClient, errorRepoAppender, processConfig,
-						configurationProperties.getRetentionPolicies()),
-				ingestionEventCategoryConfig.getFixedDelayMs(), ingestionEventCategoryConfig.getInitDelayPolMs(),
-				appStatus);
-
-		CategoryConfig productionEventCategoryConfig = configurationProperties.getProductCategories()
-				.get(ProductCategory.PRODUCTION_EVENT);
-		MqiConsumer<ProductionEvent> productionEventConsumer = new MqiConsumer<ProductionEvent>(mqiClient,
-				ProductCategory.PRODUCTION_EVENT,
-				new DataLifecycleTriggerListener<>(mqiClient, errorRepoAppender, processConfig,
-						configurationProperties.getRetentionPolicies()),
-				productionEventCategoryConfig.getFixedDelayMs(), productionEventCategoryConfig.getInitDelayPolMs(),
-				appStatus);
-
-		CategoryConfig compressionEventCategoryConfig = configurationProperties.getProductCategories()
-				.get(ProductCategory.COMPRESSED_PRODUCTS);
-		MqiConsumer<CompressionEvent> compressionEventConsumer = new MqiConsumer<CompressionEvent>(mqiClient,
-				ProductCategory.COMPRESSED_PRODUCTS,
-				new DataLifecycleTriggerListener<>(mqiClient, errorRepoAppender, processConfig,
-						configurationProperties.getRetentionPolicies()),
-				compressionEventCategoryConfig.getFixedDelayMs(), compressionEventCategoryConfig.getInitDelayPolMs(),
-				appStatus);
-
-		CategoryConfig ltaDownloadEventCategoryConfig = configurationProperties.getProductCategories()
-				.get(ProductCategory.LTA_DOWNLOAD_EVENT);
-		MqiConsumer<LtaDownloadEvent> ltaDownloadEventConsumer = new MqiConsumer<LtaDownloadEvent>(mqiClient,
-				ProductCategory.LTA_DOWNLOAD_EVENT,
-				new DataLifecycleTriggerListener<>(mqiClient, errorRepoAppender, processConfig,
-						configurationProperties.getRetentionPolicies()),
-				ltaDownloadEventCategoryConfig.getFixedDelayMs(), ltaDownloadEventCategoryConfig.getInitDelayPolMs(),
-				appStatus);
-
-		final ExecutorService executorService = Executors.newFixedThreadPool(4);
-		executorService.execute(ingestionEventConsumer);
-		executorService.execute(productionEventConsumer);
-		executorService.execute(compressionEventConsumer);
-		executorService.execute(ltaDownloadEventConsumer);
+	public void initService() {		
+		final ExecutorService executorService = Executors.newFixedThreadPool(CATEGORIES.size());
+		
+		for (final ProductCategory cat : CATEGORIES) {
+			executorService.execute(newConsumerFor(cat));
+		}
 	}
 
+	private final <E extends AbstractMessage> MqiConsumer<E> newConsumerFor(final ProductCategory cat) {
+		final CategoryConfig conf = configurationProperties.getProductCategories().get(cat);
+		final DataLifecycleTriggerListener<E> listener = new DataLifecycleTriggerListener<>(
+				mqiClient, 
+				errorRepoAppender, 
+				processConfig,
+				configurationProperties.getRetentionPolicies()
+		);
+		return new MqiConsumer<E>(
+				mqiClient,
+				cat,
+				listener,
+				conf.getFixedDelayMs(), 
+				conf.getInitDelayPolMs(),
+				appStatus
+		);
+	}
 }
