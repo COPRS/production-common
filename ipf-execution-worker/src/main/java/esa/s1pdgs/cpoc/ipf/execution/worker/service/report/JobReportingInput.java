78355f6b05c8734aed84dc23943d2de500a24a49
Anonymous <anon@anon.com>
2020-06-08 21:43:53 +0200

    S1PRO-1491: fixed handling of segment inputs

--- ipf-execution-worker/src/main/java/esa/s1pdgs/cpoc/ipf/execution/worker/service/report/JobReportingInput.java
+++ ipf-execution-worker/src/main/java/esa/s1pdgs/cpoc/ipf/execution/worker/service/report/JobReportingInput.java
@@ -1,13 +1,10 @@
 package esa.s1pdgs.cpoc.ipf.execution.worker.service.report;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import esa.s1pdgs.cpoc.common.ApplicationLevel;
-import esa.s1pdgs.cpoc.common.ProductFamily;
 import esa.s1pdgs.cpoc.report.ReportingFilenameEntries;
 import esa.s1pdgs.cpoc.report.ReportingFilenameEntry;
 import esa.s1pdgs.cpoc.report.ReportingInput;
@@ -21,26 +18,35 @@ public class JobReportingInput extends AbstractFilenameReportingProduct implemen
 		super(entries);
 		this.jobOrderUuid = jobOrderUuid;
 	}
+//	
+//	@JsonIgnore
+//	public static final JobReportingInput newInstance(
+//			final List<String> filenames,
+//			final String jobOrderUuid, 
+//			final ApplicationLevel level
+//	) {		
+//		// we just care about the L0_SEGMENT scenario. Everything else is just reported as filename,
+//		// so we set the family here to BLANK to avoid confusion of declaring it L0_SLICE for L1 or L2
+//		// productions
+//		final ProductFamily family = (level == ApplicationLevel.L0_SEGMENT) ?
+//				ProductFamily.L0_SEGMENT :
+//				ProductFamily.BLANK;
+//		
+//		final List<ReportingFilenameEntry> reportingEntries = filenames.stream()
+//				.map(name -> new ReportingFilenameEntry(family, name))
+//				.collect(Collectors.toList());
+//		
+//		return new JobReportingInput(new ReportingFilenameEntries(reportingEntries), jobOrderUuid);
+//	}
 	
 	@JsonIgnore
 	public static final JobReportingInput newInstance(
-			final List<String> filenames,
-			final String jobOrderUuid, 
-			final ApplicationLevel level
+			final List<ReportingFilenameEntry> entries,
+			final String jobOrderUuid
 	) {		
-		// we just care about the L0_SEGMENT scenario. Everything else is just reported as filename,
-		// so we set the family here to BLANK to avoid confusion of declaring it L0_SLICE for L1 or L2
-		// productions
-		final ProductFamily family = (level == ApplicationLevel.L0_SEGMENT) ?
-				ProductFamily.L0_SEGMENT :
-				ProductFamily.BLANK;
-		
-		final List<ReportingFilenameEntry> reportingEntries = filenames.stream()
-				.map(name -> new ReportingFilenameEntry(family, name))
-				.collect(Collectors.toList());
-		
-		return new JobReportingInput(new ReportingFilenameEntries(reportingEntries), jobOrderUuid);
+		return new JobReportingInput(new ReportingFilenameEntries(entries), jobOrderUuid);
 	}
+	
 
 	public String getJobOrderUuid() {
 		return jobOrderUuid;
