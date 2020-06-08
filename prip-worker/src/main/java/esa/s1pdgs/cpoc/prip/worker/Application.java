8997be2e83909042ee2a36b0c505232720aefca2
Anonymous <anon@anon.com>
2020-06-08 12:14:22 +0200

    enabling configuration properties

--- prip-worker/src/main/java/esa/s1pdgs/cpoc/prip/worker/Application.java
+++ prip-worker/src/main/java/esa/s1pdgs/cpoc/prip/worker/Application.java
@@ -2,9 +2,11 @@ package esa.s1pdgs.cpoc.prip.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
     public static void main(String[] args) {
