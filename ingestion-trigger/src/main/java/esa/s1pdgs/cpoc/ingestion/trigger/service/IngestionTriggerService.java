3e95e9c45d0f41714f329f22ab4e56b5120b26ba
Anonymous <anon@anon.com>
2020-06-15 16:39:05 +0200

    S1PRO-1527: Added liveliness probe check for successful polling

--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/service/IngestionTriggerService.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/service/IngestionTriggerService.java
@@ -1,30 +1,38 @@
 package esa.s1pdgs.cpoc.ingestion.trigger.service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import esa.s1pdgs.cpoc.appstatus.AppStatus;
 import esa.s1pdgs.cpoc.ingestion.trigger.inbox.Inbox;
 
 public final class IngestionTriggerService {		
 	private static final Logger LOG = LoggerFactory.getLogger(IngestionTriggerService.class);
 	
 	private final List<Inbox> inboxes;
-	
-	public IngestionTriggerService(final List<Inbox> inboxes) {
-		this.inboxes = inboxes;
+	private final AppStatus status;
+	private final AtomicLong counter = new AtomicLong(1L);
+		
+	public IngestionTriggerService(final List<Inbox> inboxes, final AppStatus status) {
+		this.inboxes 	= inboxes;
+		this.status		= status;
 	}
 	 
 	public final void pollAll() {
     	LOG.trace("Polling all");
+
     	for (final Inbox inbox : inboxes) {
         	LOG.debug("Polling {}", inbox.description());   
-        	try {
+        	status.setProcessing(counter.incrementAndGet());
+        	try {        	
 				inbox.poll();
 			} catch (final Exception e) {
-				LOG.error(String.format("Failed polling %s", inbox), e);
+				LOG.error(String.format("Failed polling %s", inbox), e);			
 			}
+        	status.setWaiting();        
     	}
       	LOG.trace("Done polling all");
 	}	
