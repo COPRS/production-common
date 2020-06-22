36cbaaf3343caf1d8e74276ce1650cea15f55b42
Anonymous <anon@anon.com>
2020-06-22 15:22:27 +0200

    S1PRO-1524: added error handling and success evaluation on error request resubmission

--- request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/kafka/producer/KafkaSubmissionClient.java
+++ request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/kafka/producer/KafkaSubmissionClient.java
@@ -1,20 +1,40 @@
 package esa.s1pdgs.cpoc.reqrepo.kafka.producer;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import esa.s1pdgs.cpoc.appcatalog.common.FailedProcessing;
+import esa.s1pdgs.cpoc.common.utils.Exceptions;
 
 public class KafkaSubmissionClient implements SubmissionClient {
 	
 	private final KafkaTemplate<String, Object> client;
 	
-	public KafkaSubmissionClient(KafkaTemplate<String, Object> client) {
+	public KafkaSubmissionClient(final KafkaTemplate<String, Object> client) {
 		this.client = client;
 	}
 
 	@Override
-	public void resubmit(final FailedProcessing failedProcessing, final Object message) {    
-        client.send(failedProcessing.getTopic(), message);
+	public void resubmit(final FailedProcessing failedProcessing, final Object message) {    		
+		final ListenableFuture<SendResult<String, Object>> result = client.send(
+				failedProcessing.getTopic(), 
+				message
+		);
+		try {
+			result.get();
+		} catch (final Exception e) {
+			final Throwable cause = Exceptions.unwrap(e);
+			throw new RuntimeException(
+					String.format(
+							"Error restarting failedRequest '%s' on topic '%s': %s",
+							failedProcessing.getId(),
+							failedProcessing.getTopic(), 
+							Exceptions.messageOf(cause)
+					),
+					cause
+			);
+		}		
 	}
 
 }
