e092637b443400e1fa7d6dd9d6e70f285315af68
Anonymous <anon@anon.com>
2020-06-22 15:06:14 +0200

    S1PRO-1524: Moved errorRepoAppender to a separate module to reduce Configuration class duplication

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/common/utils/LogUtils.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/common/utils/LogUtils.java
@@ -1,23 +1,16 @@
 package esa.s1pdgs.cpoc.common.utils;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 import org.apache.commons.logging.Log;
 
 public class LogUtils {
 
-    public static void traceLog(Log logger, String message) {
+    public static void traceLog(final Log logger, final String message) {
         if (logger.isTraceEnabled()) {
             logger.trace(message);
         }
     }
     
     public static final String toString(final Throwable throwable) {
-        final StringWriter writer     = new StringWriter();        
-        try (final PrintWriter printWriter = new PrintWriter(writer)){
-        	throwable.printStackTrace(printWriter);
-        }
-        return writer.toString();
+    	return Exceptions.toString(throwable);
     }
 }
