d605b217aeae676a9c4e707a6e055ba97d3d3954
Anonymous <anon@anon.com>
2020-06-22 15:06:14 +0200

    S1PRO-1524: Moved errorRepoAppender to a separate module to reduce Configuration class duplication

---
+++ error-repo-client/src/main/java/esa/s1pdgs/cpoc/errorrepo/KafkaErrorRepoAppender.java
@@ -0,0 +1,40 @@
+package esa.s1pdgs.cpoc.errorrepo;
+
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessingDto;
+
+public class KafkaErrorRepoAppender implements ErrorRepoAppender {
+	
+	private final KafkaTemplate<String, FailedProcessingDto> kafkaTemplate;
+	private final String topic;
+
+	public KafkaErrorRepoAppender(final KafkaTemplate<String, FailedProcessingDto> kafkaTemplate, final String topic) {
+		this.kafkaTemplate = kafkaTemplate;
+		this.topic = topic;
+	}
+
+	@Override
+	public void send(final FailedProcessingDto errorRequest) {	
+		try {
+			sendAndWait(errorRequest);
+		} catch (final Exception e) {
+			throw new RuntimeException(
+					""
+					
+			);
+		}
+	}
+	
+	private final void sendAndWait(final FailedProcessingDto errorRequest) throws InterruptedException, ExecutionException {
+		final ListenableFuture<SendResult<String, FailedProcessingDto>> result = kafkaTemplate.send(
+				topic, 
+				errorRequest
+		);
+		result.get();
+	}
+}
