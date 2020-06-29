a16cc31a4bfff3276221a4aaed6e1a00aed33393
Anonymous <anon@anon.com>
2020-06-29 14:08:01 +0200

    trying to log any exceptions from ProductDistributionController

--- mqi-server/src/main/java/esa/s1pdgs/cpoc/mqi/server/rest/ProductDistributionControllerAdvice.java
+++ mqi-server/src/main/java/esa/s1pdgs/cpoc/mqi/server/rest/ProductDistributionControllerAdvice.java
@@ -2,6 +2,7 @@ package esa.s1pdgs.cpoc.mqi.server.rest;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +17,14 @@ public class ProductDistributionControllerAdvice extends ResponseEntityException
     @ExceptionHandler(ProductDistributionException.class)
     @ResponseBody
 	ResponseEntity<?> handleControllerException(final HttpServletRequest request, final Throwable e) {
-    	ProductDistributionController.LOGGER.error(e);
+		ProductDistributionController.LOGGER.error("could not handle request {}", request.getRequestURI(), e);
 		return new ResponseEntity<Void>(((ProductDistributionException) e).getStatus());
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseBody
+	public ResponseEntity<?> handle(final HttpServletRequest request, RuntimeException e) {
+		ProductDistributionController.LOGGER.error("could not handle request {}", request.getRequestURI(), e);
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
