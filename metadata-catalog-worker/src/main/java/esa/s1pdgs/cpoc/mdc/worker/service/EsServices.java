92775846e17f3a78131bea3a72009e3fd294ca46
Anonymous <anon@anon.com>
2020-07-04 18:40:13 +0200

    751: trying to fix session query

--- metadata-catalog-worker/src/main/java/esa/s1pdgs/cpoc/mdc/worker/service/EsServices.java
+++ metadata-catalog-worker/src/main/java/esa/s1pdgs/cpoc/mdc/worker/service/EsServices.java
@@ -729,8 +729,7 @@ public class EsServices {
 	public final List<EdrsSessionMetadata> getEdrsSessionsFor(final String sessionId) throws Exception {
 		final SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 		// Generic fields
-		final BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
-				.must(QueryBuilders.termQuery("sessionId", sessionId));
+		final QueryBuilder queryBuilder = QueryBuilders.termQuery("sessionId", sessionId);
 		sourceBuilder.query(queryBuilder);
 		LOGGER.debug("EdrsSessionQuery: query composed is {}", queryBuilder);
 		sourceBuilder.size(SIZE_LIMIT);
