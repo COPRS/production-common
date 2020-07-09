fae88a2119dfdb1096fbcc94f3fcb3576fdabf1c
Anonymous <anon@anon.com>
2020-07-09 15:09:31 +0200

    S1PRO-1376: some test cleanup

--- mqi-client/src/test/java/esa/s1pdgs/cpoc/mqi/client/MqiMessageFilterTest.java
+++ mqi-client/src/test/java/esa/s1pdgs/cpoc/mqi/client/MqiMessageFilterTest.java
@@ -1,217 +1,97 @@
 package esa.s1pdgs.cpoc.mqi.client;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import esa.s1pdgs.cpoc.common.ProductFamily;
 import esa.s1pdgs.cpoc.mqi.model.queue.ProductionEvent;
 
 public class MqiMessageFilterTest {
-
 	@Test
-	public void noFamily() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setAllowRegex("a");
-		f.setDisallowRegex("d");
-
-		ProductionEvent pe = new ProductionEvent();
-		pe.setKeyObjectStorage("d");
-		pe.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertTrue(f.accept(pe));
-	}
-
-	@Test
-	public void wrongFamily() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.AUXILIARY_FILE);
-		f.setAllowRegex("a");
-		f.setDisallowRegex("d");
-
-		ProductionEvent pe = new ProductionEvent();
-		pe.setKeyObjectStorage("d");
-		pe.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertTrue(f.accept(pe));
-	}
-
-	@Test
-	public void rightFamily() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.AUXILIARY_FILE);
-		f.setAllowRegex("a");
-		f.setDisallowRegex("d");
-
-		ProductionEvent pe = new ProductionEvent();
-		pe.setKeyObjectStorage("d");
-		pe.setProductFamily(ProductFamily.AUXILIARY_FILE);
-
-		Assert.assertFalse(f.accept(pe));
+	public void familyNotDefined_ShallAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter(null, null, null);
+		final ProductionEvent event = newProductionEvent(null, ProductFamily.PLAN_AND_REPORT);
+		assertTrue(uut.accept(event));
 	}
 	
 	@Test
-	public void allowRegex_1() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-		f.setAllowRegex("^S1[ABCD]_OPER_REP__SUP___.*$");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("notallowed");
-		pe1.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_OPER_REP__SUP___1234");
-		pe2.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertTrue(f.accept(pe2));
-	}
-
-	@Test
-	public void disallowRegex_1() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-		f.setDisallowRegex("^((?!^S1[ABCD]_OPER_REP__SUP___.*$).)*$");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("notallowed");
-		pe1.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_OPER_REP__SUP___1234");
-		pe2.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertTrue(f.accept(pe2));
+	public void differentFamily_ShallAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter(null, null, ProductFamily.AUXILIARY_FILE);
+		final ProductionEvent event = newProductionEvent(null, ProductFamily.PLAN_AND_REPORT);
+		assertTrue(uut.accept(event));
 	}
 	
 	@Test
-	public void allowRegex_2() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-		f.setAllowRegex("^S1[ABCD_]_OPER_REP_MP_MP__PDMC.*$");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("notallowed");
-		pe1.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_OPER_REP_MP_MP__PDMC1234");
-		pe2.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertTrue(f.accept(pe2));
-	}
-
-	@Test
-	public void disallowRegex_2() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-		f.setDisallowRegex("^((?!(^S1[ABCD_]_OPER_REP_MP_MP__PDMC.*$)).)*$");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("notallowed");
-		pe1.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_OPER_REP_MP_MP__PDMC1234");
-		pe2.setProductFamily(ProductFamily.PLAN_AND_REPORT);
-
-		Assert.assertTrue(f.accept(pe2));
+	public void sameFamilyAndNoRegex_ShallAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter(null, null, ProductFamily.AUXILIARY_FILE);
+		final ProductionEvent event = newProductionEvent(null, ProductFamily.AUXILIARY_FILE);
+		assertTrue(uut.accept(event));
 	}
 	
 	@Test
-	public void allowRegex_3() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.L0_SEGMENT);
-		f.setAllowRegex("S1._(GP|HK|RF).*_RAW.*\\.SAFE");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("notallowed");
-		pe1.setProductFamily(ProductFamily.L0_SEGMENT);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_GP_RAW.SAFE");
-		pe2.setProductFamily(ProductFamily.L0_SEGMENT);
-
-		Assert.assertTrue(f.accept(pe2));
-	}
-
-	@Test
-	public void disallowRegex_3() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.L0_SEGMENT);
-		f.setDisallowRegex("S1._(?!GP|HK|RF).*_RAW.*\\.SAFE");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("S1A_XX_RAW.SAFE");
-		pe1.setProductFamily(ProductFamily.L0_SEGMENT);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_GP_RAW.SAFE");
-		pe2.setProductFamily(ProductFamily.L0_SEGMENT);
-
-		Assert.assertTrue(f.accept(pe2));
+	public void matchingAllowRegex_ShallAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter("^S1[ABCD]_OPER_REP__SUP___.*$", null, ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("S1A_OPER_REP__SUP___1234", ProductFamily.PLAN_AND_REPORT);
+		assertTrue(uut.accept(event));
 	}
 	
 	@Test
-	public void allowRegex_4() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.L0_SLICE);
-		f.setAllowRegex("^S1[A-D]_(IW|WV|EW|S[1-6]).*_RAW__0S.*");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("notallowed");
-		pe1.setProductFamily(ProductFamily.L0_SLICE);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_WV_RAW__0S123");
-		pe2.setProductFamily(ProductFamily.L0_SLICE);
-
-		Assert.assertTrue(f.accept(pe2));
+	public void notMatchingAllowRegex_ShallNotAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter("^S1[ABCD]_OPER_REP__SUP___.*$", null, ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("notallowed", ProductFamily.PLAN_AND_REPORT);
+		assertFalse(uut.accept(event));
 	}
-
+	
 	@Test
-	public void disallowRegex_4() {
-
-		MqiMessageFilter f = new MqiMessageFilter();
-		f.setProductFamily(ProductFamily.L0_SLICE);
-		f.setDisallowRegex("^S1[A-D]_(?!IW|WV|EW|S[1-6]).*_RAW__0S.*");
-
-		ProductionEvent pe1 = new ProductionEvent();
-		pe1.setKeyObjectStorage("S1A_XX_RAW__0S");
-		pe1.setProductFamily(ProductFamily.L0_SLICE);
-
-		Assert.assertFalse(f.accept(pe1));
-
-		ProductionEvent pe2 = new ProductionEvent();
-		pe2.setKeyObjectStorage("S1A_IW_RAW__0S");
-		pe2.setProductFamily(ProductFamily.L0_SLICE);
-
-		Assert.assertTrue(f.accept(pe2));
+	public void matchingDisallowRegex_ShallNotAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter(null, "^S1[ABCD]_OPER_REP__SUP___.*$", ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("S1A_OPER_REP__SUP___1234", ProductFamily.PLAN_AND_REPORT);
+		assertFalse(uut.accept(event));
 	}
 	
+	@Test
+	public void notMatchingDisallowRegex_ShallAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter(null, "^S1[ABCD]_OPER_REP__SUP___.*$", ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("accepted", ProductFamily.PLAN_AND_REPORT);
+		assertTrue(uut.accept(event));
+	}
 	
+	@Test
+	public void notMatchingDisallowRegexAndMatchingAllowRegex_ShallAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter("accepted", "^S1[ABCD]_OPER_REP__SUP___.*$", ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("accepted", ProductFamily.PLAN_AND_REPORT);
+		assertTrue(uut.accept(event));
+	}
+	
+	@Test
+	public void matchingDisallowRegexAndNonMatchingAllowRegex_ShallNotAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter("accepted", "^S1[ABCD]_OPER_REP__SUP___.*$", ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("S1A_OPER_REP__SUP___1234", ProductFamily.PLAN_AND_REPORT);
+		assertFalse(uut.accept(event));
+	}
+	
+	@Test
+	public void matchingDisallowRegexAndMatchingAllowRegex_ShallNotAccept() {
+		final MqiMessageFilter uut = newMqiMessageFilter("^S1[ABCD]_OPER_REP__SUP___.*$", "^S1[ABCD]_OPER_REP__SUP___.*$", ProductFamily.PLAN_AND_REPORT);
+		final ProductionEvent event = newProductionEvent("S1A_OPER_REP__SUP___1234", ProductFamily.PLAN_AND_REPORT);
+		assertFalse(uut.accept(event));
+	}
+	
+	private MqiMessageFilter newMqiMessageFilter(final String allow, final String disallow, final ProductFamily family) {
+		final MqiMessageFilter f = new MqiMessageFilter();
+		f.setProductFamily(family);
+		f.setAllowRegex(allow);
+		f.setDisallowRegex(disallow);
+		return f;
+	}
+	
+	private final ProductionEvent newProductionEvent(final String name, final ProductFamily family) {
+		final ProductionEvent pe = new ProductionEvent();
+		pe.setKeyObjectStorage(name);
+		pe.setProductFamily(family);
+		return pe;
+	}
 	
 }
