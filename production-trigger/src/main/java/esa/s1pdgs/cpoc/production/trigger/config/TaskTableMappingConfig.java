d101d3ab327f2fdaeaee5a0c32773fcb3c502c99
Anonymous <anon@anon.com>
2020-07-21 22:03:43 +0200

    940: fixed prop provision optionality

--- production-trigger/src/main/java/esa/s1pdgs/cpoc/production/trigger/config/TaskTableMappingConfig.java
+++ production-trigger/src/main/java/esa/s1pdgs/cpoc/production/trigger/config/TaskTableMappingConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import esa.s1pdgs.cpoc.production.trigger.taskTableMapping.ConfigurableKeyEvaluator;
 import esa.s1pdgs.cpoc.production.trigger.taskTableMapping.RoutingBasedTasktableMapper;
@@ -13,13 +14,13 @@ import esa.s1pdgs.cpoc.xml.XmlConverter;
 
 @Configuration
 public class TaskTableMappingConfig {		
-	@Value("${tasktable.pathroutingxmlfile}")
+	@Value("${tasktable.pathroutingxmlfile:}")
 	private String routingFile = null;
 	
-	@Value("${tasktable.name}")	
+	@Value("${tasktable.name:}")	
 	private String name = null;
 	
-	@Value("${tasktable.routingKeyTemplate}")	
+	@Value("${tasktable.routingKeyTemplate:}")	
 	private String routingKeyTemplate = "${product.swathtype}_${product.satelliteId}";
 		
 	@Bean
@@ -27,10 +28,11 @@ public class TaskTableMappingConfig {
 	public TasktableMapper newTastTableMapper(
 			final XmlConverter xmlConverter
 	) {
-		if (name != null) {
+		
+		if (!StringUtils.isEmpty(name)) {
 			return new SingleTasktableMapper(name);
 		}
-		if (routingFile != null) {			
+		if (!StringUtils.isEmpty(routingFile)) {			
 			return new RoutingBasedTasktableMapper
 					.Factory(xmlConverter, routingFile, new ConfigurableKeyEvaluator(routingKeyTemplate))
 					.newMapper();
