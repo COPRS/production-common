7ab68379b803c91e207f1e4514ca794be8879ab4
Anonymous <anon@anon.com>
2020-08-05 20:11:14 +0200

    S1PRO-1746: Ok, apparently mongo doesn't support polymorphism at this point so keeping required metadata in a map

--- ipf-preparation-worker/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/worker/type/segment/L0SegmentProduct.java
+++ ipf-preparation-worker/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/worker/type/segment/L0SegmentProduct.java
@@ -3,18 +3,22 @@ package esa.s1pdgs.cpoc.ipf.preparation.worker.type.segment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import esa.s1pdgs.cpoc.appcatalog.AppDataJob;
 import esa.s1pdgs.cpoc.appcatalog.AppDataJobFile;
 import esa.s1pdgs.cpoc.appcatalog.AppDataJobProduct;
-import esa.s1pdgs.cpoc.appcatalog.AppDataJobSegmentFile;
 import esa.s1pdgs.cpoc.appcatalog.util.AppDataJobProductAdapter;
 import esa.s1pdgs.cpoc.ipf.preparation.worker.type.AbstractProduct;
 import esa.s1pdgs.cpoc.metadata.model.LevelSegmentMetadata;
 
 public class L0SegmentProduct extends AbstractProduct {	
+	private static final String DATATAKE_ID = "datatakeId";
+	private static final String PRODUCT_SENSING_CONSOLIDATION = "productSensingConsolidation";
+	private static final String CONSOLIDATION = "consolidation";
+	
 	public static final List<String> POLARISTATIONS = Arrays.asList("SH","SV","VH","VV","HV","HH");
 
 	public L0SegmentProduct(final AppDataJobProductAdapter product) {
@@ -50,12 +54,12 @@ public class L0SegmentProduct extends AbstractProduct {
 	public final void addSegmentMetadata(final LevelSegmentMetadata metadata) {
 		final List<AppDataJobFile> res = product.getProductsFor(metadata.getPolarisation());
 		
-		final AppDataJobSegmentFile segment = new AppDataJobSegmentFile(
+		final AppDataJobFile segment = new AppDataJobFile(
 				metadata.getProductName(), 
 				metadata.getKeyObjectStorage(), 
 				metadata.getValidityStart(),
 				metadata.getValidityStop(),
-				metadata
+				toMetadataMap(metadata)
 		);
 		if (!res.contains(segment)) {			
 			res.add(segment);
@@ -68,12 +72,32 @@ public class L0SegmentProduct extends AbstractProduct {
 		
 		for (final String polarisation : POLARISTATIONS) {	
 			final List<LevelSegmentMetadata> meta = new ArrayList<>();
-			for (final AppDataJobFile file : product.getProductsFor(polarisation)) {
-				final AppDataJobSegmentFile segmentFile = (AppDataJobSegmentFile) file;
-				meta.add(segmentFile.getMetadata());				
+			for (final AppDataJobFile file : product.getProductsFor(polarisation)) {		
+				meta.add(toMetadataObject(file, polarisation));				
 			}
 			result.put(polarisation, meta);
 		}
 		return result;	
+	}
+	
+	private final Map<String,String> toMetadataMap(final LevelSegmentMetadata metadata) {
+		final Map<String,String> result = new LinkedHashMap<>();
+		result.put(DATATAKE_ID, metadata.getDatatakeId());
+		result.put(CONSOLIDATION, metadata.getConsolidation());
+		result.put(PRODUCT_SENSING_CONSOLIDATION, metadata.getProductSensingConsolidation());
+		return result;
+	}
+	
+	private final LevelSegmentMetadata toMetadataObject(final AppDataJobFile file, final String polarisation) {
+		final LevelSegmentMetadata meta = new LevelSegmentMetadata();
+		meta.setDatatakeId(file.getMetadata().get(DATATAKE_ID));
+		meta.setConsolidation(file.getMetadata().get(CONSOLIDATION));
+		meta.setProductSensingConsolidation(file.getMetadata().get(PRODUCT_SENSING_CONSOLIDATION));
+		meta.setKeyObjectStorage(file.getKeyObs());
+		meta.setPolarisation(polarisation);
+		meta.setProductName(file.getFilename());
+		meta.setValidityStart(file.getStartDate());
+		meta.setValidityStop(file.getEndDate());
+		return meta;	
 	}
 }
