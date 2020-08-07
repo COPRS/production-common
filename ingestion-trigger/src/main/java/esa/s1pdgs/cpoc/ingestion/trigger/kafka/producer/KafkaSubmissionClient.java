ee77dfe7964e1cf141aa0d6e9e227d21861f3889
Anonymous <anon@anon.com>
2020-08-07 12:37:49 +0200

    added evaluation of kakfaTemplate.send() result

--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/kafka/producer/KafkaSubmissionClient.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/kafka/producer/KafkaSubmissionClient.java
@@ -2,6 +2,7 @@ package esa.s1pdgs.cpoc.ingestion.trigger.kafka.producer;
 
 import org.springframework.kafka.core.KafkaTemplate;
 
+import esa.s1pdgs.cpoc.common.utils.Exceptions;
 import esa.s1pdgs.cpoc.mqi.model.queue.IngestionJob;
 
 public class KafkaSubmissionClient implements SubmissionClient {
@@ -9,14 +10,27 @@ public class KafkaSubmissionClient implements SubmissionClient {
 	private final KafkaTemplate<String, IngestionJob> client;
 	private final String topic;
 	
-	public KafkaSubmissionClient(KafkaTemplate<String, IngestionJob> client, String topic) {
+	public KafkaSubmissionClient(final KafkaTemplate<String, IngestionJob> client, final String topic) {
 		this.client = client;
 		this.topic = topic;
 	}
 
 	@Override
 	public void publish(final IngestionJob dto) {    
-        client.send(topic, dto);
+        try {
+			client.send(topic, dto).get();
+		} catch (final Exception e) {
+			final Throwable cause = Exceptions.unwrap(e);
+			throw new RuntimeException(
+					String.format(
+							"Error on publishing IngestionJob for %s to %s: %s", 
+							dto.getProductName(),
+							topic,
+							Exceptions.messageOf(cause)
+					),
+					cause
+			);
+		}
 	}
 
 }
