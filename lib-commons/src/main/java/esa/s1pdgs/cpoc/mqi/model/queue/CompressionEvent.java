270bde6718ceadf5647834f5f9aea47270890ec3
Anonymous <anon@anon.com>
2020-08-10 09:34:07 +0200

    s1pro-1676: set "compressionDirection" in CompressionEvent

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/mqi/model/queue/CompressionEvent.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/mqi/model/queue/CompressionEvent.java
@@ -5,17 +5,21 @@ import java.util.Objects;
 import esa.s1pdgs.cpoc.common.ProductFamily;
 
 public class CompressionEvent extends AbstractMessage {	
+	
+	private CompressionDirection compressionDirection = CompressionDirection.UNDEFINED;
+	
 	public CompressionEvent() {
 		super();
 	}
 	
-	public CompressionEvent(final ProductFamily productFamily, final String keyObjectStorage) {
+	public CompressionEvent(final ProductFamily productFamily, final String keyObjectStorage, final CompressionDirection compressionDirection) {
 		super(productFamily, keyObjectStorage);
+		this.compressionDirection = compressionDirection;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(creationDate, hostname, keyObjectStorage, productFamily, uid);
+		return Objects.hash(creationDate, hostname, keyObjectStorage, productFamily, uid, compressionDirection);
 	}
 
 	@Override
@@ -34,12 +38,14 @@ public class CompressionEvent extends AbstractMessage {
 				&& Objects.equals(hostname, other.hostname)
 				&& Objects.equals(keyObjectStorage, other.keyObjectStorage) 
 				&& Objects.equals(uid, other.uid)
-				&& productFamily == other.productFamily;
+				&& productFamily == other.productFamily
+		        && compressionDirection == other.compressionDirection;
 	}
 
 	@Override
 	public String toString() {
 		return "CompressionEvent [productFamily=" + productFamily + ", keyObjectStorage=" + keyObjectStorage
-				+ ", creationDate=" + creationDate + ", hostname=" + hostname + ", uid=" + uid +"]";
+				+ ", creationDate=" + creationDate + ", hostname=" + hostname + ", uid=" + uid + ", compressionDirection="
+						+ compressionDirection +"]";
 	}
 }
