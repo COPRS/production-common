3f860259a54d6acc192151b1c0db4dff3960b999
Anonymous <anon@anon.com>
2020-08-18 14:52:06 +0200

    S3: Extend S3TypeAdapter with connection to metadataClient and own settings

--- ipf-preparation-worker/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/worker/type/s3/S3Product.java
+++ ipf-preparation-worker/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/worker/type/s3/S3Product.java
@@ -1,22 +1,39 @@
 package esa.s1pdgs.cpoc.ipf.preparation.worker.type.s3;
 
+import java.util.List;
+
 import esa.s1pdgs.cpoc.appcatalog.AppDataJob;
 import esa.s1pdgs.cpoc.appcatalog.AppDataJobProduct;
+import esa.s1pdgs.cpoc.appcatalog.AppDataJobTaskInputs;
 import esa.s1pdgs.cpoc.appcatalog.util.AppDataJobProductAdapter;
 import esa.s1pdgs.cpoc.ipf.preparation.worker.type.AbstractProduct;
 
 public class S3Product extends AbstractProduct {
-
-	public S3Product(final AppDataJobProductAdapter product) {
-		super(product);
-	}
-
+	
+	/**
+	 * List of additional inputs, that were set during the main input search
+	 */
+	private List<AppDataJobTaskInputs> additionalInputs;
+	
 	public static final S3Product of(final AppDataJob job) {
 		return of(job.getProduct());
 	}
 
 	public static final S3Product of(final AppDataJobProduct product) {
 		return new S3Product(new AppDataJobProductAdapter(product));
+	}
+
+	public S3Product(final AppDataJobProductAdapter product) {
+		super(product);
+	}
+
+	public void setAdditionalInputs(List<AppDataJobTaskInputs> additionalInputs) {
+		this.additionalInputs = additionalInputs;
+	}
+	
+	@Override
+	public List<AppDataJobTaskInputs> overridingInputs() {
+		return additionalInputs;
 	}
 
 }
--- ipf-preparation-worker/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/worker/type/s3/S3TypeAdapter.java
+++ ipf-preparation-worker/src/main/java/esa/s1pdgs/cpoc/ipf/preparation/worker/type/s3/S3TypeAdapter.java
@@ -2,17 +2,35 @@ package esa.s1pdgs.cpoc.ipf.preparation.worker.type.s3;
 
 import esa.s1pdgs.cpoc.appcatalog.AppDataJob;
 import esa.s1pdgs.cpoc.common.errors.processing.IpfPrepWorkerInputsMissingException;
+import esa.s1pdgs.cpoc.ipf.preparation.worker.config.S3TypeAdapterSettings;
 import esa.s1pdgs.cpoc.ipf.preparation.worker.type.AbstractProductTypeAdapter;
 import esa.s1pdgs.cpoc.ipf.preparation.worker.type.Product;
 import esa.s1pdgs.cpoc.ipf.preparation.worker.type.ProductTypeAdapter;
+import esa.s1pdgs.cpoc.metadata.client.MetadataClient;
 import esa.s1pdgs.cpoc.mqi.model.queue.IpfExecutionJob;
 import esa.s1pdgs.cpoc.xml.model.joborder.JobOrder;
 
 public class S3TypeAdapter extends AbstractProductTypeAdapter implements ProductTypeAdapter {
 
+	private MetadataClient metadataClient;
+	
+	private S3TypeAdapterSettings settings;
+	
+	public S3TypeAdapter(
+			final MetadataClient metadataClient,
+			final S3TypeAdapterSettings settings
+	) {
+		this.metadataClient = metadataClient;
+		this.settings = settings;
+	}
+	
 	@Override
 	public Product mainInputSearch(AppDataJob job) throws IpfPrepWorkerInputsMissingException {
-		return S3Product.of(job);
+		S3Product returnValue = S3Product.of(job);
+		
+		// Umsetzung der MarginTTWFX 
+		
+		return returnValue;
 	}
 
 	@Override
