f482ed4643222bbcdb3cd8a76036a6b53f831209
Anonymous <anon@anon.com>
2020-09-16 18:47:11 +0200

    S1PRO-185: OpenAPI configuration

--- request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/config/OpenApiRequestValidatorConfiguration.java
+++ request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/config/OpenApiRequestValidatorConfiguration.java
@@ -7,17 +7,19 @@ import org.openapi4j.core.validation.ValidationException;
 import org.openapi4j.operation.validator.validation.RequestValidator;
 import org.openapi4j.parser.OpenApi3Parser;
 import org.openapi4j.parser.model.v3.OpenApi3;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiRequestValidatorConfiguration {
 
-	public final static String OPENAPI_SPEC_FILE_PATHNAME = "src/main/resources/s1pro-api_openapi.yaml";
+	@Value("${openapi.spec-file}")
+    private String openapiSpecFile;
 	
 	@Bean
 	public RequestValidator getRequestValidator() throws ResolutionException, ValidationException {
-		final OpenApi3 openApi = new OpenApi3Parser().parse(new File(OPENAPI_SPEC_FILE_PATHNAME), false);
+		final OpenApi3 openApi = new OpenApi3Parser().parse(new File(openapiSpecFile), false);
 		final RequestValidator requestValidator = new RequestValidator(openApi);
 		return requestValidator;
 	}
