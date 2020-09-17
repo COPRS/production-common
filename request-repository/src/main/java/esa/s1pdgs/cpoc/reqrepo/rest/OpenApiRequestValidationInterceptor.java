fbbdadf25f4262afb6381cd55451a02435fd8e0b
Anonymous <anon@anon.com>
2020-09-17 12:36:35 +0200

    S1PRO-185: Added disableValidation option

--- request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/rest/OpenApiRequestValidationInterceptor.java
+++ request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/rest/OpenApiRequestValidationInterceptor.java
@@ -11,6 +11,7 @@ import org.openapi4j.operation.validator.model.Request;
 import org.openapi4j.operation.validator.model.impl.RequestParameters;
 import org.openapi4j.operation.validator.validation.RequestValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -21,22 +22,32 @@ public class OpenApiRequestValidationInterceptor implements HandlerInterceptor {
 	static final Logger LOGGER = LogManager.getLogger(OpenApiRequestValidationInterceptor.class);
 
 	private final RequestValidator requestValidator;
+	private final boolean disableValidation;
 	
 	@Autowired
-	public OpenApiRequestValidationInterceptor(final RequestValidator requestValidator) {
+	public OpenApiRequestValidationInterceptor(
+		final RequestValidator requestValidator,
+		@Value("${openapi.disable-validation}:false") final boolean disableValidation)
+	{
 		this.requestValidator = requestValidator;
+		this.disableValidation = disableValidation;
 	}
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		Request servletRequest = ServletRequest.of(request);
-		try {
-			@SuppressWarnings("unused")
-			RequestParameters requestParameters = requestValidator.validate(servletRequest);
-		} catch (ValidationException e) {
-			LOGGER.debug(String.format("Check against OpenAPI specification failed. Invalid request: %s", request));
-	        response.sendError(HttpStatus.BAD_REQUEST.value());
-			return false;
+		if (disableValidation) {
+			LOGGER.debug("Skipping OpenAPI specification validation");
+		} else {			
+			Request servletRequest = ServletRequest.of(request);
+			try {
+				@SuppressWarnings("unused")
+				RequestParameters requestParameters = requestValidator.validate(servletRequest);
+				LOGGER.debug("Check against OpenAPI specification successful. Valid request: %s");
+			} catch (ValidationException e) {
+				LOGGER.debug(String.format("Check against OpenAPI specification failed. Invalid request: %s", request));
+		        response.sendError(HttpStatus.BAD_REQUEST.value());
+				return false;
+			}
 		}
 		return true;
 	}
