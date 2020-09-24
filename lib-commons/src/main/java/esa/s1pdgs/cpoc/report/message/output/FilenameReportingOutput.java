ec8821ac773fffe914338910d584beb2ff83e375
Anonymous <anon@anon.com>
2020-09-24 11:41:27 +0200

    S1PRO-1856: added trace reporting

--- lib-commons/src/main/java/esa/s1pdgs/cpoc/report/message/output/FilenameReportingOutput.java
+++ lib-commons/src/main/java/esa/s1pdgs/cpoc/report/message/output/FilenameReportingOutput.java
@@ -4,7 +4,7 @@ import esa.s1pdgs.cpoc.report.ReportingFilenameEntries;
 import esa.s1pdgs.cpoc.report.ReportingOutput;
 import esa.s1pdgs.cpoc.report.message.AbstractFilenameReportingProduct;
 
-public final class FilenameReportingOutput  extends AbstractFilenameReportingProduct implements ReportingOutput {
+public class FilenameReportingOutput  extends AbstractFilenameReportingProduct implements ReportingOutput {
 	public FilenameReportingOutput(final ReportingFilenameEntries entries) {
 		super(entries);
 	}
