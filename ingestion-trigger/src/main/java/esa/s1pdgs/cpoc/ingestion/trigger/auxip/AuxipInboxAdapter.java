c4a85ddb5e159a106a3568320e6803bf289bd7c0
Anonymous <anon@anon.com>
2020-10-12 18:11:35 +0200

    S1PRO-1874 replaced supplier with spliterator and added some tests

--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/auxip/AuxipInboxAdapter.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/auxip/AuxipInboxAdapter.java
@@ -13,9 +13,15 @@ import java.time.ZonedDateTime;
 import java.util.ArrayDeque;
 import java.util.Date;
 import java.util.Deque;
+import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import esa.s1pdgs.cpoc.auxip.client.AuxipClient;
 import esa.s1pdgs.cpoc.auxip.client.AuxipProductMetadata;
@@ -27,6 +33,8 @@ import esa.s1pdgs.cpoc.ingestion.trigger.inbox.AbstractInboxAdapter;
 import esa.s1pdgs.cpoc.ingestion.trigger.inbox.InboxEntryFactory;
 
 public class AuxipInboxAdapter extends AbstractInboxAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AuxipInboxAdapter.class);
 
     private final AuxipConfiguration configuration;
     private final ProcessConfiguration processConfiguration;
@@ -53,9 +61,18 @@ public class AuxipInboxAdapter extends AbstractInboxAdapter {
 
         TimeWindow timeWindow = timeWindowFrom(state);
 
-        return Stream.generate(
-                new AuxipMetadataSupplier(timeWindow, configuration.getMaxPageSize(), auxipClient))
-                .map(this::toEntrySupplier);
+        if(timeWindow.toCloseTo(LocalDateTime.now().minus(Duration.ofSeconds(configuration.getOffsetFromNowSec())))) {
+            LOG.info("time window is to close to now, skipping");
+            return Stream.empty();
+        }
+
+        Stream<AuxipProductMetadata> stream = StreamSupport.stream(
+                new AuxipMetadataSpliterator(timeWindow, auxipClient, configuration.getMaxPageSize(), inboxURL),
+                false);
+
+        //TODO filter
+
+        return stream.map(this::toEntrySupplier);
     }
 
     private EntrySupplier toEntrySupplier(AuxipProductMetadata metaData) {
@@ -66,7 +83,7 @@ public class AuxipInboxAdapter extends AbstractInboxAdapter {
     }
 
     private TimeWindow timeWindowFrom(AuxipState state) {
-        LocalDateTime start = LocalDateTime.from(state.getNextWindowStart().toInstant())
+        LocalDateTime start = LocalDateTime.ofInstant(state.getNextWindowStart().toInstant(), ZoneId.of("UTC"))
                 .minus(ofSeconds(configuration.getTimeWindowOverlapSec()));
         LocalDateTime stop = start.plus(Duration.ofSeconds(configuration.getTimeWindowSec()));
 
@@ -117,39 +134,79 @@ public class AuxipInboxAdapter extends AbstractInboxAdapter {
             this.start = start;
             this.stop = stop;
         }
-    }
 
-    private static class AuxipMetadataSupplier implements Supplier<AuxipProductMetadata> {
-
-        private final TimeWindow window;
-        private final int pageSize;
-        private final AuxipClient client;
-
-        private int offset = 0;
-        private final Deque<AuxipProductMetadata> collected = new ArrayDeque<>();
-
-        public AuxipMetadataSupplier(TimeWindow window, int pageSize, AuxipClient client) {
-            this.window = window;
-            this.pageSize = pageSize;
-            this.client = client;
+        public boolean toCloseTo(LocalDateTime barrier) {
+            return stop.isAfter(barrier);
         }
 
         @Override
-        public AuxipProductMetadata get() {
-            if (collected.isEmpty()) {
-                collect();
-            }
+        public String toString() {
+            return "start " + start + ", stop " + stop;
+        }
+    }
 
-            if (collected.isEmpty()) {
-                return null;
-            }
+    private static class AuxipMetadataSpliterator implements Spliterator<AuxipProductMetadata> {
 
-            return collected.poll();
+        private final TimeWindow window;
+        private final AuxipClient client;
+        private final int pageSize;
+        private final URI inboxUrl;
+
+        private int offset;
+        private final Deque<AuxipProductMetadata> collected = new ArrayDeque<>();
+
+        public AuxipMetadataSpliterator(TimeWindow window, AuxipClient client, int pageSize, URI inboxUrl) {
+            this.window = window;
+            this.client = client;
+            this.pageSize = pageSize;
+            this.inboxUrl = inboxUrl;
         }
 
-        private void collect() {
-            collected.addAll(client.getMetadata(window.start, window.stop, pageSize, offset));
+        @Override
+        public boolean tryAdvance(Consumer<? super AuxipProductMetadata> action) {
+
+            //query first to fill queue
+            if (collected.isEmpty()) {
+                collected.addAll(collect());
+            }
+
+            //if it`s still empty there will be no more entries
+            if (collected.isEmpty()) {
+                return false;
+            }
+
+            // otherwise provide an element
+            action.accept(collected.poll());
+
+            // if it was the last element, make a new query
+            if (collected.isEmpty()) {
+                collected.addAll(collect());
+            }
+
+            return !collected.isEmpty();
+        }
+
+        private List<AuxipProductMetadata> collect() {
+            LOG.info("query auxip {} with window {} pageSize {} offset {}", inboxUrl, window, pageSize, offset);
+
+            List<AuxipProductMetadata> metadata = client.getMetadata(window.start, window.stop, pageSize, offset);
             offset += pageSize;
+            return metadata;
+        }
+
+        @Override
+        public Spliterator<AuxipProductMetadata> trySplit() {
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return Long.MAX_VALUE;
+        }
+
+        @Override
+        public int characteristics() {
+            return Spliterator.DISTINCT | Spliterator.NONNULL | Spliterator.ORDERED;
         }
     }
 
