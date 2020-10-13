3fd5f96feec1e109e49c6f73d3f2704d51dfe06f
Anonymous <anon@anon.com>
2020-10-13 09:39:29 +0200

    fix: copy files with dot-prefix to local outbox and rename afterwards

--- disseminator/src/main/java/esa/s1pdgs/cpoc/disseminator/outbox/LocalOutboxClient.java
+++ disseminator/src/main/java/esa/s1pdgs/cpoc/disseminator/outbox/LocalOutboxClient.java
@@ -5,7 +5,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import org.apache.commons.io.IOUtils;
 
@@ -29,10 +31,10 @@ public final class LocalOutboxClient extends AbstractOutboxClient {
 
 	@Override
 	public final String transfer(final ObsObject obsObject, final ReportingFactory reportingFactory) throws Exception {		
-		final Path path = evaluatePathFor(obsObject);	
+		final Path path = evaluatePathFor(obsObject);
 		for (final String entry : entries(obsObject)) {
 			
-			final File destination = path.resolve(entry).toFile();
+			final File destination = path.resolve("." + entry).toFile();
 			createParentIfRequired(destination);
 			
 			try (final InputStream in = stream(obsObject.getFamily(), entry);
@@ -42,6 +44,8 @@ public final class LocalOutboxClient extends AbstractOutboxClient {
 				IOUtils.copyLarge(in, out, new byte[config.getBufferSize()]);    				
 			}
 		}
+		Files.move(path.resolve("." + obsObject.getKey()), path.resolve(obsObject.getKey()),
+				StandardCopyOption.ATOMIC_MOVE);
 		return path.toString();
 	}
 }
