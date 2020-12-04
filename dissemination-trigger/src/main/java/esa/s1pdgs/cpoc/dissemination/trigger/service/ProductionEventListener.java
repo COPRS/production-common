7cd772dbbc0c0a68ebd59157075347c0788a7e68
Anonymous <anon@anon.com>
2020-12-04 09:20:19 +0100

    S1PRO-2204: trigger listens to production events not compression events

--- dissemination-trigger/src/main/java/esa/s1pdgs/cpoc/dissemination/trigger/service/ProductionEventListener.java
+++ dissemination-trigger/src/main/java/esa/s1pdgs/cpoc/dissemination/trigger/service/ProductionEventListener.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import esa.s1pdgs.cpoc.appstatus.AppStatus;
 import esa.s1pdgs.cpoc.common.ProductCategory;
-import esa.s1pdgs.cpoc.common.ProductFamily;
 import esa.s1pdgs.cpoc.common.errors.AbstractCodedException;
 import esa.s1pdgs.cpoc.common.utils.LogUtils;
 import esa.s1pdgs.cpoc.metadata.client.MetadataClient;
@@ -25,8 +24,8 @@ import esa.s1pdgs.cpoc.mqi.client.MqiConsumer;
 import esa.s1pdgs.cpoc.mqi.client.MqiListener;
 import esa.s1pdgs.cpoc.mqi.client.MqiMessageEventHandler;
 import esa.s1pdgs.cpoc.mqi.client.MqiPublishingJob;
-import esa.s1pdgs.cpoc.mqi.model.queue.CompressionEvent;
 import esa.s1pdgs.cpoc.mqi.model.queue.DisseminationJob;
+import esa.s1pdgs.cpoc.mqi.model.queue.ProductionEvent;
 import esa.s1pdgs.cpoc.mqi.model.queue.util.CompressionEventUtil;
 import esa.s1pdgs.cpoc.mqi.model.rest.GenericMessageDto;
 import esa.s1pdgs.cpoc.mqi.model.rest.GenericPublicationMessageDto;
@@ -35,9 +34,9 @@ import esa.s1pdgs.cpoc.report.ReportingMessage;
 import esa.s1pdgs.cpoc.report.ReportingUtils;
 
 @Service
-public class CompressionEventListener implements MqiListener<CompressionEvent> {
+public class ProductionEventListener implements MqiListener<ProductionEvent> {
 
-	private static final Logger LOGGER = LogManager.getLogger(CompressionEventListener.class);
+	private static final Logger LOGGER = LogManager.getLogger(ProductionEventListener.class);
 
 	private static final String MANIFEST_SAFE_FILE = "manifest.safe";
 
@@ -49,7 +48,7 @@ public class CompressionEventListener implements MqiListener<CompressionEvent> {
 	private final AppStatus appStatus;
 
 	@Autowired
-	public CompressionEventListener(final GenericMqiClient mqiClient, final List<MessageFilter> messageFilter,
+	public ProductionEventListener(final GenericMqiClient mqiClient, final List<MessageFilter> messageFilter,
 			final MetadataClient metadataClient,
 			@Value("${dissemination-trigger.event-listener.polling-interval-ms}") final long pollingIntervalMs,
 			@Value("${dissemination-trigger.event-listener.polling-initial-delay-ms}") final long pollingInitialDelayMs,
@@ -66,56 +65,53 @@ public class CompressionEventListener implements MqiListener<CompressionEvent> {
 	public void initService() {
 		if (pollingIntervalMs > 0) {
 			final ExecutorService service = Executors.newFixedThreadPool(1);
-			service.execute(new MqiConsumer<CompressionEvent>(mqiClient, ProductCategory.LEVEL_PRODUCTS, this,
+			service.execute(new MqiConsumer<ProductionEvent>(mqiClient, ProductCategory.LEVEL_PRODUCTS, this,
 					messageFilter, pollingIntervalMs, pollingInitialDelayMs, appStatus));
 		}
 	}
 
 	@Override
-	public MqiMessageEventHandler onMessage(final GenericMessageDto<CompressionEvent> inputMessage)
+	public MqiMessageEventHandler onMessage(final GenericMessageDto<ProductionEvent> inputMessage)
 			throws AbstractCodedException {
-		LOGGER.debug("starting conversion of CompressionEvent to DisseminationJob, got message: {}", inputMessage);
-		final CompressionEvent compressionEvent = inputMessage.getBody();
+		LOGGER.debug("starting conversion of ProductionEvent to DisseminationJob, got message: {}", inputMessage);
+		final ProductionEvent productionEvent = inputMessage.getBody();
 
-		final Reporting reporting = ReportingUtils.newReportingBuilder().predecessor(compressionEvent.getUid())
+		final Reporting reporting = ReportingUtils.newReportingBuilder().predecessor(productionEvent.getUid())
 				.newReporting("DisseminationTrigger");
 
 		reporting.begin(
-				ReportingUtils.newFilenameReportingInputFor(compressionEvent.getProductFamily(),
-						compressionEvent.getKeyObjectStorage()),
-				new ReportingMessage("Handling event for %s", compressionEvent.getKeyObjectStorage()));
-
-		final ProductFamily unzippedProductFamily = CompressionEventUtil
-				.removeZipSuffixFromProductFamily(compressionEvent.getProductFamily());
-		final String unzippedKeyObs = CompressionEventUtil.removeZipSuffix(compressionEvent.getKeyObjectStorage());
+				ReportingUtils.newFilenameReportingInputFor(productionEvent.getProductFamily(),
+						productionEvent.getKeyObjectStorage()),
+				new ReportingMessage("Handling event for %s", productionEvent.getKeyObjectStorage()));
 
 		return new MqiMessageEventHandler.Builder<DisseminationJob>(ProductCategory.DISSEMINATION_JOBS)
 				.onSuccess(res -> reporting
-						.end(new ReportingMessage("End Handling event for %s", compressionEvent.getKeyObjectStorage())))
+						.end(new ReportingMessage("End Handling event for %s", productionEvent.getKeyObjectStorage())))
 				.onError(e -> reporting.error(new ReportingMessage("Error on handling event for %s: %s",
-						compressionEvent.getKeyObjectStorage(), LogUtils.toString(e))))
+						productionEvent.getKeyObjectStorage(), LogUtils.toString(e))))
 				.publishMessageProducer(() -> {
 
-					if (metadataClient.isIntersectingOceanMask(unzippedProductFamily, unzippedKeyObs)) {
-						LOGGER.info("intersects ocean mask: {}", compressionEvent.getKeyObjectStorage());
+					if (metadataClient.isIntersectingOceanMask(productionEvent.getProductFamily(), productionEvent.getKeyObjectStorage())) {
+						LOGGER.info("intersects ocean mask: {}", productionEvent.getKeyObjectStorage());
 						final DisseminationJob disseminationJob = new DisseminationJob();
-						disseminationJob.setKeyObjectStorage(compressionEvent.getKeyObjectStorage());
-						disseminationJob.setProductFamily(compressionEvent.getProductFamily());
-						disseminationJob.addDisseminationSource(compressionEvent.getProductFamily(),
-								compressionEvent.getKeyObjectStorage());
-						disseminationJob.addDisseminationSource(unzippedProductFamily,
-								unzippedKeyObs + "/" + MANIFEST_SAFE_FILE);
+						disseminationJob.setKeyObjectStorage(productionEvent.getKeyObjectStorage());
+						disseminationJob.setProductFamily(productionEvent.getProductFamily());
+						disseminationJob.addDisseminationSource(
+								CompressionEventUtil.composeCompressedProductFamily(productionEvent.getProductFamily()),
+								CompressionEventUtil.composeCompressedKeyObjectStorage(productionEvent.getKeyObjectStorage()));
+						disseminationJob.addDisseminationSource(productionEvent.getProductFamily(),
+								productionEvent.getKeyObjectStorage() + "/" + MANIFEST_SAFE_FILE);
 						disseminationJob.setUid(reporting.getUid());
 
 						final GenericPublicationMessageDto<DisseminationJob> outputMessage = new GenericPublicationMessageDto<DisseminationJob>(
-								inputMessage.getId(), compressionEvent.getProductFamily(), disseminationJob);
+								inputMessage.getId(), productionEvent.getProductFamily(), disseminationJob);
 
-						LOGGER.debug("end conversion of CompressionEvent to DisseminationJob, sent message: {}",
+						LOGGER.debug("end conversion of ProductionEvent to DisseminationJob, sent message: {}",
 								outputMessage);
 						return new MqiPublishingJob<DisseminationJob>(Collections.singletonList(outputMessage));
 					} else {
 						LOGGER.info("skipping job, product does not intersect ocean mask: {}",
-								compressionEvent.getKeyObjectStorage());
+								productionEvent.getKeyObjectStorage());
 						return new MqiPublishingJob<>(Collections.emptyList());
 					}
 				}).newResult();
