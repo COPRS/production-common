9506beb3d8066b5c950cc9089f8d331780bb6e66
Anonymous <anon@anon.com>
2020-12-04 16:48:13 +0100

    [S1PRO-2201] logging inbox entry persistence operations

--- ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/service/IngestionTriggerServiceTransactional.java
+++ ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/service/IngestionTriggerServiceTransactional.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +18,8 @@ import esa.s1pdgs.cpoc.ingestion.trigger.entity.InboxEntryRepository;
 @Component
 public class IngestionTriggerServiceTransactional {
 
+	private static final Logger LOG = LoggerFactory.getLogger(IngestionTriggerServiceTransactional.class);
+
 	private final InboxEntryRepository repository;
 	private final ProcessConfiguration processConfiguration;
 
@@ -26,16 +30,20 @@ public class IngestionTriggerServiceTransactional {
 	}
 
 	public Set<InboxEntry> getAllForPath(final String pickupURL, final String stationName, final ProductFamily productFamily) {
-		final List<InboxEntry> result = repository.findByProcessingPodAndPickupURLAndStationNameAndProductFamily(
+		final List<InboxEntry> result = this.repository.findByProcessingPodAndPickupURLAndStationNameAndProductFamily(
 				this.processConfiguration.getHostname(), pickupURL, stationName, productFamily.name());
+		LOG.debug("listing persisted inbox entries for inbox " + pickupURL + ", station " + stationName
+				+ " and product family " + productFamily + ": " + result);
 		return new HashSet<>(result);
 	}
-	
-	public void removeFinished(final Collection<InboxEntry> finishedEntries) {		
-		repository.deleteAll(finishedEntries);
-	}	
-	
+
+	public void removeFinished(final Collection<InboxEntry> finishedEntries) {
+		LOG.debug("deleting inbox entries: " + finishedEntries);
+		this.repository.deleteAll(finishedEntries);
+	}
+
 	public InboxEntry add(final InboxEntry entry) {
-		return repository.save(entry);		
+		LOG.debug("persisting inbox entry: " + entry);
+		return this.repository.save(entry);
 	}
 }
