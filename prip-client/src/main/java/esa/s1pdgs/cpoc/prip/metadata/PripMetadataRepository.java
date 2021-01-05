efee4a20c89ced57d01e891796dd58d80b3a83d9
Anonymous <anon@anon.com>
2021-01-05 18:56:59 +0100

    [S1PRO-2119] sorting (orderBy) for PRIP

--- prip-client/src/main/java/esa/s1pdgs/cpoc/prip/metadata/PripMetadataRepository.java
+++ prip-client/src/main/java/esa/s1pdgs/cpoc/prip/metadata/PripMetadataRepository.java
@@ -1,62 +1,90 @@
 package esa.s1pdgs.cpoc.prip.metadata;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import esa.s1pdgs.cpoc.prip.model.PripMetadata;
+import esa.s1pdgs.cpoc.prip.model.PripSortTerm;
 import esa.s1pdgs.cpoc.prip.model.filter.PripQueryFilter;
 
 public interface PripMetadataRepository {
 
 	/**
 	 * Saves a new PRIP metadata.
-	 * 
+	 *
 	 * @param pripMetadata
 	 */
-	public void save(PripMetadata pripMetadata);
-	
+	void save(PripMetadata pripMetadata);
+
 	/**
 	 * Finds a PRIP metadata by its ID. Returns null if not found.
-	 * 
+	 *
 	 * @param id
 	 * @return
 	 */
-	public PripMetadata findById(String id);
+	PripMetadata findById(String id);
 
 	/**
 	 * Returns all PRIP metadata.
-	 * 
+	 *
 	 * @param top
 	 * @param skip
 	 * @return
 	 */
-	public List<PripMetadata> findAll(Optional<Integer> top, Optional<Integer> skip);
+	default List<PripMetadata> findAll(Optional<Integer> top, Optional<Integer> skip) {
+		return this.findAll(top, skip, Collections.emptyList());
+	}
+
+	/**
+	 * Returns all PRIP metadata.
+	 *
+	 * @param top
+	 * @param skip
+	 * @param sortTerms
+	 * @return
+	 */
+	List<PripMetadata> findAll(Optional<Integer> top, Optional<Integer> skip, List<PripSortTerm> sortTerms);
 
 	/**
 	 * Querying the repository with filters.
-	 * When filters are empty this is like calling {@link #findAll(Optional, Optional)}. 
+	 * When filters are empty this is like calling {@link #findAll(Optional, Optional)}.
 	 *
-	 * @param filters to narrow the query 
+	 * @param filters to narrow the query
 	 * @param top for paging
 	 * @param skip fir paging
 	 * @return the search result
 	 */
-	public List<PripMetadata> findWithFilters(List<PripQueryFilter> filters, Optional<Integer> top, Optional<Integer> skip);
+	default List<PripMetadata> findWithFilters(List<PripQueryFilter> filters, Optional<Integer> top, Optional<Integer> skip) {
+		return this.findWithFilters(filters, top, skip, Collections.emptyList());
+	}
+
+	/**
+	 * Querying the repository with filters.
+	 * When filters are empty this is like calling {@link #findAll(Optional, Optional)}.
+	 *
+	 * @param filters to narrow the query
+	 * @param top for paging
+	 * @param skip fir paging
+	 * @param sortTerms
+	 * @return the search result
+	 */
+	List<PripMetadata> findWithFilters(List<PripQueryFilter> filters, Optional<Integer> top, Optional<Integer> skip, List<PripSortTerm> sortTerms);
 
 	/**
 	 * Counts all PRIP metadata.
-	 * 
+	 *
 	 * @return
 	 */
-	public int countAll();
-	
+	int countAll();
+
 	/**
 	 * Counts PRIP metadata by creation date and name using date time and name filters.
 	 * Each of the the result matches with all filters provided for the fields 'creationDate' and 'name'.
-	 * 
+	 *
 	 * @param filters can be empty
 	 * @return
 	 */
-	public int countWithFilters(List<PripQueryFilter> filters);
+	int countWithFilters(List<PripQueryFilter> filters);
 
 }
---
+++ prip-client/src/main/java/esa/s1pdgs/cpoc/prip/model/PripSortTerm.java
@@ -0,0 +1,103 @@
+package esa.s1pdgs.cpoc.prip.model;
+
+import java.util.Objects;
+
+import esa.s1pdgs.cpoc.prip.model.PripMetadata.FIELD_NAMES;
+
+public class PripSortTerm {
+
+	public enum PripSortOrder {
+		ASCENDING("asc"),
+		DESCENDING("desc");
+
+		private final String abbreviation;
+
+		private PripSortOrder(String abbreviation) {
+			this.abbreviation = abbreviation;
+		}
+
+		public String abbreviation() {
+			return this.abbreviation;
+		}
+
+		public static PripSortOrder fromString(String sortOrder) {
+			if (ASCENDING.abbreviation().equalsIgnoreCase(sortOrder) || ASCENDING.name().equalsIgnoreCase(sortOrder)) {
+				return ASCENDING;
+			}
+			if (DESCENDING.abbreviation().equalsIgnoreCase(sortOrder) || DESCENDING.name().equalsIgnoreCase(sortOrder)) {
+				return DESCENDING;
+			}
+
+			throw new IllegalArgumentException(String.format("sort order not supported: %s", sortOrder));
+		}
+	}
+
+	// --------------------------------------------------------------------------
+
+	private FIELD_NAMES sortFieldName;
+	private PripSortOrder sortOrder;
+
+	// --------------------------------------------------------------------------
+
+	public PripSortTerm(String sortFieldName, String sortOrder) {
+		this.sortFieldName = FIELD_NAMES.fromString(sortFieldName);
+		this.sortOrder = PripSortOrder.fromString(sortOrder);
+	}
+
+	public PripSortTerm(FIELD_NAMES sortFieldName, PripSortOrder sortOrder) {
+		this.sortFieldName = sortFieldName;
+		this.sortOrder = sortOrder;
+	}
+
+	// --------------------------------------------------------------------------
+
+	@Override
+	public String toString() {
+		return this.sortFieldName + " " + (null != this.sortOrder ? this.sortOrder.abbreviation() : "NO_SORT_ORDER");
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.sortFieldName, this.sortOrder);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (null == obj) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final PripSortTerm other = (PripSortTerm) obj;
+		return Objects.equals(this.sortFieldName, other.sortFieldName)
+				&& Objects.equals(this.sortOrder, other.sortOrder);
+	}
+
+	// --------------------------------------------------------------------------
+
+	public PripSortOrder getSortOrderOrDefault(PripSortOrder defaultSortOrder) {
+		return (null != this.sortOrder ? this.sortOrder : defaultSortOrder);
+	}
+
+	public PripSortOrder getSortOrder() {
+		return this.sortOrder;
+	}
+
+	public void setSortOrder(PripSortOrder sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	public FIELD_NAMES getSortFieldName() {
+		return this.sortFieldName;
+	}
+
+	public void setSortFieldName(FIELD_NAMES sortFieldName) {
+		this.sortFieldName = sortFieldName;
+	}
+
+}
