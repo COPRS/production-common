bef20844663436ded57c0cb576a8c20f2c271967
Anonymous <anon@anon.com>
2021-02-18 13:57:38 +0100

    [S1PRO-2206] starting as spring commandline app

--- directory-cleaner/src/main/java/esa/s1pdgs/cpoc/directorycleaner/Application.java
+++ directory-cleaner/src/main/java/esa/s1pdgs/cpoc/directorycleaner/Application.java
@@ -1,11 +1,32 @@
 package esa.s1pdgs.cpoc.directorycleaner;
 
-public class Application {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+//@ComponentScan("esa.s1pdgs.cpoc")
+public class Application implements CommandLineRunner {
+
+	private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("starting directory cleaner ...");
+
 		// at this point we only use this for myocean
 		final DirectoryCleaner directoryCleaner = DirectoryCleanerFactory.newDirectoryCleaner("myocean");
 		directoryCleaner.cleanDirectories();
+
+		LOGGER.info(" ... directory cleaner finished");
 	}
 
 }
