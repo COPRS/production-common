4c244f5e3347a993bc7c4f252ff8e29b545c8da1
Anonymous <anon@anon.com>
2021-04-06 16:42:19 +0200

    [S1PRO-2606] Handle exit code of 7za properly.

--- compression-worker/config/uncompression.sh
+++ compression-worker/config/uncompression.sh
@@ -5,5 +5,11 @@ INPUT=$1
 echo "Uncompressing ${INPUT}"
 7za x ${INPUT}
 
-echo "Uncompression script done."
-exit 0
+if [ $? -eq 0 ]
+then
+  echo "Uncompression script done."
+  exit $?
+else
+  echo "Error during uncompression script excecution." >&2
+  exit $?
+fi
