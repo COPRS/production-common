37312a6229276b2274f49bbeba42f2b70e512b79
Anonymous <anon@anon.com>
2021-06-22 17:23:06 +0200

    S1OPS-742: Add Upsert and Update

--- data-lifecycle-client/src/main/java/esa/s1pdgs/cpoc/datalifecycle/client/domain/persistence/DataLifecycleMetadataRepository.java
+++ data-lifecycle-client/src/main/java/esa/s1pdgs/cpoc/datalifecycle/client/domain/persistence/DataLifecycleMetadataRepository.java
@@ -3,6 +3,7 @@ package esa.s1pdgs.cpoc.datalifecycle.client.domain.persistence;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.lang.NonNull;
@@ -22,6 +23,16 @@ public interface DataLifecycleMetadataRepository {
 
 	DataLifecycleMetadata saveAndGet(@NonNull DataLifecycleMetadata metadata) throws DataLifecycleMetadataRepositoryException;
 
+	void upsert(@NonNull DataLifecycleMetadata insertMetadata, @NonNull Map<String, Object> updateMetadata)
+			throws DataLifecycleMetadataRepositoryException;
+
+	void upsertAndRefresh(@NonNull DataLifecycleMetadata insertMetadata, @NonNull Map<String, Object> updateMetadata)
+			throws DataLifecycleMetadataRepositoryException;
+
+	void update(@NonNull String productName, Map<String, Object> updateMetadata) throws DataLifecycleMetadataRepositoryException;
+
+	void updateAndRefresh(@NonNull String productName, @NonNull Map<String, Object> updateMetadata) throws DataLifecycleMetadataRepositoryException;
+	
 	Optional<DataLifecycleMetadata> findByProductName(@NonNull String productName) throws DataLifecycleMetadataRepositoryException;
 
 	List<DataLifecycleMetadata> findByProductNames(@NonNull List<String> productNames) throws DataLifecycleMetadataRepositoryException;
