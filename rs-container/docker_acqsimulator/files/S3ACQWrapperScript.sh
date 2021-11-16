b501ea94f7473c8a0578c8af614fc6b3a6409019
w-jka <w-jka@werum.de>
2021-11-16 11:46:02 +0100

    RS-105: Execution Worker assumes product.LIST is called AIOProc.LIST

--- rs-container/docker_acqsimulator/files/S3ACQWrapperScript.sh
+++ rs-container/docker_acqsimulator/files/S3ACQWrapperScript.sh
@@ -89,19 +89,19 @@ function create_outputs() {
 # Create product list file for execution worker
 ################################################################################
 function create_productlist() {
-        log "Create product.LIST file"
+        log "Create AIOProc.LIST file"
 
         # This only works, if the JobOrder was provided with an absolut path
         local workingdir=$(dirname $JOBORDER)
-        local listfile=${workingdir}/product.LIST
+        local listfile=${workingdir}/AIOProc.LIST
 
-        # Write product.LIST file with all outputs from
+        # Write AIOProc.LIST file with all outputs from
         for entry in $workingdir/*ISIP;
         do
                 echo "$(basename $entry)" >> $listfile
         done
 
-        log "product.LIST files created"
+        log "AIOProc.LIST files created"
 }
 
 ################################################################################
