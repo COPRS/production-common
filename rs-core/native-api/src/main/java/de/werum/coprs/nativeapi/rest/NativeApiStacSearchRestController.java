760d3c3862ff3dd37848e52c01b2a0fbb1c10afb
w-msc <88837700+w-msc@users.noreply.github.com>
2022-01-18 10:56:41 +0100

    RS-182: return media type changed to 'application/geo+json'

--- rs-core/native-api/src/main/java/de/werum/coprs/nativeapi/rest/NativeApiStacSearchRestController.java
+++ rs-core/native-api/src/main/java/de/werum/coprs/nativeapi/rest/NativeApiStacSearchRestController.java
@@ -3,13 +3,13 @@ package de.werum.coprs.nativeapi.rest;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +35,7 @@ public class NativeApiStacSearchRestController {
 		this.nativeApiStacService = nativeApiStacService;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.GET, produces = "application/geo+json")
 	public ResponseEntity<StacItemCollection> handleStacItemSearch(
 			final HttpServletRequest request,
 			@RequestParam(value = "datetime", required = false) final String datetime) {
