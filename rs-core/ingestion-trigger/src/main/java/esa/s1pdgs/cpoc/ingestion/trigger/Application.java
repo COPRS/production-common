5e7cfd7c9eb34261132e8c6949a7f60e8e8815b9
w-jka <w-jka@werum.de>
2022-03-14 09:31:50 +0100

    RS-213: Exclude SecurityAutoConfiguration

--- rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Application.java
+++ rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Application.java
@@ -2,10 +2,11 @@ package esa.s1pdgs.cpoc.ingestion.trigger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableConfigurationProperties
 @ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
