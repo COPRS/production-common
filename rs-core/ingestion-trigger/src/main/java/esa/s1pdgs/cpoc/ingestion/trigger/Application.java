38e015f9ef1ecacfd5f261ab8394721b45e53814
w-jka <w-jka@werum.de>
2022-03-15 08:59:57 +0100

    RS-213: Reintroduce ComponentScan

--- rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Application.java
+++ rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Application.java
@@ -3,8 +3,10 @@ package esa.s1pdgs.cpoc.ingestion.trigger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
