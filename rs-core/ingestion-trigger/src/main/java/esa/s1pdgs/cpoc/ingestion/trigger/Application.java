0c989ed86dabf734a6e9c02de7a8c062f9400bf6
w-jka <w-jka@werum.de>
2022-03-15 08:45:08 +0100

    RS-213: Remove additional annotations in IngestionTriggerApplication

--- rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Application.java
+++ rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/Application.java
@@ -3,12 +3,8 @@ package esa.s1pdgs.cpoc.ingestion.trigger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableConfigurationProperties
-@ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
