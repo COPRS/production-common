adb030482c93c22926efe9f9e39d6aa1c1d889c6
w-jka <w-jka@werum.de>
2022-03-29 15:09:49 +0200

    RS-213: Remove AppStatus from ingestion-trigger

--- rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/service/IngestionTriggerService.java
+++ rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/service/IngestionTriggerService.java
@@ -8,7 +8,6 @@ import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import esa.s1pdgs.cpoc.appstatus.AppStatus;
 import esa.s1pdgs.cpoc.ingestion.trigger.inbox.Inbox;
 import esa.s1pdgs.cpoc.mqi.model.queue.IngestionJob;
 
@@ -16,12 +15,10 @@ public final class IngestionTriggerService implements Supplier<List<IngestionJob
 	private static final Logger LOG = LoggerFactory.getLogger(IngestionTriggerService.class);
 	
 	private final List<Inbox> inboxes;
-	private final AppStatus status;
 	private final AtomicLong counter = new AtomicLong(1L);
 		
-	public IngestionTriggerService(final List<Inbox> inboxes, final AppStatus status) {
+	public IngestionTriggerService(final List<Inbox> inboxes) {
 		this.inboxes 	= inboxes;
-		this.status		= status;
 	}
 	
 	public List<IngestionJob> get() {
@@ -30,15 +27,13 @@ public final class IngestionTriggerService implements Supplier<List<IngestionJob
     	List<IngestionJob> jobs = new ArrayList<>();
 		
     	for (final Inbox inbox : inboxes) {
-        	LOG.debug("Polling {}", inbox.description());   
-        	status.setProcessing(counter.incrementAndGet());
+        	LOG.debug("Polling {}", inbox.description());
         	try {        	
         		List<IngestionJob> inboxJobs = inbox.poll();
 				jobs.addAll(inboxJobs);
 			} catch (final Exception e) {
 				LOG.error(String.format("Failed polling %s", inbox), e);			
-			}
-        	status.setWaiting();        
+			}     
     	}
       	LOG.trace("Done polling all");
       	
