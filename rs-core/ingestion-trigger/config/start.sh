59d6381139138fcabcd7c26d6ff4c57d0d1a1cde
w-fsi <88133065+w-fsi@users.noreply.github.com>
2022-04-01 11:15:57 +0200

    * renaming SCDF applications

--- rs-core/ingestion-trigger/config/start.sh
+++ rs-core/ingestion-trigger/config/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java $jvm_flags_global $([ ! -z "$JAVA_XMS" ] && echo "-Xms${JAVA_XMS}") $([ ! -z "$JAVA_XMX" ] && echo "-Xmx${JAVA_XMX}") -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-ingestion-trigger.jar --spring.config.location=/app/config/application.yml $@
+exec java $jvm_flags_global $([ ! -z "$JAVA_XMS" ] && echo "-Xms${JAVA_XMS}") $([ ! -z "$JAVA_XMX" ] && echo "-Xmx${JAVA_XMX}") -Djava.security.egd=file:/dev/./urandom -jar /app/rs-ingestion-trigger.jar $@
