07c1023fbb05200f7cdf5bd1fc923fdc41411e53
w-jed <w-jed@werum.de>
2022-04-14 14:29:09 +0200

    * Merge ingestion-worker changes

--- rs-core/ingestion-worker/src/main/java/esa/s1pdgs/cpoc/ingestion/worker/product/ProductServiceImpl.java
+++ rs-core/ingestion-worker/src/main/java/esa/s1pdgs/cpoc/ingestion/worker/product/ProductServiceImpl.java
@@ -54,6 +54,10 @@ public class ProductServiceImpl implements ProductService {
 				ingestion.getTimeliness()
 		);
 
+		// S1OPS-971: This is a workaround for MDC to allow access of additional metadata
+		if (null != ingestion.getAdditionalMetadata()) {
+			dto.setAdditionalMetadata(ingestion.getAdditionalMetadata());
+		}
 		checkExistingInObs(obsAdapter, ingestion);
 		upload(obsAdapter, ingestion, family, inboxAdapter, uri, obsKey);
 
