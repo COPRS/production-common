c5161d83f235eb6d4a634a2a7f67cf84d50dc744
w-ls <stiege@werum.de>
2022-04-27 15:47:16 +0200

    RS-XX: If the compressed file does not contain a directory, created it

--- rs-core/compression-worker/config/uncompression.sh
+++ rs-core/compression-worker/config/uncompression.sh
@@ -8,10 +8,22 @@ if echo ${INPUT} | egrep -i '\.zip$'; then
   result=$?
 elif echo ${INPUT} | egrep -i '\.(tar\.gz|tar|tgz)$'; then
   echo "Uncompressing tarred ${INPUT}"
-  # since tar 1.15 (2004-12-20), 'z' doesn't need to be provided to properly untar gzipped files
-  # see https://www.gnu.org/software/tar/
-  tar xf ./${INPUT}
-  result=$?
+  
+  # if the tar doesn't contain any subdirectories, create a subdirectory with the basename
+  # of the tar
+  if tar tf ./${INPUT} | grep -v /; then
+    # remove the compression suffix
+    SUBDIR=$(echo $INPUT | sed -r 's;\.(tar\.gz|tar|tgz)$;;gI')
+    echo "Creating subdirectory ${SUBDIR}"
+    mkdir ${SUBDIR}
+    tar xf ./${INPUT} -C ${SUBDIR}
+    result=$?
+  else
+    # since tar 1.15 (2004-12-20), 'z' doesn't need to be provided to properly untar gzipped files
+    # see https://www.gnu.org/software/tar/
+    tar xf ./${INPUT}
+    result=$?
+  fi
 else
   echo "ERROR: Unexpected file to uncompress ${INPUT}. Supported are: .zip, .tar.gz, .tgz and tar"  
   exit 1
