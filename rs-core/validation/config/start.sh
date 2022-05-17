3e10d4d5dcc6105febe7d5a872b0d3add593559f
w-fsi <88133065+w-fsi@users.noreply.github.com>
2022-05-17 10:30:40 +0200

    * removing validation service as not used in COPRS

--- rs-core/validation/config/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec java $jvm_flags_global $([ ! -z "$JAVA_XMS" ] && echo "-Xms${JAVA_XMS}") $([ ! -z "$JAVA_XMX" ] && echo "-Xmx${JAVA_XMX}") -Djava.security.egd=file:/dev/./urandom -jar /app/s1pdgs-validation.jar --spring.config.location=/app/config/application.yml
