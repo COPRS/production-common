5d0d524789f9ab359172f8696cb441f120259e84
w-jed <w-jed@werum.de>
2022-06-07 13:17:26 +0200

    RS-314: Fix

--- rs-core/dlq-manager/src/main/java/esa/s1pdgs/cpoc/dlq/manager/Application.java
+++ rs-core/dlq-manager/src/main/java/esa/s1pdgs/cpoc/dlq/manager/Application.java
@@ -2,9 +2,11 @@ package esa.s1pdgs.cpoc.dlq.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @ComponentScan("esa.s1pdgs.cpoc")
 public class Application {
     public static void main(final String[] args) {
