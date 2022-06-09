baaaf3bf8770f078301c7fe25e76f7dc3639e6bd
w-jka <w-jka@werum.de>
2022-06-09 16:38:27 +0200

    RS-386: Rename s1pro-execution-worker to rs-execution-worker

---
+++ rs-core/execution-worker/config/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/app/airbus/entrypoint.sh
+exec java $jvm_flags_global $([ ! -z "$JAVA_XMS" ] && echo "-Xms${JAVA_XMS}") $([ ! -z "$JAVA_XMX" ] && echo "-Xmx${JAVA_XMX}") -Djava.security.egd=file:/dev/./urandom -jar /app/rs-execution-worker.jar "$@"
