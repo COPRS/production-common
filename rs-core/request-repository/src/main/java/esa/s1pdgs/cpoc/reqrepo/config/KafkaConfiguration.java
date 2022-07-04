0ed40fa20816c7ec5c93c647a303b6360bb60da8
w-jed <w-jed@werum.de>
2022-07-04 20:12:40 +0200

    RS-315: Import KafkaProducerConfiguration (pt2)

--- rs-core/request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/config/KafkaConfiguration.java
+++ rs-core/request-repository/src/main/java/esa/s1pdgs/cpoc/reqrepo/config/KafkaConfiguration.java
@@ -1,9 +1,11 @@
 package esa.s1pdgs.cpoc.reqrepo.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import esa.s1pdgs.cpoc.message.kafka.config.KafkaProducerConfiguration;
 
+@Configuration
 @Import(KafkaProducerConfiguration.class)
 public class KafkaConfiguration {
 
