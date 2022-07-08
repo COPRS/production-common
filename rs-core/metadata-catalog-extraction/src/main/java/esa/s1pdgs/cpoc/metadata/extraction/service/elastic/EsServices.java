23be1772ccb4b13e9de83afc1254c5fb11a383a9
Birol Colak <birol.colak@werum.de>
2022-07-08 10:18:30 +0200

    RS-436, RS-429 : Workaround for dropping self-intersecting footprints for S3 L0 products

--- rs-core/metadata-catalog-extraction/src/main/java/esa/s1pdgs/cpoc/metadata/extraction/service/elastic/EsServices.java
+++ rs-core/metadata-catalog-extraction/src/main/java/esa/s1pdgs/cpoc/metadata/extraction/service/elastic/EsServices.java
@@ -200,6 +200,13 @@ public class EsServices {
 					product.remove("segmentCoordinates");
 					fixed = true;
 				}
+				
+				if (family == ProductFamily.S3_L0 && e.getDetailedMessage() != null && e.getDetailedMessage().contains("Self-intersection at or near point")) {
+					warningMessage = "Invalid self-intersecting footprint detected, dropping it as a workaround for #RS-436";
+					LOGGER.warn(warningMessage);
+					product.remove("sliceCoordinates");
+					fixed = true;
+				}
 
 				if (!fixed) {
 					throw e;
