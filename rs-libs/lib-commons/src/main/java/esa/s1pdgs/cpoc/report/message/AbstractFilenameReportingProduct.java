87b3bb0656377f56c7387c21d4ba8d194d50c8e2
w-bc <91957777+w-bc@users.noreply.github.com>
2022-07-13 10:47:34 +0200

    RS-449: Move from “filename_strings” to “filename_string” when single element

--- rs-libs/lib-commons/src/main/java/esa/s1pdgs/cpoc/report/message/AbstractFilenameReportingProduct.java
+++ rs-libs/lib-commons/src/main/java/esa/s1pdgs/cpoc/report/message/AbstractFilenameReportingProduct.java
@@ -1,23 +1,30 @@
 package esa.s1pdgs.cpoc.report.message;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import esa.s1pdgs.cpoc.report.ReportingFilenameEntries;
 
 public abstract class AbstractFilenameReportingProduct {
-	@JsonProperty("filename_strings")
-	@JsonInclude(value=Include.NON_EMPTY, content=Include.NON_NULL)
+	@JsonIgnore
 	private final List<String> filenames;
-	
+
 	protected AbstractFilenameReportingProduct(final ReportingFilenameEntries entries) {
 		this.filenames = entries.getFilenames();
 	}
 
-	public final List<String> getFilenames() {
-		return filenames;
+	@JsonAnyGetter
+	public final Map<String, List<String>> getFilenames() {
+		if (filenames == null || filenames.size() == 0) {
+			return null;
+		} else if (filenames.size() > 1) {
+			return Collections.singletonMap("filename_strings", filenames);
+		} else {
+			return Collections.singletonMap("filename_string", filenames);
+		}
 	}
 }
