3f040b053f4211234c563129f94d88c441ea5a19
w-fsi <88133065+w-fsi@users.noreply.github.com>
2022-10-04 15:17:07 +0200

    RS-584: When detecting custom retrieval mode for SL1, set it to ValIntersect

--- rs-libs/xml-tools/src/main/java/esa/s1pdgs/cpoc/xml/model/tasktable/TaskTableInputAlternative.java
+++ rs-libs/xml-tools/src/main/java/esa/s1pdgs/cpoc/xml/model/tasktable/TaskTableInputAlternative.java
@@ -37,6 +37,9 @@ public class TaskTableInputAlternative {
 	 */
 	@XmlElement(name = "Retrieval_Mode")
 	private String retrievalMode;
+	
+	@XmlElement(name = "customClass", required = false)
+	private String customClass;
 
 	/**
 	 * 
@@ -114,6 +117,18 @@ public class TaskTableInputAlternative {
 	 */
 	public String getRetrievalMode() {
 		return retrievalMode;
+	}
+	
+	public void setRetrievalMode(String retrievalMode) {
+		this.retrievalMode = retrievalMode;
+	}
+	
+	
+	/**
+	 * @return the custom class or null
+	 */
+	public String getCustomClass() {
+		return customClass;
 	}
 
 	/**
