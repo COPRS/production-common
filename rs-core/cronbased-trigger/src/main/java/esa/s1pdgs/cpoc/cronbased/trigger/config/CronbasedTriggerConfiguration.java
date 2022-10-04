354616d3502d6ac7765f5b39efcad245bd16cc06
w-jka <w-jka@werum.de>
2022-10-04 14:41:22 +0200

    RS-526: Add new application cronbased-trigger

---
+++ rs-core/cronbased-trigger/src/main/java/esa/s1pdgs/cpoc/cronbased/trigger/config/CronbasedTriggerConfiguration.java
@@ -0,0 +1,32 @@
+package esa.s1pdgs.cpoc.cronbased.trigger.config;
+
+import java.util.List;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import esa.s1pdgs.cpoc.cronbased.trigger.db.CronbasedTriggerEntryRepository;
+import esa.s1pdgs.cpoc.cronbased.trigger.service.CronbasedTriggerService;
+import esa.s1pdgs.cpoc.metadata.client.MetadataClient;
+import esa.s1pdgs.cpoc.mqi.model.queue.CatalogEvent;
+
+@Configuration
+public class CronbasedTriggerConfiguration {
+
+	@Autowired
+	private CronbasedTriggerProperties properties;
+	
+	@Autowired
+	private MetadataClient metadataClient;
+	
+	@Autowired
+	private CronbasedTriggerEntryRepository repository;
+
+	@Bean
+	public Function<Message<?>, List<Message<CatalogEvent>>> cronbasedTrigger() {
+		return new CronbasedTriggerService(properties, metadataClient, repository);
+	}
+}
