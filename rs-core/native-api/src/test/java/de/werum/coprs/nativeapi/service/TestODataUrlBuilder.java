08f3259e6e7a3ae50b8e97ce7b9d3e01e3749193
w-fsi <88133065+w-fsi@users.noreply.github.com>
2023-02-09 10:16:59 +0100

    RS-XX

--- rs-core/native-api/src/test/java/de/werum/coprs/nativeapi/service/TestODataUrlBuilder.java
+++ rs-core/native-api/src/test/java/de/werum/coprs/nativeapi/service/TestODataUrlBuilder.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+ 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestODataUrlBuilder {
