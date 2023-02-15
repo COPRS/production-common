32412df3fd83a62cb4212372bdae051d8aeea7c7
w-fsi <88133065+w-fsi@users.noreply.github.com>
2023-02-15 10:07:39 +0100

    RS-XX: Fixing typo in test

--- rs-core/native-api/src/test/java/de/werum/coprs/nativeapi/service/TestODataUrlBuilder.java
+++ rs-core/native-api/src/test/java/de/werum/coprs/nativeapi/service/TestODataUrlBuilder.java
@@ -28,7 +28,7 @@ public class TestODataUrlBuilder {
 		System.out.println(url2);
 
 		Assert.assertEquals(
-				"http://s1pro-prip-frontend-svc.processing.svc.cluster.local:8080/odata/v1/Products?$filter=ContentDate/Start gt 2010-10-18T14:33:00.000Z and ContentDate/End lt 2023-02-06T14:33:00.000 and contains(Name,'S3A_PRODUCT')&$top=100')",
+				"http://s1pro-prip-frontend-svc.processing.svc.cluster.local:8080/odata/v1/Products?$filter=ContentDate/Start gt 2010-10-18T14:33:00.000Z and ContentDate/End lt 2023-02-06T14:33:00.000 and contains(Name,'S3A_PRODUCT')&$top=100",
 				url1);
 		Assert.assertEquals(
 				"http://s1pro-prip-frontend-svc.processing.svc.cluster.local:8080/odata/v1/Products?$filter=ContentDate/Start gt 2010-10-18T14:33:00.000Z and ContentDate/End lt 2023-02-06T14:33:00.000 and contains(Name,'S3A_PRODUCT')&$expand=Attributes,Quicklooks&$top=100",
