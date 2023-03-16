ae7a5458bdb1a32ad76136c65be6211ef2d3a079
w-jka <w-jka@werum.de>
2023-03-16 10:47:21 +0100

    RS-857: Add allowDiskUse option to findAll() in request-repository

--- rs-core/request-parking-lot/src/main/java/de/werum/coprs/requestparkinglot/repo/FailedProcessingRepo.java
+++ rs-core/request-parking-lot/src/main/java/de/werum/coprs/requestparkinglot/repo/FailedProcessingRepo.java
@@ -1,7 +1,10 @@
 package de.werum.coprs.requestparkinglot.repo;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.Meta;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,9 @@ import esa.s1pdgs.cpoc.errorrepo.model.rest.FailedProcessing;
 @Service
 public interface FailedProcessingRepo extends MongoRepository<FailedProcessing, String>{
 
+	@Meta(allowDiskUse = true)
+	public List<FailedProcessing> findAll(Sort sort);
+	
 	public Optional<FailedProcessing> findById(String id);
 
 	public void deleteById(String id);
