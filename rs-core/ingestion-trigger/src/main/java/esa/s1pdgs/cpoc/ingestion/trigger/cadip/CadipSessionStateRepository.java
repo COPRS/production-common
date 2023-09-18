8b929cba30e8df36d9edbf1793ede59f25ce9cc2
w-fsi <88133065+w-fsi@users.noreply.github.com>
2023-09-18 15:47:15 +0200

    RS-XX: Making CadipSessionState ObjectId instead of long

--- rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/cadip/CadipSessionStateRepository.java
+++ rs-core/ingestion-trigger/src/main/java/esa/s1pdgs/cpoc/ingestion/trigger/cadip/CadipSessionStateRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CadipSessionStateRepository extends MongoRepository<CadipSessionState, String> {
+public interface CadipSessionStateRepository extends MongoRepository<CadipSessionState, ObjectId> {
 
 	Optional<CadipSessionState> findByPodAndCadipUrlAndSessionId(final String pod, final String cadipUrl,
 			final String sessionId);
